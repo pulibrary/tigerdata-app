91807bbd59d105c0f3185375caf3f642690db7a7
mccalluc <mccalluc@users.noreply.github.com>
2022-11-16 14:57:55 -0500

    require "simplecov"

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "simplecov"
+SimpleCov.start "rails"
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
