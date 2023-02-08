d1dd16acadd6763bc2340c98a1c54fcc9cb3292a
mccalluc <mccalluc@users.noreply.github.com>
2023-02-08 15:28:46 -0500

    in deploy.rb, add "main" default

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,5 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :branch, ENV["BRANCH"] || "main"
