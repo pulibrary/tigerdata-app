876a6d975604a273445cc5e049f56f0b0b6e280f
mccalluc <mccalluc@users.noreply.github.com>
2023-02-09 09:42:46 -0500

    ignore jslint errors in generated JS

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,7 @@ import { eagerLoadControllersFrom } from '@hotwired/stimulus-loading';
 
 eagerLoadControllersFrom('controllers', application);
 
-// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
+// Lazy load controllers as they appear in the DOM
+// (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
