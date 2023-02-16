95fb98cc2b2e86b68b0f9e6d81dfdd8dcd7d894b
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-16 17:09:58 -0500

    rephrase

--- app/models/api_middleware.rb
+++ app/models/api_middleware.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 class ApiMiddleware
   # The database and Mediaflux should only be accessed through this class.
-  # Since our backend is in flux, and we're not sure what will be handled
-  # by the database and what by mediaflux, we do not want references to
-  # either scattered across the codebase.
+  # Since the respective responsibilities of the the two systems have not
+  # been determined, we want all references to both in just one place.
   #
-  # As the middleware grows to support the needs of the frontend,
+  # As the middleware grows to support the needs of the UI,
   # we need to be sure to add corresponding API routes.
   def projects
     [{ name: "fake project" }]
