9bc86b0698efc93562d4c9ef64679d16594f6f37
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-17 17:28:23 -0500

    eslint fix

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,26 +1,27 @@
-// import 'bootstrap/js/src/alert'  
-// import 'bootstrap/js/src/button'  
-// import 'bootstrap/js/src/carousel'  
-import 'bootstrap/js/src/collapse'  
-import 'bootstrap/js/src/dropdown'  
-// import 'bootstrap/js/src/modal'  
-// import 'bootstrap/js/src/popover'  
-import 'bootstrap/js/src/scrollspy'  
-// import 'bootstrap/js/src/tab'  
-// import 'bootstrap/js/src/toast'  
-// import 'bootstrap/js/src/tooltip' 
+// import 'bootstrap/js/src/alert'
+// import 'bootstrap/js/src/button'
+// import 'bootstrap/js/src/carousel'
+import 'bootstrap/js/src/collapse';
+import 'bootstrap/js/src/dropdown';
+// import 'bootstrap/js/src/modal'
+// import 'bootstrap/js/src/popover'
+import 'bootstrap/js/src/scrollspy';
+// import 'bootstrap/js/src/tab'
+// import 'bootstrap/js/src/toast'
+// import 'bootstrap/js/src/tooltip'
 
 import * as bootstrap from 'bootstrap';
+
 window.bootstrap = bootstrap;
 
 // initialize the page
 window.addEventListener('load', (event) => {
-    initPage();
+  initPage();
 });
 window.addEventListener('turbo:render', (event) => {
-    initPage();
+  initPage();
 });
 
-window.test_jquery = function() {
-    $('#test-jquery').html('jQuery works!')
-}
+window.test_jquery = function () {
+  $('#test-jquery').html('jQuery works!');
+};
