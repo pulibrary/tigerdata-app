fec29d6b34a632bf86ada1f7e26ea9d6829de6e7
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-17 11:54:17 -0500

    "yarn remove vite-plugin-ruby"... tests pass now

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
 import ViteRails from 'vite-plugin-rails'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
     ViteRails(),
   ],
 })
