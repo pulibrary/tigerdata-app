d1063961fbee19a752aa115bc7c26efffd971334
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-17 16:41:36 -0500

    checkpoint: instructions for popover followed, but not working

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,36 +1,32 @@
-/* eslint no-console: "off" */
+// import 'bootstrap/js/src/alert'  
+// import 'bootstrap/js/src/button'  
+// import 'bootstrap/js/src/carousel'  
+import 'bootstrap/js/src/collapse'  
+import 'bootstrap/js/src/dropdown'  
+// import 'bootstrap/js/src/modal'  
+// import 'bootstrap/js/src/popover'  
+import 'bootstrap/js/src/scrollspy'  
+// import 'bootstrap/js/src/tab'  
+// import 'bootstrap/js/src/toast'  
+// import 'bootstrap/js/src/tooltip' 
 
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails');
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
-
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails');
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
-
-import $ from 'jquery';
-window.$ = $;
+// initialize the page
+window.addEventListener('load', (event) => {
+    initPage();
+});
+window.addEventListener('turbo:render', (event) => {
+    initPage();
+});
+function initPage() {
+      // initialize popovers
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+}
 
 window.test_jquery = function() {
     $('#test-jquery').html('jQuery works!')
