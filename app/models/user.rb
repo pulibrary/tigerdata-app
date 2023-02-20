59cefbb8fe9d5baf2fecfb8e7ae69aa58e021a87
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-20 15:29:53 -0500

    add auth to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :omniauthable
+
+  def self.from_cas(access_token)
+    User.where(provider: access_token.provider, uid: access_token.uid).first
+ 
+    # You can update this logic to create users automatically after they have authenticated
+    # via CAS. The hash in `request.env["omniauth.auth"]` has the information about the
+    # authenticated user.
+ 
+  end
 end
