79cbf69a0e8c46e02a53c786e7ac871dc0f2d4a3
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-20 15:42:34 -0500

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,10 +6,9 @@ class User < ApplicationRecord
 
   def self.from_cas(access_token)
     User.where(provider: access_token.provider, uid: access_token.uid).first
- 
+
     # You can update this logic to create users automatically after they have authenticated
     # via CAS. The hash in `request.env["omniauth.auth"]` has the information about the
     # authenticated user.
- 
   end
 end
