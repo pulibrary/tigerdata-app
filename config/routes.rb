207a21f59121d8085da9d00c7e2e420b4efbdd1e
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-20 15:35:32 -0500

    add devise to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get "sign_in", to: "devise/sessions#new", as: :new_user_session
+    get "sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
