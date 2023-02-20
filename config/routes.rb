ea16b1fcf54d8c0bf90444ca3b8334f747f20531
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-20 14:23:09 -0500

    rails generate devise User; rails db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
