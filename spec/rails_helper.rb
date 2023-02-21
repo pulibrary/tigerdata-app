573fa6ea385186ea6502ff0f8d7c6a1ccff2c6c6
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-21 15:09:37 -0500

    remove copy and paste

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,19 +63,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  Capybara.register_driver :headless_chrome do |app|
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-  
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :chrome,
-      options: options
-    )
-  end
-  
   Capybara.register_driver :chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless') unless ENV["RUN_IN_BROWSER"]
   
     Capybara::Selenium::Driver.new(
       app,
@@ -86,12 +76,7 @@ RSpec.configure do |config|
   
   Capybara.default_driver = :chrome
 
-
   config.before(:each, type: :system) do
-    if ENV["RUN_IN_BROWSER"]
-      driven_by(:chrome)
-    else
-      driven_by(:headless_chrome)
-    end
+    driven_by(:chrome)
   end
 end
