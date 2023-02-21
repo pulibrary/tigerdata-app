745da8e4886056bbbdee99714e6b35c92d99f528
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-21 16:45:34 -0500

    intentional test failure, to confirm that screen captures work

--- spec/system/welcome_spec.rb
+++ spec/system/welcome_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "WelcomeController" do
   it "visits the root", js: true do
     visit "/"
 
-    expect(page).to have_content "Under Construction"
+    expect(page).to have_content "Under Construction?"
     expect(page).to have_content "Welcome to TigerData"
     click_on "Test jQuery"
     expect(page).to have_content "jQuery works!"
