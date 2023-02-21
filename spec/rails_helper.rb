3766a95ec18100f2713f3e3d92a188dffccd44d5
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-21 15:06:11 -0500

    copy paste from SO silences warning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,11 +63,35 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  Capybara.register_driver :headless_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+  
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options
+    )
+  end
+  
+  Capybara.register_driver :chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+  
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options
+    )
+  end
+  
+  Capybara.default_driver = :chrome
+
+
   config.before(:each, type: :system) do
     if ENV["RUN_IN_BROWSER"]
-      driven_by(:selenium_chrome)
+      driven_by(:chrome)
     else
-      driven_by(:selenium_chrome_headless)
+      driven_by(:headless_chrome)
     end
   end
 end
