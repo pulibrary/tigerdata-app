8012588a8e791e35e6e8887e05214fa2eee08213
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-21 14:50:40 -0500

    Add support for RUN_IN_BROWSER

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each, type: :system) do
+    if ENV["RUN_IN_BROWSER"]
+      driven_by(:selenium_chrome)
+    else
+      driven_by(:selenium_chrome_headless)
+    end
+  end
 end
