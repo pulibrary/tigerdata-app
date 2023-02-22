029b7bd4392b0ea62ee145c5b9e2fd7c59f0fbb3
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-22 15:38:33 -0500

    remove unused factory

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def cas
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_cas(request.env["omniauth.auth"])
 
     if @user.nil?
--- spec/factories/user.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-FactoryBot.define do
-  factory :user do
-    sequence(:uid) { "uid#{srand}" }
-    sequence(:email) { "email-#{srand}@princeton.edu" }
-    provider "cas"
-    password "foobarfoo"
-  end
-end
