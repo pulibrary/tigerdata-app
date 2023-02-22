1da820e2d596908f3bd0c3b8153c87c93bcfc98e
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-22 14:01:15 -0500

    Add more code from the original CAS PR for pdc_describe; login works, but the UI should update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
     @projects = MediafluxWrapper.new.projects
 
