f9e803a32ef101b884899906247c5836bfd37f81
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-23 15:05:15 -0500

    new tests pass

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -4,21 +4,30 @@ require "rails_helper"
 RSpec.describe Users::OmniauthCallbacksController do
   before { request.env["devise.mapping"] = Devise.mappings[:user] }
 
-  context "valid user login" do
-    it "redirects to home page with success notice" do
-      allow(User).to receive(:from_cas) { FactoryBot.create(:user) }
+  context "TigerData user" do
+    it "redirects to home page with notice" do
+      allow(User).to receive(:from_cas) { FactoryBot.create(:user, uid: 'knight') }
       get :cas
       expect(response).to redirect_to(root_path)
-      expect(flash[:notice]).to eq("Successfully authenticated from Princeton Central Authentication Service account.")
+      expect(flash.notice).to eq("Welcome, knight")
     end
   end
 
-  context "invalid user" do
-    it "redirects to home page with warning notice" do
+  context "non-TigerData user" do
+    it "redirects to home page with alert" do
+      allow(User).to receive(:from_cas) { FactoryBot.create(:user) }
+      get :cas
+      expect(response).to redirect_to(root_path)
+      expect(flash.alert).to eq("You are not a recognized TigerData user")
+    end
+  end
+
+  context "non-CAS user" do
+    it "redirects to home page with alert" do
       allow(User).to receive(:from_cas) { nil }
       get :cas
       expect(response).to redirect_to(root_path)
-      expect(flash[:notice]).to eq("You are not authorized")
+      expect(flash.alert).to eq("You are not a recognized CAS user")
     end
   end
 end
