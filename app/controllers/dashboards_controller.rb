12c8d0c79300a01c2cac134f99d1785c929fdb24
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-27 12:01:41 -0500

    rubocop

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 class DashboardsController < ApplicationController
-    def show
-        uid = current_user.uid
-        allowed_roles = YAML.load_file("users.yaml")[uid]["roles"]
-        @role = params[:role]
-        if !(allowed_roles.include?(@role))
-            render '/access_denied', :status => 403
-        end
+  def show
+    uid = current_user.uid
+    allowed_roles = YAML.load_file("users.yaml")[uid]["roles"]
+    @role = params[:role]
+    unless allowed_roles.include?(@role)
+      render "/access_denied", status: :forbidden
     end
+  end
 end
-  
