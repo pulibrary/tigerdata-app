9aced9c11048047b86a19d9bba047284ab217794
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-27 11:04:50 -0500

    change user list to user hash

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.nil?
       redirect_to root_path
       flash.alert = "You are not a recognized CAS user."
-    elsif !YAML.load_file("users.yaml").include?(@user.uid)
+    elsif !YAML.load_file("users.yaml").keys.include?(@user.uid)
       redirect_to root_path
       flash.notice = "TigerData is coming soon; Access is currently limited."
     else
