b00a3e0fc76646272be03d824723ea03dc0ee1fb
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-02-28 12:47:37 -0500

    simplify yaml output

--- lib/xml_utilities.rb
+++ lib/xml_utilities.rb
@@ -1,18 +1,32 @@
+require 'yaml'
+
 # frozen_string_literal: true
 TYPENAMES = { 1 => "element", 2 => "attribute", 3 => "text", 4 => "cdata", 8 => "comment" }.freeze
 
-def xml_to_hash(document)
+def xml_node_to_hash(node)
   # Based on https://stackoverflow.com/a/10144623
-
-  node = document.root
-
-  { kind: TYPENAMES[node.node_type], name: node.name }.tap do |h|
+  {}.tap do |h|
+    h[:name] = node.name
+    node_type_name = TYPENAMES[node.node_type]
+    if node_type_name != "element"
+      h[:name] += " (#{node_type_name})"
+    end
     if node.namespace
       h[:nshref] = node.namespace.href
       h[:nsprefix] = node.namespace.prefix
     end
     h[:text] = node.text unless node.text.empty?
-    h[:attr] = node.attribute_nodes.map(&:to_hash) if node.element? && !node.attribute_nodes.empty?
-    h.merge! kids: node.children.map(&:to_hash) if node.element? && !node.children.empty?
+    h[:attr] = node.attribute_nodes.map{ |attr_node| xml_node_to_hash(attr_node) } if node.element? && !node.attribute_nodes.empty?
+    h.merge! subelements: node.children.map{ |child_node| xml_node_to_hash(child_node) } if node.element? && !node.children.empty?
   end
+end
+
+def xml_doc_to_hash(document)
+  xml_node_to_hash(document.root)
+end
+
+def xml_doc_to_html(document)
+  as_hash = xml_doc_to_hash(document)
+  as_yaml = YAML.dump(as_hash).gsub(/\A---\n/, "")
+  "<pre>#{CGI::escapeHTML(as_yaml)}</pre>"
 end
--- spec/lib/xml_utilities_spec.rb
+++ spec/lib/xml_utilities_spec.rb
@@ -4,6 +4,14 @@ require "xml_utilities"
 
 describe "xml_utilities" do
   it "translates xml to hash" do
-    expect(xml_to_hash(Nokogiri("<a></a>"))).to eq({ kind: "element", name: "a" })
+    expect(xml_doc_to_hash(Nokogiri("<a></a>"))).to eq({ name: "a" })
+  end
+
+  it "translates xml to hash" do
+    expect(xml_doc_to_html(Nokogiri("<a></a>"))).to eq("<pre>:name: a\n</pre>")
+  end
+
+  it "translates asset type definition to hash" do
+    expect(xml_doc_to_html(Nokogiri('<definition><element name="my_string" type="string" /></definition>'))).to eq("<pre>:name: definition\n:subelements:\n- :name: element\n  :attr:\n  - :name: name (attribute)\n    :text: my_string\n  - :name: type (attribute)\n    :text: string\n</pre>")
   end
 end
