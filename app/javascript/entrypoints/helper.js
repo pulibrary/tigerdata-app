0b134f3d977439f232a72b9870025b50be893508
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-03-01 09:30:45 -0500

    Demonstrate JS testing (#85)
    * Add failing stub test
    
    * test 2+2=4
    
    * add JS testing to CI
    
    * add jsdom
    
    * fix names in circle ci config
    
    * eslint passes
    
    * make test positive
    
    * test js helper
    
    * eslint

---
+++ app/javascript/entrypoints/helper.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/prefer-default-export
+export function setTargetHtml(event, html) {
+  $(event.target).html(html);
+}
---
+++ app/javascript/tests/helper.test.js
@@ -0,0 +1,21 @@
+// @vitest-environment jsdom
+
+import {
+  // assert,
+  describe, expect, it,
+} from 'vitest';
+import { JSDOM } from 'jsdom';
+import { setTargetHtml } from '../entrypoints/helper';
+
+describe('helper', () => {
+  it('has a window', () => {
+    expect(typeof window).toBe('object');
+  });
+
+  it('can test jQuery', () => {
+    const dom = new JSDOM('<!DOCTYPE html><h1>Hello world</h1></html>');
+    const h1 = dom.window.document.querySelector('h1');
+    setTargetHtml({ target: h1 }, 'Goodbye');
+    expect(h1.innerHTML).toEqual('Goodbye');
+  });
+});
