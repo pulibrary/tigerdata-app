0adc12e73554bb1e1666e497beb75c969bf41348
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-03-01 15:55:01 -0500

    seeds; rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+
+Role.create(name: "Data Sponsor", description_md:"
+- The person bearing primary responsibility for a project
+- Must be eligible to be a Principal Investigator")
+
+Role.create(name: "Data Manager", description_md:"
+- The person who manages day-to-day project activities
+- Must be trained by the Research Data Service")
+
+Role.create(name: "Data User", description_md:"
+- A person who accesses data/metadata in a project
+- Must be an affiliate of Princeton University")
