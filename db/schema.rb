2290c8840140e319f59203c9e34a4ae57a1ab876
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-03-01 15:29:15 -0500

    Add Role

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_203551) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_202533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.text "description_md"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
