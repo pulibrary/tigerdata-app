734491e3b57d7b6fbf15c924bf3f86776b9e6726
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-03-02 11:19:29 -0500

    Seed projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 class Project < ApplicationRecord
   has_many :project_user_roles, dependent: :restrict_with_exception
+
+  def name
+    data["name"]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,25 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-YAML.load_file("seeds/roles.yaml").each do |role|
+YAML.load_file("seeds/roles.yaml").each do |seed_role|
     Role.create(
-        name: role["name"],
-        description_md: role["description_md"]
+        name: seed_role["name"],
+        description_md: seed_role["description_md"]
     )
 end
 
-YAML.load_file("seeds/users.yaml").each do |user|
+YAML.load_file("seeds/users.yaml").each do |seed_user|
+    uid = seed_user["uid"]
     u = User.create(
         provider: "cas",
-        uid: user["uid"],
-        email: user["uid"] + "@princeton.edu"
+        uid: uid,
+        email: uid + "@princeton.edu"
     )
-    user["allowed_roles"].each do |allowed_role|
+    seed_user["allowed_roles"].each do |allowed_role|
         r = Role.find_by(name: allowed_role)
         u.allowed_roles.create(role: r)
+
+        p = Project.create(data: {name: "Sample project with #{uid} as #{allowed_role}"})
+        p.project_user_roles.create(role: r, user: u)
     end
 end
