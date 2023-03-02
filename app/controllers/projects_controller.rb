f3abc79dd9fd77a6cf94876d2abebe0488ab181b
Chuck McCallum <mccalluc@users.noreply.github.com>
2023-03-02 11:59:23 -0500

    rubocop

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class ProjectsController < ApplicationController
-    def show
-        @project = Project.find_by(id: params[:id])
-    end
+  def show
+    @project = Project.find_by(id: params[:id])
   end
-  
+end
