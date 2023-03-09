e00a63261e274d60494b4676a067aedd49f4fb87
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2023-03-09 10:40:47 -0500

    Ensuring that the config/deploy.rb file for Capistrano does not lock to any version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.1"
 
 set :application, "tigerdata"
 set :repo_url, "https://github.com/pulibrary/tiger-data-app.git"
