7e7435a858d48263c0e0c5e06bb230f9bc9a1d69
Hector Correa <hector_correa@princeton.edu>
2023-03-09 15:14:51 -0500

    Implements basic pagination. Shows results only for authenticated users.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,26 +2,34 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    host = Rails.configuration.mediaflux["api_host"]
-    domain = Rails.configuration.mediaflux["api_domain"]
-    user = Rails.configuration.mediaflux["api_user"]
-    password = Rails.configuration.mediaflux["api_password"]
-    transport = "http"
-
-    mf = MediaFluxClient.new(host, domain, user, password, transport)
-    @demo_namespace = "/tigerdata/td-demo-001"
-    @mf_version = mf.version
-    @assets = query_assets(mf)
+    return if current_user.nil?
+    @mf_version = media_flux.version
+    @demo_namespace = params[:namespace].nil? ? "/tigerdata/td-demo-001" : params[:namespace]
+    start = params[:start].nil? ? 1 : params[:start].to_i
+    @result = query_assets(@demo_namespace, start)
   end
 
+  def media_flux
+    @media_flux ||= begin
+      Rails.logger.info "Connecting to MF"
+      host = Rails.configuration.mediaflux["api_host"]
+      domain = Rails.configuration.mediaflux["api_domain"]
+      user = Rails.configuration.mediaflux["api_user"]
+      password = Rails.configuration.mediaflux["api_password"]
+      transport = "http"
+      MediaFluxClient.new(host, domain, user, password, transport)
+    end
+  end
 
-  def query_assets(mf)
+  def query_assets(namespace, start)
+    # Get the IDs of the assets...
+    result = media_flux.query("namespace='#{namespace}'", idx: start)
+    # ...fetch the individual metadata for each id
     assets = []
-    asset_ids = mf.query("namespace='#{@demo_namespace}'")
-    asset_ids.each do |id|
-      metadata = mf.get_metadata(id)
+    result[:ids].each do |id|
+      metadata = media_flux.get_metadata(id)
       assets << metadata
     end
-    assets
+    { assets: assets, result: result }
   end
 end
