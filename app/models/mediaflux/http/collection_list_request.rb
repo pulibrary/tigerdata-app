644f0f3431c39279823f367070ab950a0c32687a
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2023-03-14 11:37:41 -0400

    Correcting and extending the documentation for Mediaflux class methods

--- app/models/mediaflux/http/collection_list_request.rb
+++ app/models/mediaflux/http/collection_list_request.rb
@@ -2,14 +2,20 @@
 module Mediaflux
   module Http
     class CollectionListRequest < Request
+      # Specifies the default namespace to use when listing Collections
+      # @return [String]
       def self.default_namespace
         "tigerdata"
       end
 
+      # Specifies the Mediaflux service to use when listing Collections
+      # @return [String]
       def self.service
         "asset.collection.list"
       end
 
+      # Provides the arguments for the Mediaflux service used to list collections
+      # @return [Hash]
       def self.service_args
         {
           namespace: default_namespace
