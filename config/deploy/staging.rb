7cfa78f94150f5a2ac2d0ffceae22997354441a5
Hector Correa <hector_correa@princeton.edu>
2023-03-20 14:35:51 -0400

    Remove duplicate branch setting line (it's already in deploy.rb)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,5 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-set :branch, ENV["BRANCH"] || :main
-
 server "tigerdata-staging1", user: "deploy", roles: %w[app db web]
 server "tigerdata-staging2", user: "deploy", roles: %w[app db web]
