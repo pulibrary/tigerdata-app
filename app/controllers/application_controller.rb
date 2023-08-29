647b0e089f35cd45e9d65fc1b3428abcf93697cc
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-08-29 11:59:12 -0400

    Adding mediaflux session information into the session cookie (#159)
    This keeps the login around between pages requests
    
    Also removed the mediaflux client from the Welcome controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  around_action :mediaflux_session
 
   def new_session_path(_scope)
     new_user_session_path
   end
+
+  private
+
+    def mediaflux_session
+      current_user&.mediaflux_from_session(session)
+      yield
+    rescue Mediaflux::Http::SessionExpired
+      current_user.clear_mediaflux_session(session)
+      current_user.mediaflux_from_session(session)
+      retry
+    end
 end
