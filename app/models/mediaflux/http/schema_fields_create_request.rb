e9892ae4e3c3ad68ea843405e5f3871dda6948b7
Hector Correa <hector_correa@princeton.edu>
2023-09-07 11:21:01 -0400

    Creates TigerData schema and project definition (#175)
    * Got the request to create a metadata schema and a few fields to work via Nokogiri
    
    * Moved the Project schema logic inside the Project class
    
    * Switch to update to be more forgiving
    
    * Minor tweaks
    
    * Removed misleading comment

---
+++ app/models/mediaflux/http/schema_fields_create_request.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+module Mediaflux
+  module Http
+    class SchemaFieldsCreateRequest < Request
+      attr_reader :schema_name, :document, :description, :fields
+
+      # Mediaflux service to use to define fields in a schema namespace
+      # @return [String]
+      def self.service
+        "asset.doc.type.update"
+      end
+
+      def initialize(schema_name:, document:, description:, fields:, session_token:)
+        super(session_token: session_token)
+        @schema_name = schema_name
+        @document = document
+        @description = description
+        @fields = fields
+      end
+
+      private
+
+        def build_http_request_body(name:)
+          super(name: name) do |xml|
+            xml.args do
+              xml.create true
+              xml.description @description
+              xml.type "#{@schema_name}:#{@document}"
+              xml.definition do
+                @fields.each do |field|
+                  xml.element(field)
+                end
+              end
+            end
+          end
+        end
+    end
+  end
+end
