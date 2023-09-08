133e778374870146df0daa99dd4433960208b409
Hector Correa <hector_correa@princeton.edu>
2023-09-08 15:28:29 -0400

    Saving custom metadata in our Projects (#181)
    * First pass at saving in our project the fields defined in our custom tigerdata:project schema. Horrible sins were commited, but it works
    
    * Switched to use our schema instead of the test one with a single field
    
    * Use data instead of hard coded values in create_asset_request. Moved hard coded values to the Model
    
    * Removed test code in rake task

--- app/models/mediaflux/http/create_asset_request.rb
+++ app/models/mediaflux/http/create_asset_request.rb
@@ -10,12 +10,13 @@ module Mediaflux
       # @param collection [Boolean] create a collection asset if true
       # @param namespace [String] Optional Parent namespace for the asset to be created in
       # @param pid [Integer] Optional Parent id for the asset to be created in
-      def initialize(session_token:, namespace: nil, name:, collection: true, parent_id: nil)
+      def initialize(session_token:, namespace: nil, name:, collection: true, parent_id: nil, tigerdata_values: nil)
         super(session_token: session_token)
         @namespace = namespace
         @asset_name = name
         @collection = collection
         @parent_id = parent_id
+        @tigerdata_values = tigerdata_values
       end
 
       # Specifies the Mediaflux service to use when creating assets
@@ -31,12 +32,40 @@ module Mediaflux
 
       private
 
+        # The generated XXML mimics what we get when we issue an Aterm command as follows:
+        # > asset.set :id path=/sandbox_ns/rdss_collection
+        #     :meta <
+        #       :tigerdata:project <
+        #         :title "RDSS test project"
+        #         :description "The description of the project"
+        #         ...the rest of the fields go here..
+        #       >
+        #     >
+        #
+        # rubocop:disable Metrics/MethodLength
+        # rubocop:disable Metrics/AbcSize
         def build_http_request_body(name:)
           super do |xml|
             xml.args do
               xml.name asset_name
               xml.namespace namespace if namespace.present?
               xml.pid parent_id if parent_id.present?
+              if @tigerdata_values
+                xml.meta do
+                  xml.send("tigerdata:project", "xmlns:tigerdata" => "tigerdata") do
+                    xml.code @tigerdata_values[:code]
+                    xml.title @tigerdata_values[:title]
+                    xml.description @tigerdata_values[:description]
+                    xml.data_sponsor @tigerdata_values[:data_sponsor]
+                    xml.data_manager @tigerdata_values[:data_manager]
+                    @tigerdata_values[:departments].each do |department|
+                      xml.departments department
+                    end
+                    xml.created_on @tigerdata_values[:created_on]
+                    xml.created_by @tigerdata_values[:created_by]
+                  end
+                end
+              end
               if collection
                 xml.collection do
                   xml.parent.set_attribute("contained-asset-index", true)
@@ -48,6 +77,8 @@ module Mediaflux
             end
           end
         end
+      # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength
     end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Project
-  attr_accessor :id, :name, :path, :title, :organization, :file_count, :store_name
+  attr_accessor :id, :name, :path, :title, :organization, :file_count, :store_name,
+    :data_sponsor
 
   def initialize(id, name, path, title, organization, store = nil, session_id:)
     @id = id
@@ -25,17 +26,32 @@ class Project
     test_create_request.error?
   end
 
+  # rubocop:disable Metrics/MethodLength
   def self.create!(name, store_name, organization, session_id:)
     # Create a namespace for the project (within the namespace of the organization)...
     project_namespace = organization.path + "/" + safe_name(name) + "-ns"
     Mediaflux::Http::NamespaceCreateRequest.new(namespace: project_namespace, description: "Namespace for project #{name}", store: store_name, session_token: session_id).resolve
 
     # ...create a project as a collection asset inside this new namespace
-    create_request = Mediaflux::Http::CreateAssetRequest.new(session_token: session_id, namespace: project_namespace, name: safe_name(name))
+
+    # TODO: Switch to user entered values
+    values = {
+      code: "project-123",
+      title: "title of project 123",
+      description: "description of project 123",
+      data_sponsor: "hc123",
+      data_manager: "zz123",
+      departments: ["PUL", "PUL-RDSS"],
+      created_on: "now",
+      created_by: "hc8719"
+    }
+
+    create_request = Mediaflux::Http::CreateAssetRequest.new(session_token: session_id, namespace: project_namespace, name: safe_name(name), tigerdata_values: values)
     get_request = Mediaflux::Http::GetMetadataRequest.new(session_token: session_id, id: create_request.id)
     collection_asset = get_request.metadata
     Project.new(collection_asset[:id], collection_asset[:name], collection_asset[:path], collection_asset[:description], organization, store_name, session_id: session_id)
   end
+  # rubocop:enable Metrics/MethodLength
 
   def self.get(id, session_id:, organization: nil)
     # Fetch the collection asset for the project...
