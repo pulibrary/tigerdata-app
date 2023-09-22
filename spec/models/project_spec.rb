fe17c6c8ff9f38f0411a5aab033273a84775d51e
Hector Correa <hector_correa@princeton.edu>
2023-09-22 13:59:48 -0400

    Rubocop nitpicking

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,16 +4,16 @@ require "rails_helper"
 RSpec.describe Project, type: :model do
   describe "#sponsored_projects" do
     before do
-      Project.create(metadata: {data_sponsor: 'hc1234', title: 'project 111'})
-      Project.create(metadata: {data_sponsor: 'hc1234', title: 'project 222'})
-      Project.create(metadata: {data_sponsor: 'zz8888', title: 'project 333'})
+      Project.create(metadata: { data_sponsor: "hc1234", title: "project 111" })
+      Project.create(metadata: { data_sponsor: "hc1234", title: "project 222" })
+      Project.create(metadata: { data_sponsor: "zz8888", title: "project 333" })
     end
 
     it "returns projects for the sponsor" do
-      sponsored_projects = described_class.sponsored_projects('hc1234')
-      expect(sponsored_projects.find { |project| project.title == 'project 111' }).not_to be nil
-      expect(sponsored_projects.find { |project| project.title == 'project 222' }).not_to be nil
-      expect(sponsored_projects.find { |project| project.title == 'project 444' }).to be nil
+      sponsored_projects = described_class.sponsored_projects("hc1234")
+      expect(sponsored_projects.find { |project| project.title == "project 111" }).not_to be nil
+      expect(sponsored_projects.find { |project| project.title == "project 222" }).not_to be nil
+      expect(sponsored_projects.find { |project| project.title == "project 444" }).to be nil
     end
   end
 end
