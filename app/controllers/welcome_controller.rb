30f378423c790e3b4f118f89580fdfd3f6a0cf72
Carolyn Cole <cac9@princeton.edu>
2023-09-27 11:56:04 -0400

    Moving Mediaflux version information to it's own page Remove this information from the dashboard as it is not really part of it
    refs #173

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     return if current_user.nil?
-    version_request = Mediaflux::Http::VersionRequest.new(session_token: current_user.mediaflux_session)
-    @mf_version = version_request.version
     @projects = Project.sponsored_projects(@current_user.uid)
   end
 end
