41797d26b95a1cfbc7a643275d1f5d734b8b25e5
carolyncole <1599081+carolyncole@users.noreply.github.com>
2023-09-27 12:23:20 -0400

    Moving Mediaflux version information to it's own page (#191)
    Remove this information from the dashboard as it is not really part of it
    
    refs #173

---
+++ spec/views/mediaflux_infos/index.html.erb_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe "mediaflux_info/index", type: :view do
+  before(:each) do
+    assign(:mf_version, { version: "1001" })
+  end
+
+  it "renders a mediaflux information" do
+    render
+    assert_select "p", "Connected to MediaFlux 1001 at mediaflux.example.com"
+  end
+end
