0867ca5458ae7829df3dd2b8498c9e9c7e6d51ae
HyppoliteJ <jh6441@princeton.edu>
2023-11-21 10:54:25 -0500

    updated accessibilty specs

--- spec/system/accessibility_spec.rb
+++ spec/system/accessibility_spec.rb
@@ -11,4 +11,30 @@ describe "application accessibility", type: :system, js: true, stub_mediaflux: t
         .excluding(".tt-hint") # Issue is in typeahead.js library
     end
   end
+
+  context "when browsing the organizations page" do
+    it "complies with WCAG 2.0 AA and Section 508" do
+      visit "/organizations"
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
+    end
+  end
+
+  context "when browsing media_flux" do
+    let(:sponsor_user) { FactoryBot.create(:user, uid: "pul123") }
+    it "complies with WCAG 2.0 AA and Section 508" do
+      sign_in sponsor_user
+      visit "/mediaflux_info"
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
+    end
+  end
+
+  context "when browsing the help page" do
+    it "complies with WCAG 2.0 AA and Section 508" do
+      visit "/help"
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
+    end
+  end
 end
--- spec/system/project_spec.rb
+++ spec/system/project_spec.rb
@@ -53,6 +53,8 @@ RSpec.describe "Project Page", type: :system, stub_mediaflux: true do
       expect(page).to have_content "project 123"
       expect(page).to have_content "This project has not been saved to Mediaflux"
       expect(page).not_to have_button "Approve Project"
+      expect(page).to be_axe_clean
+        .according_to(:wcag2a, :wcag2aa, :wcag21a, :wcag21aa, :section508)
     end
 
     context "An Mediaflux Administrator" do
