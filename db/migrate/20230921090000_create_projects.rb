3d842fd256e10cf2542dce4ce313af65590e2774
HyppoliteJ <jh6441@princeton.edu>
2023-12-13 10:18:17 -0500

    removing the unused created-by-user-id field

--- db/migrate/20230921090000_create_projects.rb
+++ db/migrate/20230921090000_create_projects.rb
@@ -1,7 +1,6 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
     def change
       create_table :projects do |t|
-        t.integer :created_by_user_id
         t.integer :mediaflux_id
         t.jsonb :metadata_json
         t.timestamps
