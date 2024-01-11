d64e2666bea4a9b4b951f8e8245c610d31623216
Hector Correa <hector_correa@princeton.edu>
2024-01-11 16:39:47 -0500

    Remove the approve code

--- app/models/project.rb
+++ app/models/project.rb
@@ -50,14 +50,15 @@ class Project < ApplicationRecord
   end
 
   def approve!(session_id:, xml_namespace: nil)
-    asset_id = ProjectMediaflux.create!(project: self, session_id: session_id, xml_namespace: xml_namespace)
-    if asset_id.present?
-      Rails.logger.debug "Project #{id} has been saved to MediaFlux (asset id #{asset_id.to_i})"
-      self.mediaflux_id = asset_id.to_i
-      save!
-    else
-      raise "Error saving project to mediaflux"
-    end
+    # TODO: this method needs to be implemented to set the status
+    # asset_id = ProjectMediaflux.create!(project: self, session_id: session_id, xml_namespace: xml_namespace)
+    # if asset_id.present?
+    #   Rails.logger.debug "Project #{id} has been saved to MediaFlux (asset id #{asset_id.to_i})"
+    #   self.mediaflux_id = asset_id.to_i
+    #   save!
+    # else
+    #   raise "Error saving project to mediaflux"
+    # end
   end
 
   def update_mediaflux(session_id:)
