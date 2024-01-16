f7bb0d52d1905c1400f7fee5f1de40e32f5d10f5
Bess Sadler <bess.sadler@princeton.edu>
2024-01-16 14:22:59 -0500

    Pull pending status from the status field instead of hard coding it (#354)

--- spec/system/project_roles_spec.rb
+++ spec/system/project_roles_spec.rb
@@ -79,7 +79,7 @@ RSpec.describe "Project Edit Page Roles Validation", type: :system do
     click_on "Return to Dashboard"
     click_on("My test project")
     expect(page).to have_content("This project has not been saved to Mediaflux")
-    expect(page).to have_content("My test project (pending)")
+    expect(page).to have_content("My test project (#{::Project::PENDING_STATUS})")
     expect(page).to have_content(read_only.uid)
     expect(page).to have_content(read_write.uid)
   end
