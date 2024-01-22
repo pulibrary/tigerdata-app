a15761b94379da93c5ab2f5f6f6cf103d532e865
Carolyn Cole <cac9@princeton.edu>
2024-01-22 09:49:45 -0500

    Utilizing the display name for the display on the show page Removing non breaking space (U+00A0) from tests as they seem unnecissary

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe User, type: :model do
   let(:access_token) { OmniAuth::AuthHash.new(provider: "cas", uid: "who", extra: { mail: "who@princeton.edu", givenname: "guess", sn: "who", pudisplayname: "Guess Who?" }) }
   let(:access_token2) { OmniAuth::AuthHash.new(provider: "cas", uid: "who2", extra: { mail: "who2@princeton.edu" }) }
   let(:access_token3) { OmniAuth::AuthHash.new(provider: "cas", uid: "who3", extra: { mail: "who3@princeton.edu", givenname: "", sn: "", pudisplayname: "" }) }
+  let(:access_token4) { OmniAuth::AuthHash.new(provider: "cas", uid: "who4", extra: { mail: "who4@princeton.edu", givenname: "Guess", sn: "McWho", pudisplayname: "Guess McWho" }) }
+  let(:access_token5) { OmniAuth::AuthHash.new(provider: "cas", uid: "who5", extra: { mail: "who5@princeton.edu", givenname: "Guess", sn: "Who-You", pudisplayname: "Guess Who-You" }) }
+  let(:access_token6) { OmniAuth::AuthHash.new(provider: "cas", uid: "who6", extra: { mail: "who6@princeton.edu", givenname: "Guess", sn: "Y'Who", pudisplayname: "Guess Y'Who" }) }
   describe "#from_cas" do
     it "returns a user object" do
       user = described_class.from_cas(access_token)
@@ -22,7 +25,7 @@ RSpec.describe User, type: :model do
       expect(user.uid).to eq("who")
       expect(user.given_name).to eq("guess")
       expect(user.family_name).to eq("who")
-      expect(user.display_name_safe).to eq("Guess Who")
+      expect(user.display_name_safe).to eq("Guess Who?")
     end
     it "testing a uid" do
       user = described_class.from_cas(access_token2)
@@ -35,6 +38,24 @@ RSpec.describe User, type: :model do
       expect(user).to be_a described_class
       expect(user.uid).to eq("who3")
       expect(user.display_name_safe).to eq("who3")
+    end
+    it "testing a last name with distinct capitaliztion" do
+      user = described_class.from_cas(access_token4)
+      expect(user).to be_a described_class
+      expect(user.uid).to eq("who4")
+      expect(user.display_name_safe).to eq("Guess McWho")
+    end
+    it "testing a last name with hyphen" do
+      user = described_class.from_cas(access_token5)
+      expect(user).to be_a described_class
+      expect(user.uid).to eq("who5")
+      expect(user.display_name_safe).to eq("Guess Who-You")
+    end
+    it "testing a last name with quote" do
+      user = described_class.from_cas(access_token6)
+      expect(user).to be_a described_class
+      expect(user.uid).to eq("who6")
+      expect(user.display_name_safe).to eq("Guess Y'Who")
     end
   end
   context "loading Registration List" do
