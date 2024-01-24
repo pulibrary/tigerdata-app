57d4ca1178b66d357fcf0432b1fa90b7586fe91b
leefaisonr <rl3667@princeton.edu>
2024-01-24 15:59:16 -0500

    Refactor project show system specs into their own file
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>

--- spec/system/project_spec.rb
+++ spec/system/project_spec.rb
@@ -278,33 +278,4 @@ RSpec.describe "Project Page", type: :system, stub_mediaflux: true do
       expect(page).to have_content("(#{::Project::PENDING_STATUS})")
     end
   end
-
-  context "Show page" do
-    it "shows the project correct navigation buttons" do
-      sign_in sponsor_user
-      visit "/projects/#{project_not_in_mediaflux.id}"
-      expect(page).to have_content(project_not_in_mediaflux.title)
-      expect(page).to have_link("Edit")
-      click_on("Return to Dashboard")
-      expect(page).to have_content("Welcome, #{sponsor_user.given_name}!")
-      click_on(project_not_in_mediaflux.title)
-      expect(page).to have_link("Withdraw Project Request")
-    end
-    context "Provenance Events" do
-      let(:project) { FactoryBot.create(:project) }
-      let(:submission_event) { FactoryBot.create(:submission_event, project: project) }
-      it "shows provenance events" do
-        submission_event
-        sign_in sponsor_user
-        visit "/projects/#{project.id}"
-        expect(page).to have_content "#{submission_event.event_details}, #{submission_event.created_at.to_time.in_time_zone('America/New_York').iso8601}"
-      end
-      it "shows the project status under the provenance section" do
-        submission_event
-        sign_in sponsor_user
-        visit "/projects/#{project.id}"
-        expect(page).to have_content "Status\n#{::Project::PENDING_STATUS}"
-      end
-    end
-  end
 end
