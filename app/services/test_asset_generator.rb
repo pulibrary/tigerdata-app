ff7fbd8c2252716f419ccda445e5d8127b8542c4
Carolyn Cole <cac9@princeton.edu>
2024-01-26 11:52:58 -0500

    Adding in the levels, still need directories

--- app/services/test_asset_generator.rb
+++ app/services/test_asset_generator.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
 class TestAssetGenerator
+  attr_reader :levels, :base_name, :file_count_per_directory
   def initialize(user:, project_id:, levels: 5, directory_per_level: 100, file_count_per_directory: 1000)
     @user = user
     @project = Project.find(project_id)
     @levels = levels
     @directory_per_level = directory_per_level
     @file_count_per_directory = file_count_per_directory
+    @base_name = @project.directory
   end
 
   def generate
-    Mediaflux::Http::TestAssetCreateRequest.new(session_token: @user.mediaflux_session, parent_id: @project.mediaflux_id).resolve
+    generate_level(@project.mediaflux_id, levels)
   end
+
+  private
+    def generate_level(parent_id, level)
+      if level > 1
+        collection = Mediaflux::Http::CreateAssetRequest.new(session_token: @user.mediaflux_session, name: "#{base_name}-#{level}", pid: parent_id)
+        generate_level(collection.id, level-1)
+      end
+      Mediaflux::Http::TestAssetCreateRequest.new(session_token: @user.mediaflux_session, parent_id: parent_id, count: file_count_per_directory).resolve
+    end
 end
--- spec/services/test_asset_generator_spec.rb
+++ spec/services/test_asset_generator_spec.rb
@@ -15,10 +15,36 @@ RSpec.describe TestAssetGenerator do
     end
 
     it "creates test data" do
-      allow(Mediaflux::Http::TestAssetCreateRequest).to receive(:new).with(session_token: "mediaflux_sessionid", parent_id: 1234).and_return(test_asset_create)
+      allow(Mediaflux::Http::TestAssetCreateRequest).to receive(:new).with(session_token: "mediaflux_sessionid", parent_id: 1234, count: 1).and_return(test_asset_create)
       subject.generate
       expect(user).to have_received(:mediaflux_session)
       expect(test_asset_create).to have_received(:resolve)
+    end
+
+    context "multiple files" do
+      let(:subject) { described_class.new(user:, project_id: project.id, levels: 1, directory_per_level: 1, file_count_per_directory: 3) }
+
+      it "creates test data" do
+        allow(Mediaflux::Http::TestAssetCreateRequest).to receive(:new).with(session_token: "mediaflux_sessionid", parent_id: 1234, count: 3).and_return(test_asset_create)
+        subject.generate
+        expect(user).to have_received(:mediaflux_session)
+        expect(test_asset_create).to have_received(:resolve)
+      end
+    end
+
+    context "Multiple levels" do
+      let(:subject) { described_class.new(user:, project_id: project.id, levels: 2, directory_per_level: 1, file_count_per_directory: 1) }
+      let(:test_collection_create) { instance_double(Mediaflux::Http::CreateAssetRequest, id: "5678") }
+
+      it "creates collections and files" do
+        allow(Mediaflux::Http::CreateAssetRequest).to receive(:new).with(session_token: "mediaflux_sessionid", pid: 1234, name: anything).and_return(test_collection_create)
+        allow(Mediaflux::Http::TestAssetCreateRequest).to receive(:new).with(session_token: "mediaflux_sessionid", parent_id: "5678", count: 1).and_return(test_asset_create)
+        allow(Mediaflux::Http::TestAssetCreateRequest).to receive(:new).with(session_token: "mediaflux_sessionid", parent_id: 1234, count: 1).and_return(test_asset_create)
+        subject.generate
+        expect(user).to have_received(:mediaflux_session).exactly(3).times
+        expect(test_asset_create).to have_received(:resolve).twice
+        expect(test_collection_create).to have_received(:id)
+      end
     end
   end
 end
