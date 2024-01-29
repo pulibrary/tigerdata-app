a3225c4c0be41535013c954a323d368cdec9808f
HyppoliteJ <jh6441@princeton.edu>
2024-01-29 14:06:34 -0500

    added recurssion for the creation of multiple directories per level

--- app/services/test_asset_generator.rb
+++ app/services/test_asset_generator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class TestAssetGenerator
-  attr_reader :levels, :base_name, :file_count_per_directory, :mediaflux_session
+  attr_reader :levels, :base_name, :file_count_per_directory, :directory_per_level, :mediaflux_session
   def initialize(user:, project_id:, levels: 5, directory_per_level: 100, file_count_per_directory: 1000)
     @user = user
     @project = Project.find(project_id)
@@ -18,16 +18,18 @@ class TestAssetGenerator
   private
     def generate_level(parent_id, level)
       return if level == 0
-
       collection = Mediaflux::Http::CreateAssetRequest.new(session_token: mediaflux_session, name: "#{base_name}-#{level}", pid: parent_id)
       collection_id = collection.id  # resolves the request and extracts the id
-      generate_directory(collection_id)
-
+      byebug
+      generate_directory(collection_id, directory_per_level)
       generate_level(collection_id, level-1)
     end
 
-    def generate_directory(parent_id)
-      dir_collection = Mediaflux::Http::CreateAssetRequest.new(session_token: mediaflux_session, name: "#{base_name}-#{parent_id}", pid: parent_id)
+    def generate_directory(parent_id, directory_count)
+      return if directory_count == 0
+      name_extention = (0...10).map { ('a'..'z').to_a[rand(26)] }.join
+      dir_collection = Mediaflux::Http::CreateAssetRequest.new(session_token: mediaflux_session, name: "#{base_name}-#{parent_id}-#{name_extention}", pid: parent_id)
       Mediaflux::Http::TestAssetCreateRequest.new(session_token: mediaflux_session, parent_id: dir_collection.id, count: file_count_per_directory).resolve
+      generate_directory(parent_id, directory_count-1)
     end
 end
