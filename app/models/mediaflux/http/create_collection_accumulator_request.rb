c50361fb9a0ff19abfd0d579627d990ff1b13af5
carolyncole <1599081+carolyncole@users.noreply.github.com>
2024-02-02 11:55:38 -0500

    Adding an accumulator task so folks can create accumulators (#422)
    ```
    user = User.first
    collection = 1234
    accum = Mediaflux::Http::CreateCollectionAccumulatorRequest.new(session_token: user.mediaflux_session, collection:, name: test-asset-create-count2,  type:collection.asset.count)
    accum.resolve
    accum.response_xml
    ```

---
+++ app/models/mediaflux/http/create_collection_accumulator_request.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+module Mediaflux
+  module Http
+    class CreateCollectionAccumulatorRequest < Request
+      attr_reader :namespace, :asset_name, :collection
+
+      # Constructor
+      # @param session_token [String] the API token for the authenticated session
+      # @param collection [String] Id of the collection
+      # @param name [String] Name of the Accumulator
+      # @param type [String] type of the Accumulator
+      #     	"collection.asset.count"
+      #      	"content.all.copy.tag.size"
+      #     	"content.all.size"
+      #      	"content.all.store.size"
+      #      	"content.all.store.size.by.owner"
+      #      	"content.all.store.size.by.xpath"
+      #      	"content.all.type.size"
+      def initialize(session_token:, collection:, name:, type:)
+        super(session_token: session_token)
+        @name = name
+        @collection = collection
+        @type = type
+      end
+
+      # Specifies the Mediaflux service to use when creating assets
+      # @return [String]
+      def self.service
+        "asset.collection.accumulator.add"
+      end
+
+      private
+
+        # The generated XML mimics what we get when we issue an Aterm command as follows:
+        # > asset.collection.accumulator.add :id 3218 :cascade true
+        #      :accumulator < :name mytest-01509-count :type collection.asset.count >
+        #
+        def build_http_request_body(name:)
+          super do |xml|
+            xml.args do
+              xml.id @collection
+              xml.cascade true
+              xml.accumulator do
+                xml.name @name
+                xml.type @type
+              end
+            end
+          end
+        end
+    end
+  end
+end
