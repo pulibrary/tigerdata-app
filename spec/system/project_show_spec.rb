2d720d02dfa202c2447706f490499d4eb28944f2
HyppoliteJ <jh6441@princeton.edu>
2024-02-05 12:14:53 -0500

    added a spec test to create a project in mediaflux and fetch the accumulator

--- spec/system/project_show_spec.rb
+++ spec/system/project_show_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe "Project Page", type: :system, stub_mediaflux: true do
+RSpec.describe "Project Page", type: :system, stub_mediaflux: true, js: true do
   let(:sponsor_user) { FactoryBot.create(:user, uid: "pul123") }
   let(:data_manager) { FactoryBot.create(:user, uid: "pul987") }
   let(:read_only) { FactoryBot.create :user }
@@ -37,7 +37,7 @@ RSpec.describe "Project Page", type: :system, stub_mediaflux: true do
       expect(page).to have_link("Withdraw Project Request")
     end
     context "Provenance Events" do
-      let(:project) { FactoryBot.create(:project, metadata: metadata) }
+      let(:project) { FactoryBot.create(:project, project_id: "jh34", data_sponsor: sponsor_user.uid) }
       let(:submission_event) { FactoryBot.create(:submission_event, project: project) }
       it "shows provenance events" do
         submission_event
@@ -53,12 +53,24 @@ RSpec.describe "Project Page", type: :system, stub_mediaflux: true do
       end
       it "can take the user to the project contents page" do
         submission_event
+        session_id = sponsor_user.mediaflux_session
+        
+        # Create a project in mediaflux, attach an accumulator, and generate assests for the collection
+        project.mediaflux_id = ProjectMediaflux.create!(project:, session_id: )
+        project.save!
+        accum_req = Mediaflux::Http::CreateCollectionAccumulatorRequest.new(session_token: session_id, collection: project.mediaflux_id, name:"file count", type:"collection.asset.count")
+        accum_req.resolve
+        TestAssetGenerator.new(user: sponsor_user, project_id: project.id, levels: 2, directory_per_level: 2, file_count_per_directory: 1).generate
+        
+        # sign in and be able to view the file count for the collection
         sign_in sponsor_user
         visit "/projects/#{project.id}"
         expect(page).to have_selector(:link_or_button, "Review Contents")
         click_on("Review Contents")
         expect(page).to have_content("Project Contents")
         expect(page).to have_content("File Count")
+        expect(find(:css, "#file_count").text).to eq "4"
+        
         # Be able to return to the dashboard
         expect(page).to have_selector(:link_or_button, "Return to Dashboard")
         click_on("Return to Dashboard")
