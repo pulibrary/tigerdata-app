479e31bc46ae691a6f5ec5200e5b10dc895df65b
Bess Sadler <bess.sadler@princeton.edu>
2024-02-08 16:53:29 -0500

    Rubocop fixes

--- spec/models/mediaflux/http/create_collection_quota_request_spec.rb
+++ spec/models/mediaflux/http/create_collection_quota_request_spec.rb
@@ -12,17 +12,15 @@ RSpec.describe Mediaflux::Http::CreateCollectionQuotaRequest, type: :model do
   describe "#resolve" do
     before do
       stub_request(:post, mediaflux_url)
-      .with(
-        body: "<?xml version=\"1.0\"?>\n<request>\n  <service name=\"asset.collection.quota.set\" session=\"fake_session\">\n    <args>\n      <id>1304</id>\n      <quota>\n        <allocation>1 MB</allocation>\n        <description>1 MB quota for 1304</description>\n      </quota>\n    </args>\n  </service>\n</request>\n",
-        headers: {
-        'Accept'=>'*/*',
-        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-        'Connection'=>'keep-alive',
-        'Content-Type'=>'text/xml; charset=utf-8',
-        'Keep-Alive'=>'30',
-        'User-Agent'=>'Ruby'
-        }).
-      to_return(status: 200, body: "", headers: {})
+        .with(
+        body: "<?xml version=\"1.0\"?>\n<request>\n "\
+        " <service name=\"asset.collection.quota.set\" "\
+        "session=\"fake_session\">\n    <args>\n      <id>1304</id>\n "\
+        "     <quota>\n        <allocation>1 MB</allocation>\n "\
+        "       <description>1 MB quota for 1304</description>\n     "\
+        " </quota>\n    </args>\n  </service>\n</request>\n"
+      )
+        .to_return(status: 200, body: "", headers: {})
     end
 
     it "parses a response" do
