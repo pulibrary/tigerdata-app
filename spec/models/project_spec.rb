319c9424aac27e0de4c7f452f51f52bce5eb2ac4
HyppoliteJ <jh6441@princeton.edu>
2024-02-16 14:37:48 -0500

    merging main to get tests passing

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -146,7 +146,7 @@ RSpec.describe Project, type: :model, stub_mediaflux: true do
         .to_return(status: 200, body: "", headers: {})
     end
 
-    xit "fetches the file list" do
+    it "fetches the file list" do
       file_list = project.file_list(session_id: "test-session-token", size: 10)
       expect(file_list[:files].count).to eq 8
       expect(file_list[:files][0].name).to eq "file1.txt"
