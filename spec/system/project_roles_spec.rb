43d7010362f6f6504d33de7c2cd0b5315a231a83
leefaisonr <rl3667@princeton.edu>
2024-02-20 10:36:36 -0500

    ensure that user is on the show page once they submit an edit form

--- spec/system/project_roles_spec.rb
+++ spec/system/project_roles_spec.rb
@@ -179,6 +179,7 @@ RSpec.describe "Project Edit Page Roles Validation", type: :system do
       find(:css, "#btn-add-rw-user").click
       page.find("body").click
       click_on "Submit"
+      visit "/projects/#{projects.id}"
       expect(page).to have_content "#{ro_data_user.display_name} (read only)"
       expect(page).to have_content rw_data_user.display_name
     end
@@ -194,6 +195,7 @@ RSpec.describe "Project Edit Page Roles Validation", type: :system do
       find(:css, "#btn-add-rw-user").click
       page.find("body").click
       click_on "Submit"
+      visit "/projects/#{projects.id}"
       expect(page).to have_content "#{ro_data_user.display_name} (read only)"
       expect(page).to have_content rw_data_user.display_name
     end
