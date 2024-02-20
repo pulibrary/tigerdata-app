c5a570c6372dff239f6624761adff42eeac28825
leefaisonr <rl3667@princeton.edu>
2024-02-20 09:53:59 -0500

    Ensure project in system spec is approved so it can be edited
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>

--- spec/system/project_roles_spec.rb
+++ spec/system/project_roles_spec.rb
@@ -162,7 +162,7 @@ RSpec.describe "Project Edit Page Roles Validation", type: :system do
   end
 
   context "Data Sponsors and Data Managers can assign Data Users" do
-    let(:project) { FactoryBot.create(:project) }
+    let(:project) { FactoryBot.create(:project, status: Project::APPROVE_STATUS) }
     let(:sponsor_user) { User.find_by(uid: project.metadata_json["data_sponsor"]) }
     let(:data_manager) { User.find_by(uid: project.metadata_json["data_manager"]) }
     let!(:ro_data_user) { FactoryBot.create(:user) }
