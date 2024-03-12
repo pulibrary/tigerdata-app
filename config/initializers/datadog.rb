cc130f06ae101208fd87e9fb4404fa147a4487ed
Bess Sadler <bess.sadler@princeton.edu>
2024-03-12 17:07:59 -0400

    Turn off ddtrace in test

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -8,18 +8,18 @@ Datadog.configure do |c|
   c.env = Rails.env
   c.service = "tigerdata"
   c.version = "1.0.0"
-  c.profiling.enabled = true
+  c.profiling.enabled = Rails.env.staging? || Rails.env.production?
 
-  c.tracing.report_hostname = true
-  c.tracing.analytics.enabled = true
+  c.tracing.report_hostname = Rails.env.staging? || Rails.env.production?
+  c.tracing.analytics.enabled = Rails.env.staging? || Rails.env.production?
   c.tracing.enabled = Rails.env.staging? || Rails.env.production?
-  c.tracing.report_hostname = true
-  c.tracing.log_injection = true
+  c.tracing.report_hostname = Rails.env.staging? || Rails.env.production?
+  c.tracing.log_injection = Rails.env.staging? || Rails.env.production?
 
   # From https://docs.datadoghq.com/tracing/metrics/runtime_metrics/ruby/
   # To enable runtime metrics collection, set `true`. Defaults to `false`
   # You can also set DD_RUNTIME_METRICS_ENABLED=true to configure this.
-  c.runtime_metrics.enabled = true
+  c.runtime_metrics.enabled = Rails.env.staging? || Rails.env.production?
 
   # Optionally, you can configure the DogStatsD instance used for sending runtime metrics.
   # DogStatsD is automatically configured with default settings if `dogstatsd-ruby` is available.
