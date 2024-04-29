2a7dac8e145fcfeec696f48dfed4ace633be1eb6
Jaymee Hyppolite <54775395+JaymeeH@users.noreply.github.com>
2024-04-29 12:04:55 -0400

    Fixing validation and MetadataError (#663)

--- lib/tiger_data/metadata_error.rb
+++ lib/tiger_data/metadata_error.rb
@@ -7,7 +7,7 @@ module TigerData
     def self.missing_metadata(schema_version:, errors:)
       error_messages = errors.full_messages.join(", ")
       # include current expected metadata schema version number, as well as what metadata is missing
-      raise TigerData::MetadataError, "Project failed to create with metadata schema version #{schema_version} with the missing fields: #{error_messages}"
+      raise TigerData::MetadataError, "Project creation failed with metadata schema version #{schema_version} due to the missing fields: #{error_messages}"
     end
   end
 end
--- spec/models/project_mediaflux_spec.rb
+++ spec/models/project_mediaflux_spec.rb
@@ -98,7 +98,9 @@ RSpec.describe ProjectMediaflux, type: :model, stub_mediaflux: true do
           collection_id = ProjectMediaflux.create!(project: incomplete_project, session_id: session_token)
         }.to raise_error do |error|
           expect(error).to be_a(TigerData::MetadataError)
-          expect(error.message).to include("Project failed to create with metadata schema version 0.6 with the missing fields:")
+          expect(error.message).to include("Project creation failed with metadata schema version 0.6 due to the missing fields:")
+          expect(incomplete_project.metadata_model.errors.attribute_names.size).to eq 1
+          expect(incomplete_project.metadata_model.errors.attribute_names[0].to_s).to eq "project_id"
         end
       end
     end
