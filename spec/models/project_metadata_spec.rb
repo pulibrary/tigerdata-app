ff3a3bdd4909c800d9b72ba74eb9e22f6747a9ae
Carolyn Cole <cac9@princeton.edu>
2024-05-08 14:01:36 -0400

    Updating the project directory with the actual contents in mediaflux fixes #593

--- spec/models/project_metadata_spec.rb
+++ spec/models/project_metadata_spec.rb
@@ -199,25 +199,19 @@ RSpec.describe ProjectMetadata, type: :model do
                   }
         project_metadata.approve_project(params:)
         
-        #create a project in mediaflux
+        # create a project in mediaflux
         session_token = current_user.mediaflux_session
         collection_id = valid_project.save_in_mediaflux(session_id: session_token)
         
+        # change the project directory so it will not match up when activated
+        original_directory = valid_project.project_directory
+        valid_project.metadata_json["project_directory"] = "/abc/123/def"
+        valid_project.save!
+        valid_project.reload
+
         #validate that the collection id exists in mediaflux
         project_metadata.activate_project(collection_id:,current_user:)
-        response = Mediaflux::Http::AssetMetadataRequest.new(session_token: current_user.mediaflux_session, id: collection_id)
-        metadata = response.metadata
-        expect(metadata[:collection]).to be_truthy
-
-        #validate that the project doi in rails matches the project doi in mediaflux
-        xml = response.response_xml
-        asset = xml.xpath("/response/reply/result/asset")
-        doi = asset.xpath("//tigerdata:project/ProjectID", "tigerdata" => "tigerdata").text
-        expect(doi).to eq valid_project.metadata_json["project_id"]
-
-        #change the status of the project to active
-        valid_project.metadata_json["status"] = Project::ACTIVE_STATUS
-        valid_project.save!
+        expect(valid_project.project_directory).to eq(original_directory)
 
         expect(valid_project.metadata_json["status"]).to eq Project::ACTIVE_STATUS
 
