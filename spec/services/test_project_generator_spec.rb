f61281adb0edc14cbcae8c6c4d3f4fd9366bfbaf
carolyncole <1599081+carolyncole@users.noreply.github.com>
2024-05-08 08:58:42 -0400

    Remove created project after testing it (#690)
    This allows the test to be run more than one time

--- spec/services/test_project_generator_spec.rb
+++ spec/services/test_project_generator_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe TestProjectGenerator, connect_to_mediaflux: true do
       )      
       metadata_query.resolve
       expect(metadata_query.metadata[:path]).to eq "/td-test-001/tigerdata/test-project-00001"
+      Mediaflux::Http::AssetDestroyRequest.new(session_token: user.mediaflux_session, collection: project.mediaflux_id, members: true).resolve
     end
   end
 end
