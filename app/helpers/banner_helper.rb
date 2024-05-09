f5cbc471e1e19b4ea498611cdf4e5fa3a7d442ea
leefaisonr <rl3667@princeton.edu>
2024-05-09 13:34:32 -0400

    Update banner partial

--- app/helpers/banner_helper.rb
+++ app/helpers/banner_helper.rb
@@ -3,12 +3,10 @@
 require "yaml"
 
 module BannerHelper
-  # rubocop:disable Rails/ContentTag
   def banner_content
     @yaml_data = YAML.load_file("config/banner.yml")
     return false if @yaml_data[Rails.env].nil?
-    @banner_title = "<h1>#{@yaml_data[Rails.env]['title']}</h1>"
-    @banner_body = "<p>#{@yaml_data[Rails.env]['body']}</p>"
+    @banner_title = @yaml_data[Rails.env]['title']
+    @banner_body = @yaml_data[Rails.env]['body']
   end
-  # rubocop:enable Rails/ContentTag
 end
