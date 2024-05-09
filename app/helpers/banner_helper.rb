144df980e696e4f6fa034bddf879616b217714f5
leefaisonr <rl3667@princeton.edu>
2024-05-09 13:57:03 -0400

    Rubocop

--- app/helpers/banner_helper.rb
+++ app/helpers/banner_helper.rb
@@ -6,7 +6,7 @@ module BannerHelper
   def banner_content
     @yaml_data = YAML.load_file("config/banner.yml")
     return false if @yaml_data[Rails.env].nil?
-    @banner_title = @yaml_data[Rails.env]['title']
-    @banner_body = @yaml_data[Rails.env]['body']
+    @banner_title = @yaml_data[Rails.env]["title"]
+    @banner_body = @yaml_data[Rails.env]["body"]
   end
 end
