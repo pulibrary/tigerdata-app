7f9dcde0c43cb6da87743754b87450af35619c23
HyppoliteJ <jh6441@princeton.edu>
2024-05-21 14:59:46 -0400

    Creating a mediaflux class to settle timezones Converts all times from mediaflux to UTC based on the gmt offset before converting to an america/new_york timezone and iso format closes #568

---
+++ app/models/mediaflux_time.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+class MediafluxTime
+  def convert(xml_snip:)
+    # takes an xml snip of a time object from a mediaflux response and returns a Princeton time value
+    # xml_snip: (required) The xml path to a time object from mediaflux
+
+    xml = xml_snip
+    time = xml.text.to_time
+    gmt = xml.xpath("./@gmt-offset").text.to_f
+
+    if gmt.zero?
+      return time.in_time_zone("America/New_York").iso8601
+    elsif gmt.positive?
+      time -= gmt.hours
+    else
+      time += gmt.hours
+    end
+
+    princeton_time = time.in_time_zone("America/New_York").iso8601
+    princeton_time
+  end
+end
