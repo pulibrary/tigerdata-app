e7586f8553203717b0ba03a353172b3dc89c02a4
HyppoliteJ <jh6441@princeton.edu>
2024-06-06 17:18:12 -0400

    creating the tester-trainer emulation bar moving the emulation menu to the emulation bar spec test for new emulation banner updated user factory to add trainers

---
+++ app/helpers/emulator_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "yaml"
+
+module EmulatorHelper
+  def emulator_content
+    @yaml_data = YAML.load_file("config/emulator.yml")
+    return false if @yaml_data[Rails.env].nil? || @yaml_data[Rails.env] == "production"
+    @emulator_title = @yaml_data[Rails.env]["title"]
+    @emulator_body = @yaml_data[Rails.env]["body"]
+  end
+end
