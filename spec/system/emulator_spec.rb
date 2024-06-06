e0b2f8f62b189a38bb675ba86aa4845a3c8f0b90
HyppoliteJ <jh6441@princeton.edu>
2024-06-06 17:21:56 -0400

    rubocop 🚨

--- spec/system/emulator_spec.rb
+++ spec/system/emulator_spec.rb
@@ -3,10 +3,10 @@
 require "rails_helper"
 
 describe "Website banner", type: :system, js: true do
-    let(:current_user) { FactoryBot.create(:trainer, uid: "pul123") }
-    it "has the banner on the homepage" do
-        sign_in current_user
-        visit "/"
-        expect(page).to have_css "#emulator"
-    end
+  let(:current_user) { FactoryBot.create(:trainer, uid: "pul123") }
+  it "has the banner on the homepage" do
+    sign_in current_user
+    visit "/"
+    expect(page).to have_css "#emulator"
+  end
 end
