404617a1676829d5be8e94dd5115a024998177ca
HyppoliteJ <jh6441@princeton.edu>
2024-06-07 12:36:13 -0400

    restricting the emulation banner to the home page

--- app/helpers/emulator_helper.rb
+++ app/helpers/emulator_helper.rb
@@ -6,6 +6,7 @@ module EmulatorHelper
   def emulator_content
     @yaml_data = YAML.load_file("config/emulator.yml")
     return false if @yaml_data[Rails.env].nil? || @yaml_data[Rails.env] == "production"
+    return false unless current_page?("/")
     @emulator_title = @yaml_data[Rails.env]["title"]
     @emulator_body = @yaml_data[Rails.env]["body"]
   end
