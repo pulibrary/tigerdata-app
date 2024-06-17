38f31f39a1fb4f038edb371b1706a076587c53ef
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2024-06-17 14:08:24 -0400

    [wip] Starting to implement the RSpec test suite for UserRequest
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Carolyn Cole <carolyncole@users.noreply.github.com>
    Co-authored-by: Jaymee Hyppolite <jaymeeh@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

--- app/models/user_request.rb
+++ app/models/user_request.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
 class UserRequest < ApplicationRecord
+  belongs_to :user
+  belongs_to :project
 end
--- spec/models/user_request_spec.rb
+++ spec/models/user_request_spec.rb
@@ -1,5 +1,51 @@
-require 'rails_helper'
+# frozen_string_literal: true
+require "rails_helper"
 
-RSpec.describe UserRequest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe UserRequest, type: :model do
+  subject(:user_request) { described_class.new(**args) }
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project) }
+  # This uses the base class for all ActiveJobs within the application. Hence, any job should be supported.
+  let(:job) { instance_double(ApplicationJob, id: 1234) }
+
+  before do
+    user_request.save
+  end
+
+  describe "#user_id" do
+    let(:args) do
+      {
+        user: user
+      }
+    end
+
+    it "accesses and mutates the associations with user models" do
+      expect(user_request.user).to be(user)
+    end
+  end
+
+  describe "#project_id" do
+    let(:args) do
+      {
+        project: project
+      }
+    end
+
+    it "accesses and mutates the associations with project models" do
+      expect(user_request.project).to be(project)
+    end
+  end
+
+  describe "#job_id" do
+    let(:args) do
+      {
+        job_id: job.id
+      }
+    end
+
+    it "accesses and mutates the associations with ActiveJob instances" do
+      expect(user_request.job_id).to eq(job.id)
+    end
+  end
 end
