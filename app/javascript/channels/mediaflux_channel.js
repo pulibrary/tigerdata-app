6fcc56f3ae18786a038f88e85650a9954e6d5ec6
James R. Griffin III <1443986+jrgriffiniii@users.noreply.github.com>
2024-06-17 10:20:31 -0400

    Implementing an experimental attempt at integrating Mediaflux status (#770)
    indicator using ActionCable

--- app/javascript/channels/mediaflux_channel.js
+++ app/javascript/channels/mediaflux_channel.js
@@ -1,26 +1,12 @@
 import consumer from './consumer';
+import MediafluxComponent from '../components';
 
-class MediafluxStatusComponent {
-  constructor(element) {
-    this.element = element;
-  }
-
-  setOnline(value) {
-    this.online = value;
-    if (this.online) {
-      this.element.classList.add('active');
-      this.element.classList.remove('inactive');
-    } else {
-      this.element.classList.add('inactive');
-      this.element.classList.remove('active');
-    }
-  }
-}
+const MEDIAFLUX_DOM_SELECTOR = '.mediaflux-status';
 
 consumer.subscriptions.create({ channel: 'MediafluxChannel' }, {
   buildComponent() {
-    const element = document.querySelector('.mediaflux-status');
-    const component = new MediafluxStatusComponent(element);
+    const element = document.querySelector(MEDIAFLUX_DOM_SELECTOR);
+    const component = new MediafluxComponent(element);
     this.component = component;
   },
   connected() {
---
+++ app/javascript/tests/mediaflux.test.js
@@ -0,0 +1,33 @@
+// @vitest-environment jsdom
+
+import {
+  describe, it, expect,
+} from 'vitest';
+
+import { JSDOM } from 'jsdom';
+import Mediaflux from '../components/mediaflux';
+
+describe('Mediaflux', () => {
+  const dom = new JSDOM('<!DOCTYPE html><div class="mediaflux-status"></div></html>');
+  const { window } = dom;
+  const { document } = window;
+  const element = document.querySelector('.mediaflux-status');
+
+  it('binds to the DOM', () => {
+    const mediaflux = new Mediaflux(element);
+    expect(mediaflux.element).toEqual(element);
+  });
+  describe('#setOnline', () => {
+    it('updates the state of the instance', () => {
+      const mediaflux = new Mediaflux(element);
+      mediaflux.setOnline(true);
+
+      expect(element.classList.contains('active')).toEqual(true);
+      expect(element.classList.contains('inactive')).toEqual(false);
+      mediaflux.setOnline(false);
+
+      expect(element.classList.contains('active')).toEqual(false);
+      expect(element.classList.contains('inactive')).toEqual(true);
+    });
+  });
+});
