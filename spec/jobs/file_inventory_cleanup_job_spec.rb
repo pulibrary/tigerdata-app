6a3e285ed6684c8db04f3ed7e31d00253de3d799
HyppoliteJ <jh6441@princeton.edu>
2024-06-20 15:01:15 -0400

    rubocop

--- spec/jobs/file_inventory_cleanup_job_spec.rb
+++ spec/jobs/file_inventory_cleanup_job_spec.rb
@@ -1,21 +1,22 @@
-require 'rails_helper'
+# frozen_string_literal: true
+require "rails_helper"
 
 RSpec.describe FileInventoryCleanupJob, connect_to_mediaflux: true, type: :job do
   let(:user) { FactoryBot.create(:user) }
-  let(:project_in_mediaflux) { FactoryBot.create(:project_with_doi)}
-  let(:eight_days_ago) {Time.current.in_time_zone("America/New_York") - 8.days}
+  let(:project_in_mediaflux) { FactoryBot.create(:project_with_doi) }
+  let(:eight_days_ago) { Time.current.in_time_zone("America/New_York") - 8.days }
 
-  before do 
+  before do
     ProjectMediaflux.create!(session_id: user.mediaflux_session, project: project_in_mediaflux)
   end
 
-  describe "#perform_now" do 
-    it "deletes any files older than 7 days" do 
+  describe "#perform_now" do
+    it "deletes any files older than 7 days" do
       req = FileInventoryJob.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
       req.completion_time = eight_days_ago
       req.save
 
-      #TODO
+      # TODO
       #  run cleanup job, expect file to not exist
       expect(File.exist?(req.output_file)).to be_truthy
       described_class.perform_now
