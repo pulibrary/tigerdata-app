ab58a6268db3a25b0a47297e7bf699acec076fda
HyppoliteJ <jh6441@princeton.edu>
2024-06-20 14:24:22 -0400

    file inventory job sets the completion time of the request after running

--- app/jobs/file_inventory_job.rb
+++ app/jobs/file_inventory_job.rb
@@ -20,9 +20,7 @@ class FileInventoryJob < ApplicationJob
 
     # Make the FileInventoryRequest object
     # TODO: Figure out what request_details should have.
-    FileInventoryRequest.create(user_id: user.id, project_id: project.id, job_id: @job_id, state: UserRequest::PENDING, request_details: { output_file: filename })
-
-    mark_user_job_as_complete(project: project, user: user)
+    FileInventoryRequest.create(user_id: user.id, project_id: project.id, job_id: @job_id, state: UserRequest::PENDING, request_details: { output_file: filename, project_title: project.title }, completion_time: Time.current.in_time_zone("America/New_York"))
   end
 
   private
@@ -38,10 +36,4 @@ class FileInventoryJob < ApplicationJob
       pathname.join("#{job_id}.csv").to_s
     end
 
-    def mark_user_job_as_complete(project:, user:)
-      user_job = UserJob.create_and_link_to_user(job_id: job_id, user: user, job_title: "File list for #{project.title}")
-      user_job.completed_at = Time.current.in_time_zone("America/New_York").iso8601
-      user_job.save!
-      user_job.reload
-    end
 end
--- spec/jobs/file_inventory_job_spec.rb
+++ spec/jobs/file_inventory_job_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe FileInventoryJob, connect_to_mediaflux: true do
   let(:sponsor_user) { FactoryBot.create(:project_sponsor, uid: "pul123") }
   let(:project_in_mediaflux) { FactoryBot.create(:project_with_doi)}
 
+  before do 
+    ProjectMediaflux.create!(session_id: user.mediaflux_session, project: project_in_mediaflux)
+  end
+
   describe "#perform_now" do
     let(:file_inventory_job) { described_class.perform_now(user_id:, project_id:) }
     it "creates a file inventory request attached to the user and the project" do
@@ -15,30 +19,30 @@ RSpec.describe FileInventoryJob, connect_to_mediaflux: true do
       file_inventory_request = FileInventoryRequest.first 
       expect(file_inventory_request.state).to eq UserRequest::PENDING
     end
-    it "updates the UserJob#completed_at attribute" do
-      job = described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
-      user_job = UserJob.where(job_id: job.job_id).first
-      expect(user_job.completed_at).to_not be nil
-    end
 
     it "saves the output to a file" do
-      job = described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
-      output_file = Pathname.new(Rails.configuration.mediaflux["shared_files_location"]).join("#{job.job_id}.csv").to_s
+      file_inventory_request = described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
+      output_file = Pathname.new(Rails.configuration.mediaflux["shared_files_location"]).join("#{file_inventory_request.job_id}.csv").to_s
       expect(File.exist?(output_file)).to be true
     end
 
     it "puts the file path into the file inventory request" do
-      job = described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
-      output_file = Pathname.new(Rails.configuration.mediaflux["shared_files_location"]).join("#{job.job_id}.csv").to_s
+      file_inventory_request = described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
+      output_file = Pathname.new(Rails.configuration.mediaflux["shared_files_location"]).join("#{file_inventory_request.job_id}.csv").to_s
       file_inventory_request = FileInventoryRequest.first
       expect(file_inventory_request.output_file).to eq(output_file)
     end
 
     it "puts the title into the file inventory request" do
-      job = described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
-      output_file = Pathname.new(Rails.configuration.mediaflux["shared_files_location"]).join("#{job.job_id}.csv").to_s
+      described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
       file_inventory_request = FileInventoryRequest.first
       expect(file_inventory_request.request_details["project_title"]).to eq(project_in_mediaflux.title)
+    end
+
+    it "puts the completion time into the file inventory request" do 
+      described_class.perform_now(user_id: user.id, project_id: project_in_mediaflux.id)
+      file_inventory_request = FileInventoryRequest.first
+      expect(file_inventory_request.completion_time).to be_instance_of(ActiveSupport::TimeWithZone)
     end
 
     context "when an invalid User ID is specified" do
