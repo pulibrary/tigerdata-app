ec814c8cd52c84cbf5343659101e21f63b9e2d4f
Hector Correa <hector_correa@princeton.edu>
2024-06-27 14:57:19 -0400

    Made the AJAX URL variable (#798)
    * Make the URL variable
    
    * Lint nitpicking

--- app/javascript/entrypoints/mediafluxVersion.js
+++ app/javascript/entrypoints/mediafluxVersion.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export
-export function displayMediafluxVersion() {
+export function displayMediafluxVersion(url) {
   const placeholderInPage = $('#mediaflux_version').length === 1;
   if (placeholderInPage === true) {
     $.ajax({
       type: 'GET',
-      url: '/mediaflux_info.json',
+      url,
       success(data) {
         $('#mediaflux_version').text(`| Mediaflux: ${data.version}`);
       },
