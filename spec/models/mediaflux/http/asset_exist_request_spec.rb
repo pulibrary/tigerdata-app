fb1c4293f80c0a434a6fcb9585dcc4895aa4e7c4
Hector Correa <hector_correa@princeton.edu>
2024-07-01 15:41:24 -0400

    Added test for collection asset create root

--- spec/models/mediaflux/http/asset_exist_request_spec.rb
+++ spec/models/mediaflux/http/asset_exist_request_spec.rb
@@ -2,16 +2,16 @@
 require "rails_helper"
 
 RSpec.describe Mediaflux::Http::AssetExistRequest, type: :model, connect_to_mediaflux: true do
+  let(:session_token) { Mediaflux::Http::LogonRequest.new.session_token }
+  let(:namespace_root) { Rails.configuration.mediaflux["api_root_collection_namespace"] }
+  let(:name) { FFaker::InternetSE.company_name_single_word }
+
+  before do
+    create_req = Mediaflux::Http::CollectionAssetCreateRootRequest.new(session_token: session_token, namespace: namespace_root, name: name)
+    create_req.resolve
+  end
+
   describe "#exist" do
-    let(:session_token) { Mediaflux::Http::LogonRequest.new.session_token }
-    let(:namespace_root) { Rails.configuration.mediaflux["api_root_collection_namespace"] }
-    let(:name) { FFaker::InternetSE.company_name_single_word }
-
-    before do
-      create_req = Mediaflux::Http::CollectionAssetCreateRootRequest.new(session_token: session_token, namespace: namespace_root, name: name)
-      create_req.resolve
-    end
-
     it "validates that an asset exist or not" do
       valid_path = "/#{namespace_root}/#{name}"
       subject = described_class.new(session_token: session_token, path: valid_path)
