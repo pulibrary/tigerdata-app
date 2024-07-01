a636f76c8979b07694bffed0d43a869d3b757167
Hector Correa <hector_correa@princeton.edu>
2024-07-01 15:55:22 -0400

    Added test for root collection asset class

--- spec/models/mediaflux/http/collection_asset_create_root_request_spec.rb
+++ spec/models/mediaflux/http/collection_asset_create_root_request_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Mediaflux::Http::CollectionAssetCreateRootRequest, type: :model, 
   let(:name) { FFaker::InternetSE.company_name_single_word }
 
   it "creates a collection asset only if it does not exist already" do
-      subject = described_class.new(session_token: session_token, namespace: namespace_root, name: name)
-      expect(subject.id.to_i).not_to be 0
+    subject = described_class.new(session_token: session_token, namespace: namespace_root, name: name)
+    expect(subject.id.to_i).not_to be 0
 
-      subject = described_class.new(session_token: session_token, namespace: namespace_root, name: name)
-      expect(subject.id.to_i).to be 0
-      expect(subject.response_error[:message].include?("already contains")).to be true
+    subject = described_class.new(session_token: session_token, namespace: namespace_root, name: name)
+    expect(subject.id.to_i).to be 0
+    expect(subject.response_error[:message].include?("already contains")).to be true
   end
 end
