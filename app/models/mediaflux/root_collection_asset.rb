251dd2af176ad7dcc34c29ef00bd26e089eed9aa
Hector Correa <hector_correa@princeton.edu>
2024-07-02 08:13:19 -0400

    Adds class to create the root collection asset (#803)
    * WIP - creating a MF class to check if an asset exist and another to create the root namespace
    
    * Reshuffle the code a little bit. Better error checking
    
    * Added test to asset_exist_request
    
    * Added test for collection asset create root
    
    * Added test for root collection asset class
    
    * Removed my rake test tasks

---
+++ app/models/mediaflux/root_collection_asset.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+module Mediaflux
+  class RootCollectionAsset
+    attr_reader :error
+
+    def initialize(session_token:, namespace:, name:)
+      @session_token = session_token
+      @namespace = namespace || Rails.configuration.mediaflux["api_root_collection_namespace"]   # /td-prod-001
+      @name = name || Rails.configuration.mediaflux["api_root_collection_name"]                  # tigerdata
+      @path = Pathname.new(namespace).join(name).to_s
+      @error = nil
+    end
+
+    def create
+      check_root = Mediaflux::Http::AssetExistRequest.new(session_token: @session_token, path: @path)
+      return true if check_root.exist?
+
+      create_request = Mediaflux::Http::CollectionAssetCreateRootRequest.new(session_token: @session_token, namespace: @namespace, name: @name)
+      create_request.resolve
+      @error = create_request.response_error
+      create_request.id.present?
+    end
+  end
+end
