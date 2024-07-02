bdfc5eb3c8bbc2ec34391bd6f40b66139c071838
Hector Correa <hector_correa@princeton.edu>
2024-07-02 14:11:47 -0400

    Added test for namespace exist. Refactored the code to create the root collection and its associated tests

--- app/models/mediaflux/http/namespace_exist_request.rb
+++ app/models/mediaflux/http/namespace_exist_request.rb
@@ -4,9 +4,9 @@ module Mediaflux
     class NamespaceExistRequest < Request
       # Constructor
       # @param session_token [String] the API token for the authenticated session
-      def initialize(session_token:, path:)
+      def initialize(session_token:, namespace:)
         super(session_token: session_token)
-        @path = path
+        @namespace = namespace
       end
 
       # Specifies the Mediaflux service to use
@@ -15,7 +15,7 @@ module Mediaflux
         "asset.namespace.exists"
       end
 
-      # Be aware that asset.exists does not return the ID of the asset validated
+      # Be aware that asset.namespace.exists does not return the ID of the asset validated
       # so we cannot return it back to the user (Mediaflux returns the path that
       # we validated but not the actual asset ID. Hence we return true/false for
       # this kind of request.
@@ -28,7 +28,7 @@ module Mediaflux
         def build_http_request_body(name:)
           super do |xml|
             xml.args do
-              xml.id "path=#{@path}"
+              xml.namespace @namespace
             end
           end
         end
--- app/models/mediaflux/root_collection_asset.rb
+++ app/models/mediaflux/root_collection_asset.rb
@@ -5,12 +5,14 @@ module Mediaflux
 
     def initialize(session_token:, namespace:, name:)
       @session_token = session_token
-      @namespace = namespace || Rails.configuration.mediaflux["api_root_collection_namespace"]   # /td-prod-001
+      @namespace = namespace || Rails.configuration.mediaflux["api_root_collection_namespace"]   # /princeton or /td-demo-001
       @name = name || Rails.configuration.mediaflux["api_root_collection_name"]                  # tigerdata
       @path = Pathname.new(namespace).join(name).to_s
       @error = nil
     end
 
+    # Creates the root collection asset (e.g. `tigerdata`)
+    # Notice that this collection asset lives under a root namespace (e.g. `/princeton` in production or `td-demo-001` in development)
     def create
       check_root = Mediaflux::Http::AssetExistRequest.new(session_token: @session_token, path: @path)
       return true if check_root.exist?
@@ -28,17 +30,13 @@ module Mediaflux
     private
 
       def create_root_namespace
-        check_root_namespace = Mediaflux::Http::NamespaceExistRequest.new(session_token: @session_token, path: @namespace)
+        check_root_namespace = Mediaflux::Http::NamespaceExistRequest.new(session_token: @session_token, namespace: @namespace)
         return true if check_root_namespace.exist?
 
         create_namespace_request = Mediaflux::Http::NamespaceCreateRequest.new(session_token: @session_token, namespace: @namespace)
         create_namespace_request.resolve
         @error = create_namespace_request.response_error
-        if @error.present?
-          false
-        else
-          true
-        end
+        @error.blank?
       end
   end
 end
