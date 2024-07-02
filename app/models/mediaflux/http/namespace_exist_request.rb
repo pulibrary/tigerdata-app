0751ed563dc9dab993435e61ba131d01de78b19c
Hector Correa <hector_correa@princeton.edu>
2024-07-02 13:40:26 -0400

    Check for root namespace and creates it if it does not exist

---
+++ app/models/mediaflux/http/namespace_exist_request.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+module Mediaflux
+  module Http
+    class NamespaceExistRequest < Request
+      # Constructor
+      # @param session_token [String] the API token for the authenticated session
+      def initialize(session_token:, path:)
+        super(session_token: session_token)
+        @path = path
+      end
+
+      # Specifies the Mediaflux service to use
+      # @return [String]
+      def self.service
+        "asset.namespace.exists"
+      end
+
+      # Be aware that asset.exists does not return the ID of the asset validated
+      # so we cannot return it back to the user (Mediaflux returns the path that
+      # we validated but not the actual asset ID. Hence we return true/false for
+      # this kind of request.
+      def exist?
+        response_xml.xpath("/response/reply/result/exists").text == "true"
+      end
+
+      private
+
+        def build_http_request_body(name:)
+          super do |xml|
+            xml.args do
+              xml.id "path=#{@path}"
+            end
+          end
+        end
+    end
+  end
+end
--- app/models/mediaflux/root_collection_asset.rb
+++ app/models/mediaflux/root_collection_asset.rb
@@ -15,10 +15,30 @@ module Mediaflux
       check_root = Mediaflux::Http::AssetExistRequest.new(session_token: @session_token, path: @path)
       return true if check_root.exist?
 
-      create_request = Mediaflux::Http::CollectionAssetCreateRootRequest.new(session_token: @session_token, namespace: @namespace, name: @name)
-      create_request.resolve
-      @error = create_request.response_error
-      create_request.id.present?
+      if create_root_namespace
+        create_request = Mediaflux::Http::CollectionAssetCreateRootRequest.new(session_token: @session_token, namespace: @namespace, name: @name)
+        create_request.resolve
+        @error = create_request.response_error
+        create_request.id.present?
+      else
+        false
+      end
     end
+
+    private
+
+      def create_root_namespace
+        check_root_namespace = Mediaflux::Http::NamespaceExistRequest.new(session_token: @session_token, path: @namespace)
+        return true if check_root_namespace.exist?
+
+        create_namespace_request = Mediaflux::Http::NamespaceCreateRequest.new(session_token: @session_token, namespace: @namespace)
+        create_namespace_request.resolve
+        @error = create_namespace_request.response_error
+        if @error.present?
+          false
+        else
+          true
+        end
+      end
   end
 end
