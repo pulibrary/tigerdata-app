bb7e52b8b407716276eec5670ed0e1928128c6d0
James R. Griffin III <1443986+jrgriffiniii@users.noreply.github.com>
2024-07-05 12:55:46 -0400

    Supporting upgrades to Yarn release 4.3.1 (#809)
    * Supporting upgrades to Yarn release 4.3.1
    
    * Ensuring that authentication failures are handled for the mediafluxVersion JavaScript functionality

--- app/javascript/entrypoints/mediafluxVersion.js
+++ app/javascript/entrypoints/mediafluxVersion.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line import/prefer-default-export
-export function displayMediafluxVersion(url) {
+export function displayMediafluxVersion(url, authenticated) {
   const placeholderInPage = $('#mediaflux_version').length === 1;
-  if (placeholderInPage === true) {
+  if (placeholderInPage === true && authenticated) {
     $.ajax({
       type: 'GET',
       url,
       success(data) {
         $('#mediaflux_version').text(`| Mediaflux: ${data.version}`);
+      },
+      error(_jqXHR, _textStatus, errorThrown) {
+        console.error(errorThrown);
       },
     });
   }
