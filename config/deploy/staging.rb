1cc33ef77a10fb5435a490ab7087030430d7b673
carolyncole <1599081+carolyncole@users.noreply.github.com>
2024-07-09 13:46:31 -0400

    Revert "Staging is the only server we should be creating the schema on (#805)" (#818)
    This reverts commit c58041da5d15c032b0c220e96e612ff6eb4af108.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,5 +4,5 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "tigerdata-staging1.lib.princeton.edu", user: "deploy", roles: %w[app db web rake schema]
+server "tigerdata-staging1.lib.princeton.edu", user: "deploy", roles: %w[app db web rake]
 server "tigerdata-staging2.lib.princeton.edu", user: "deploy", roles: %w[app db web]
