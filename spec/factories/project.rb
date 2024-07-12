5b18ff57cf6ad8e211821e06996ce1a2e767c16e
Hector Correa <hector_correa@princeton.edu>
2024-07-12 13:32:44 -0400

    Fixed system/project_showe spec
    Co-authored-by: Jaymee Hyppolite <jaymeeh@users.noreply.github.com>

--- spec/factories/project.rb
+++ spec/factories/project.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  # TODO: we should either remove this so we don't write the metadata hash value
-  # directly and instead always use our new and shiny metadata_model.
-  # We will remove this once all other tests have been updated to use metadata_model.
   factory :project, class: "Project" do
     transient do
       data_sponsor { FactoryBot.create(:project_sponsor).uid }
       data_manager { FactoryBot.create(:data_manager).uid }
-      data_user_read_only { [] }
-      data_user_read_write { [] }
+      ro_users { [] }
+      rw_users { [] }
       title { FFaker::Movie.title }
       created_on { Time.current.in_time_zone("America/New_York").iso8601 }
       created_by { FactoryBot.create(:user).uid }
@@ -17,8 +14,8 @@ FactoryBot.define do
       updated_by { FactoryBot.create(:user).uid }
       project_id { "" }
       status { "pending" }
-      storage_capacity { { size: { requested: 500 }, unit: { requested: "GB" } } }
-      storage_performance { { requested: "standard" } }
+      storage_capacity { { size: { requested: 500 }, unit: { requested: "GB" } }.with_indifferent_access }
+      storage_performance { { requested: "standard" }.with_indifferent_access }
       project_purpose { "research" }
       project_directory { "big-data" }
       schema_version { ::TigerdataSchema::SCHEMA_VERSION }
@@ -31,13 +28,12 @@ FactoryBot.define do
         }
       end
     end
-    mediaflux_id { nil }
-    metadata do
-      {
+    metadata_model do
+      hash = {
         data_sponsor: data_sponsor,
         data_manager: data_manager,
-        data_user_read_only: data_user_read_only,
-        data_user_read_write: data_user_read_write,
+        ro_users: ro_users,
+        rw_users: rw_users,
         departments: ["RDSS", "PRDS"],
         project_directory: project_directory,
         title: title,
@@ -56,69 +52,16 @@ FactoryBot.define do
         approved_on: approved_on,
         submission: submission
       }
+      ProjectMetadata.new_from_hash(hash)
     end
-
-    factory :project_with_metadata, class: "Project" do
-      transient do
-        data_sponsor { FactoryBot.create(:project_sponsor).uid }
-        data_manager { FactoryBot.create(:data_manager).uid }
-        ro_users { [] }
-        rw_users { [] }
-        title { FFaker::Movie.title }
-        created_on { Time.current.in_time_zone("America/New_York").iso8601 }
-        created_by { FactoryBot.create(:user).uid }
-        updated_on { Time.current.in_time_zone("America/New_York").iso8601 }
-        updated_by { FactoryBot.create(:user).uid }
-        project_id { "" }
-        status { "pending" }
-        storage_capacity { { size: { requested: 500 }, unit: { requested: "GB" } } }
-        storage_performance { { requested: "standard" } }
-        project_purpose { "research" }
-        project_directory { "big-data" }
-        schema_version { ::TigerdataSchema::SCHEMA_VERSION }
-        approved_by { nil }
-        approved_on { nil }
-        submission do
-          {
-            requested_by: FactoryBot.create(:user).uid,
-            request_date_time: Time.current.in_time_zone("America/New_York").iso8601
-          }
-        end
-      end
-      metadata_model do
-        hash = {
-          data_sponsor: data_sponsor,
-          data_manager: data_manager,
-          ro_users: ro_users,
-          rw_users: rw_users,
-          departments: ["RDSS", "PRDS"],
-          project_directory: project_directory,
-          title: title,
-          description: "a random description",
-          created_on: created_on,
-          created_by: created_by,
-          updated_on: updated_on,
-          updated_by: updated_by,
-          project_id: project_id,
-          status: status,
-          storage_capacity: storage_capacity,
-          storage_performance_expectations: storage_performance,
-          project_purpose: project_purpose,
-          schema_version: schema_version,
-          approved_by: approved_by,
-          approved_on: approved_on,
-          submission: submission
-        }
-        ProjectMetadata.new_from_hash(hash)
-      end
-      mediaflux_id { nil }
-    end
+    mediaflux_id { nil }
 
     factory :project_with_doi, class: "Project" do
       sequence :project_id do |n|
         "doi:000000#{n}/00000000000#{n}"
       end
     end
+
     factory :project_with_dynamic_directory, class: "Project" do
       transient do
         sequence :project_directory do |n|
