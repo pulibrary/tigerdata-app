dc4c7a1271ea36b4aab8645d8eab7ead0034753a
Hector Correa <hector_correa@princeton.edu>
2024-07-15 15:31:33 -0400

    Moved the create project test from the project_metadata to the project

--- spec/models/project_metadata_spec.rb
+++ spec/models/project_metadata_spec.rb
@@ -116,83 +116,8 @@ RSpec.describe ProjectMetadata, type: :model do
     end
   end
 
-  # context "when no project is present" do
-  #   describe "#create" do
-  #     let(:datacite_stub) { instance_double(PULDatacite, draft_doi: "aaabbb123") }
-  #     before do
-  #       allow(PULDatacite).to receive(:new).and_return(datacite_stub)
-  #     end
-
-  #     it "does not call draft doi" do
-  #       project_metadata = described_class.new(current_user: current_user, project: Project.new)
-  #       update = project_metadata.create(params: {})
-  #       expect(datacite_stub).not_to have_received(:draft_doi)
-  #       expect(update).to be_blank
-  #     end
-  #   end
-  # end
-
   context "when a project is present" do
     let(:project) { FactoryBot.create :project }
-
-    describe "#create" do
-      let(:datacite_stub) { instance_double(PULDatacite, draft_doi: "aaabbb123") }
-      before do
-        allow(PULDatacite).to receive(:new).and_return(datacite_stub)
-      end
-
-      it "does not call out to draft a doi if the project is invalid" do
-        project.metadata_json["data_sponsor"] = ''
-        project_metadata = described_class.new(current_user: current_user, project:)
-        doi = project_metadata.create(params: {})
-        project_metadata.create(params: {}) # doesn't call the doi service twice
-        expect(datacite_stub).not_to have_received(:draft_doi)
-        expect(doi).to be_blank
-      end
-
-      it "calls out to draft a doi" do
-        sponsor =  FactoryBot.create(:user, uid: "abc")
-        data_manager =  FactoryBot.create(:user, uid: "def")
-
-        project_metadata = described_class.new(current_user: current_user, project:)
-        params = {data_sponsor: "abc", data_manager: "def", departments: "dep", project_directory: "dir", title: "title abc", description: "description 123" }
-        doi = project_metadata.create(params:)
-        project_metadata.create(params: {}) # doesn't call the doi service twice
-        expect(datacite_stub).to have_received(:draft_doi)
-        expect(doi).to eq("aaabbb123")
-        expect(project.reload.metadata[:project_id]).to eq("aaabbb123")
-      end
-
-      it "Creates a Provenance Event: Submission" do
-        sponsor =  FactoryBot.create(:user, uid: "abc")
-        data_manager =  FactoryBot.create(:user, uid: "def")
-        data_sponsor = User.find_by(uid: "abc")
-        project_metadata = described_class.new(current_user: current_user, project:)
-        params = {data_sponsor: "abc", data_manager: "def", departments: "dep", project_directory: "dir", title: "title abc", description: "description 123" }
-        doi = project_metadata.create(params:)
-        project_metadata.create(params: {}) # doesn't call the doi service twice
-
-        project.reload
-        expect(project.provenance_events.count).to eq 2
-        submission_event = project.provenance_events.first #testing the Submission Event
-        expect(submission_event.event_type).to eq ProvenanceEvent::SUBMISSION_EVENT_TYPE
-        expect(submission_event.event_person).to eq current_user.uid
-        expect(submission_event.event_details).to eq "Requested by #{data_sponsor.display_name_safe}"
-
-        status_update_event = project.provenance_events.last #testing the Status Update Event when the project is first submitted, #TODO: will need a separate test for when a project is approved by an admin
-        expect(status_update_event.event_type).to eq ProvenanceEvent::STATUS_UPDATE_EVENT_TYPE
-        expect(status_update_event.event_person).to eq current_user.uid
-        expect(status_update_event.event_details).to eq "The Status of this project has been set to pending"
-      end
-
-      it "does not call out to draft a doi if one isalready set" do
-        project.metadata_json["project_id"] = "aaabbb123"
-        project_metadata = described_class.new(current_user: current_user, project:)
-        doi = project_metadata.create(params: {})
-        expect(datacite_stub).not_to have_received(:draft_doi)
-        expect(doi).to eq("aaabbb123")
-      end
-    end
 
     describe "#approve_project" do
       it "Records the mediaflux id and sets the status to approved" do
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -239,4 +239,47 @@ RSpec.describe Project, type: :model, stub_mediaflux: true do
       expect(project).not_to be_pending
     end
   end
+
+  describe "#create" do
+    let(:datacite_stub) { instance_double(PULDatacite, draft_doi: "aaabbb123") }
+    let(:current_user) { FactoryBot.create(:user) }
+    let(:project) { FactoryBot.create(:project) }
+
+    before do
+      allow(PULDatacite).to receive(:new).and_return(datacite_stub)
+    end
+
+    context "DOI minting" do
+      it "attempts to mint a DOI when project_id is empty" do
+        initial_metadata = ProjectMetadata.new_from_hash({})
+        project.create!(initial_metadata: initial_metadata, user: current_user)
+        expect(datacite_stub).to have_received(:draft_doi)
+      end
+
+      it "does not mint a new DOI when the project has a project_id" do
+        project.metadata_model.update_with_params({"project_id" => "123"},  current_user)
+        doi = project.create!(initial_metadata: project.metadata_model, user: current_user)
+        expect(datacite_stub).not_to have_received(:draft_doi)
+        expect(doi).to eq "123"
+      end
+    end
+
+    context "Provenance events" do
+      it "creates the provenance events when creating a new project" do
+        project.metadata_model.update_with_params({"project_id" => "123"},  current_user)
+        project.create!(initial_metadata: project.metadata_model, user: current_user)
+
+        expect(project.provenance_events.count).to eq 2
+        submission_event = project.provenance_events.first #testing the Submission Event
+        expect(submission_event.event_type).to eq ProvenanceEvent::SUBMISSION_EVENT_TYPE
+        expect(submission_event.event_person).to eq current_user.uid
+        expect(submission_event.event_details).to eq "Requested by #{current_user.display_name_safe}"
+
+        status_update_event = project.provenance_events.last #testing the Status Update Event when the project is first submitted, #TODO: will need a separate test for when a project is approved by an admin
+        expect(status_update_event.event_type).to eq ProvenanceEvent::STATUS_UPDATE_EVENT_TYPE
+        expect(status_update_event.event_person).to eq current_user.uid
+        expect(status_update_event.event_details).to eq "The Status of this project has been set to pending"
+      end
+    end
+  end
 end
