d806b4dd2830308c6fba8f0e5d94b15e41a6a2bc
Hector Correa <hector_correa@princeton.edu>
2024-07-18 11:23:22 -0400

    Try to make the test less flaky (#843)

--- spec/models/mediaflux/collection_asset_create_root_request_spec.rb
+++ spec/models/mediaflux/collection_asset_create_root_request_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 RSpec.describe Mediaflux::CollectionAssetCreateRootRequest, type: :model, connect_to_mediaflux: true do
   let(:session_token) { Mediaflux::LogonRequest.new.session_token }
   let(:namespace_root) { Rails.configuration.mediaflux["api_root_collection_namespace"] }
-  let(:name) { FFaker::InternetSE.company_name_single_word }
+  let(:name) { FFaker::InternetSE.company_name_single_word + Random.rand(1000).to_s }
 
   it "creates a collection asset only if it does not exist already" do
     subject = described_class.new(session_token: session_token, namespace: namespace_root, name: name)
