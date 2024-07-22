78a62bf4110a322edcfca86e758d3848a451368a
HyppoliteJ <jh6441@princeton.edu>
2024-07-22 16:57:38 -0400

    refactoring namespace create request to connect to mediaflux

--- spec/models/mediaflux/namespace_create_request_spec.rb
+++ spec/models/mediaflux/namespace_create_request_spec.rb
@@ -1,28 +1,15 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe Mediaflux::NamespaceCreateRequest, type: :model do
-  let(:mediflux_url) { "http://mediaflux.example.com:8888/__mflux_svc__" }
-
-  let(:namespace_response) do
-    filename = Rails.root.join("spec", "fixtures", "files", "generic_response.xml")
-    File.new(filename).read
-  end
+RSpec.describe Mediaflux::NamespaceCreateRequest, connect_to_mediaflux: true ,type: :model do
+  let(:mediaflux_url) { "#{Rails.configuration.mediaflux["api_host"].to_s}/#{Rails.configuration.mediaflux["api_port"].to_s}" }
+  let(:user) { FactoryBot.create(:user)}
 
   describe "#resolve" do
-    # TODO: refactor the stub_mediaflux to connect to the real mediaflux
-    #     1 Test: 22
-    before do
-      stub_request(:post, "http://mediaflux.example.com:8888/__mflux_svc__")
-        .with(body: "<?xml version=\"1.0\"?>\n<request>\n  <service name=\"asset.namespace.create\" session=\"secretsecret/2/31\">\n    "\
-                               "<args>\n      <namespace all=\"true\">abc</namespace>\n    </args>\n  </service>\n</request>\n")
-        .to_return(status: 200, body: namespace_response, headers: {})
-    end
-
     it "disconnects the session" do
-      namespace_request = described_class.new(session_token: "secretsecret/2/31", namespace: "abc")
+      namespace_request = described_class.new(session_token: user.mediaflux_session, namespace: "td-test-001")
       namespace_request.resolve
-      expect(WebMock).to have_requested(:post, mediflux_url)
+      expect(namespace_request.resolved?).to eq true
     end
   end
 end
