53cf2ecf8b67e2b9b15c46cc5c5495462c50f3a9
HyppoliteJ <jh6441@princeton.edu>
2024-07-23 14:03:00 -0400

    refactoring range query request

--- spec/models/mediaflux/range_query_request_spec.rb
+++ spec/models/mediaflux/range_query_request_spec.rb
@@ -1,34 +1,30 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe Mediaflux::RangeQueryRequest, type: :model do
-  let(:mediflux_url) { "http://mediaflux.example.com:8888/__mflux_svc__" }
-
-  let(:query_response) do
-    filename = Rails.root.join("spec", "fixtures", "files", "asset_query_range_size_response.xml")
-    File.new(filename).read
-  end
+RSpec.describe Mediaflux::RangeQueryRequest, connect_to_mediaflux: true, type: :model do
+  let(:mediflux_url) { "http://0.0.0.0:8888/__mflux_svc__" }
+  let(:user) { FactoryBot.create(:user) }
+  let(:approved_project) { FactoryBot.create(:approved_project) }
+  let(:mediaflux_response) { "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<response><reply type=\"result\"><result></result></reply></response>" }
 
   before do
-    stub_request(:post, mediflux_url)
-      .with(body: "<?xml version=\"1.0\"?>\n<request>\n  <service name=\"asset.query\" session=\"secretsecret/2/31\">\n    "\
-                  "<args>\n      <collection>1234</collection>\n      <action>range</action>\n      <xpath>content/size</xpath>\n    </args>\n  </service>\n</request>\n")
-      .to_return(status: 200, body: query_response, headers: {})
+    # create a real collection for the test to create an accumulator for
+    approved_project.mediaflux_id = nil
+    @mediaflux_id = ProjectMediaflux.create!(project: approved_project, session_id: user.mediaflux_session)
+    Mediaflux::TestAssetCreateRequest.new(session_token: user.mediaflux_session, parent_id: @mediaflux_id, count: 70, pattern: "#{FFaker::Book.title}.txt").resolve
   end
 
   describe "#minimum" do
     it "returns the minimum value" do
-      query_request = described_class.new(session_token: "secretsecret/2/31", xpath: "content/size", collection: 1234)
+      query_request = described_class.new(session_token: user.mediaflux_session, xpath: "content/size", collection: @mediaflux_id)
       expect(query_request.minimum).to eq(100)
-      expect(WebMock).to have_requested(:post, mediflux_url)
     end
   end
 
   describe "#maximum" do
     it "returns the maximum value" do
-      query_request = described_class.new(session_token: "secretsecret/2/31", xpath: "content/size", collection: 1234)
-      expect(query_request.maximum).to eq(55_000)
-      expect(WebMock).to have_requested(:post, mediflux_url)
+      query_request = described_class.new(session_token: user.mediaflux_session, xpath: "content/size", collection: @mediaflux_id)
+      expect(query_request.maximum).to eq(100)
     end
   end
 end
