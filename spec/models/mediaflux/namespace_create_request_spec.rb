890518d4d756631feb6c5d5878837383ac1ede9b
HyppoliteJ <jh6441@princeton.edu>
2024-07-23 12:11:38 -0400

    ensuring that tests do not return errors in responses

--- spec/models/mediaflux/namespace_create_request_spec.rb
+++ spec/models/mediaflux/namespace_create_request_spec.rb
@@ -2,14 +2,29 @@
 require "rails_helper"
 
 RSpec.describe Mediaflux::NamespaceCreateRequest, connect_to_mediaflux: true, type: :model do
-  let(:mediaflux_url) { "#{Rails.configuration.mediaflux['api_host']}/#{Rails.configuration.mediaflux['api_port']}" }
+  # let(:mediaflux_url) { "http://0.0.0.0:8888/__mflux_svc__" }
   let(:user) { FactoryBot.create(:user) }
+  let(:mediaflux_response) { "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<response><reply type=\"result\"><result></result></reply></response>" }
 
   describe "#resolve" do
+    before do
+      # delete the namespace if it exists
+      destroy_request = Mediaflux::NamespaceDestroyRequest.new(session_token: user.mediaflux_session, namespace: "td-test-001")
+      destroy_request.destroy
+    end
+    after do
+      create_request = Mediaflux::NamespaceCreateRequest.new(
+      session_token: user.mediaflux_session,
+      namespace: Rails.configuration.mediaflux[:api_root_to_clean]
+    )
+      create_request.resolve
+    end
+
     it "disconnects the session" do
       namespace_request = described_class.new(session_token: user.mediaflux_session, namespace: "td-test-001")
       namespace_request.resolve
-      expect(namespace_request.resolved?).to eq true
+      expect(namespace_request.error?).to eq false
+      expect(namespace_request.response_body).to eq mediaflux_response
     end
   end
 end
--- spec/models/mediaflux/test_asset_create_request_spec.rb
+++ spec/models/mediaflux/test_asset_create_request_spec.rb
@@ -3,13 +3,22 @@ require "rails_helper"
 
 RSpec.describe Mediaflux::TestAssetCreateRequest, connecnt_to_mediaflux: true, type: :model do
   let(:user) { FactoryBot.create(:user) }
+  let(:approved_project) { FactoryBot.create(:approved_project) }
+  let(:mediaflux_response) { "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<response><reply type=\"result\"><result></result></reply></response>" }
   # let(:mediaflux_url) { "http://0.0.0.0:8888/__mflux_svc__" }
 
   describe "#resolve" do
+    before do
+      # create a collection for the test to create files under
+      approved_project.mediaflux_id = nil
+      @mediaflux_id = ProjectMediaflux.create!(project: approved_project, session_id: user.mediaflux_session)
+    end
+
     it "disconnects the session" do
-      namespace_request = described_class.new(session_token: user.mediaflux_session, parent_id: 123, count: 20, pattern: "abc")
+      namespace_request = described_class.new(session_token: user.mediaflux_session, parent_id: @mediaflux_id, count: 20, pattern: "abc")
       namespace_request.resolve
-      expect(namespace_request.resolved?).to eq true
+      expect(namespace_request.error?).to eq false
+      expect(namespace_request.response_body).to eq mediaflux_response
     end
   end
 end
