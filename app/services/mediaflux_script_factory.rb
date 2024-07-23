eb4c6641cf261238073c82256ec6695bec9dc723
Hector Correa <hector_correa@princeton.edu>
2024-07-23 17:09:14 -0400

    Refactored the create aterm script code

--- app/services/mediaflux_script_factory.rb
+++ app/services/mediaflux_script_factory.rb
@@ -1,91 +1,76 @@
 # frozen_string_literal: true
 
 class MediafluxScriptFactory
-  def project
-    @project ||= Project.find(@project_id)
+  def initialize(project:)
+    @project = project
+    @metadata = project.metadata_model
+
+    root_ns = Rails.configuration.mediaflux["api_root_collection_namespace"]
+    parent_collection = Rails.configuration.mediaflux["api_root_collection_name"]
+    @root_info = Mediaflux::RootCollectionAsset.new(session_token: nil, root_ns: root_ns, parent_collection: parent_collection)
   end
-  delegate :project_directory, to: :project
-  def project_parent
-    project.project_directory_parent_path
+
+  def aterm_script
+    prolog = "# Run these steps from Aterm to create a project in Mediaflux with its related components"
+    [prolog, script_asset_create].join("\r\n\r\n")
   end
-  alias path_id project_parent
-  delegate :metadata, to: :project
+
+  private
 
   def project_namespace
-    @project_namespace ||= "#{project_directory}NS"
+    Pathname.new(@root_info.parent_ns).join(@project.project_directory_short + "NS")
   end
 
-  def departments
-    metadata["departments"]
-  end
-
-  def department_fields
-    departments.map { |department| ":Department \"#{department}\"" }
-  end
-
-  def created_on_metadata
-    project.metadata_json["created_on"]
-  end
-
-  def created_on_time
-    Time.zone.parse(created_on_metadata)
-  end
-
-  def created_on_formatted
-    created_on_time.strftime("%e-%b-%Y %H:%M:%S")
+  def project_parent_path
+    Pathname.new(@root_info.path)
   end
 
   def created_on
-    created_on_formatted.upcase
+    Mediaflux::Time.format_date_for_mediaflux(@metadata.created_on)
   end
 
-  def requested_by
-    project.metadata_model.created_by
+  def department_fields
+    @metadata.departments.map { |department| ":Department \"#{department}\"" }
   end
 
-  def request_date_time
-    project.metadata_model.created_on
-  end
-
-  def requested_date
-    Mediaflux::Time.format_date_for_mediaflux(request_date_time)
-  end
-
-  # rubocop:disable Metrics/AbcSize
-  def build_create_script
+  def script_asset_create
+    # TODO: handle read-only and read-write user
     <<-ATERM
-      # Run these steps from Aterm to create a project in Mediaflux with its related components
 
       # Create the namespace for the project
       asset.namespace.create :namespace #{project_namespace}
 
       # Create the collection asset for the project
       asset.create
-        :pid path=#{project_parent}
+        :pid path=#{project_parent_path}
         :namespace #{project_namespace}
-        :name #{project_directory}
+        :name #{@project.project_directory_short}
         :collection -unique-name-index true -contained-asset-index true -cascade-contained-asset-index true true
         :type "application/arc-asset-collection"
         :meta <
           :tigerdata:project <
-            :ProjectDirectory "#{project_directory}"
-            :Title "#{project.metadata_json['title']}"
-            :Description "#{project.metadata_json['description']}"
-            :Status "#{project.metadata_json['status']}"
-            :DataSponsor "#{project.metadata_json['data_sponsor']}"
-            :DataManager "#{project.metadata_json['data_manager']}"
+            :ProjectDirectory "#{@metadata.project_directory}"
+            :Title "#{@metadata.title}"
+            :Description "#{@metadata.description}"
+            :Status "#{@metadata.status}"
+            :DataSponsor "#{@metadata.data_sponsor}"
+            :DataManager "#{@metadata.data_manager}"
             #{department_fields.join(' ')}
             :CreatedOn "#{created_on}"
-            :CreatedBy "#{project.metadata_json['created_by']}"
-            :ProjectID "#{project.metadata_json['project_id']}"
-            :StorageCapacity < :Size "#{project.metadata_json['storage_capacity']['size']['requested']}>" :Unit #{project.metadata_json['storage_capacity']['unit']['requested']}"
-            :StoragePerformance "#{project.metadata_json['storage_performance_expectations']['requested']}"
-            :ProjectPurpose "#{project.metadata_json['project_purpose']}"
-            :Submission < :RequestedBy "#{requested_by}" :RequestDateTime "#{requested_date}" >
-            :SchemaVersion "#{project.metadata['schema_version']}"
+            :CreatedBy "#{@metadata.created_by}"
+            :ProjectID "#{@metadata.project_id}"
+            :StorageCapacity < :Size #{@metadata.storage_capacity['size']['requested']} :Unit "#{@metadata.storage_capacity['unit']['requested']}" >
+            :Performance "#{@metadata.storage_performance_expectations['requested']}"
+            :ProjectPurpose "#{@metadata.project_purpose}"
+            :Submission < :RequestedBy "#{@metadata.created_by}" :RequestDateTime "#{created_on}" >
+            :SchemaVersion "#{@metadata.schema_version}"
           >
         >
+    ATERM
+  end
 
+  def script_accumulators
+    <<-ATERM
     # Define accumulator for file count
     asset.collection.accumulator.add
       :id path=#{path_id}
@@ -103,16 +88,15 @@ class MediafluxScriptFactory
       :name #{project_directory}-size
         :type content.all.size
       >
+    ATERM
+  end
 
+  def script_quotas
+    <<-ATERM
     # Define storage quota
     asset.collection.quota.set
       :id path=#{path_id}
       :quota < :allocation 500 GB :on-overflow fail :description "500 GB quota for #{project_directory}>"
     ATERM
-  end
-  # rubocop:enable Metrics/AbcSize
-
-  def initialize(project_id)
-    @project_id = project_id
   end
 end
--- lib/tasks/projects.rake
+++ lib/tasks/projects.rake
@@ -73,15 +73,17 @@ namespace :projects do
     puts "Mediaflux asset #{asset_id} updated"
   end
 
-  desc "Outputs to a file an aterm script to create a project (namespace and collection) in Mediaflux"
+  desc "Outputs to a file an aterm script to create a project in Mediaflux"
   task :create_script, [:project_id] => [:environment] do |_, args|
     project_id = args[:project_id]
-    service = MediafluxScriptFactory.new(project_id: project_id)
-    script = service.build_create_script
-
+    project = Project.find(project_id)
+    service = MediafluxScriptFactory.new(project: project)
     file_name = "project_create_#{project.id}.txt"
     puts "Saving script to #{file_name}"
-    File.write(file_name, script)
+    puts "=="
+    puts service.aterm_script
+    puts ""
+    # File.write(file_name, service.aterm_script)
   end
 
   task :download_file_list, [:netid, :project_id] => [:environment] do |_, args|
