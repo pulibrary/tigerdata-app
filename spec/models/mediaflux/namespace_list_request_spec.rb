1d017893a4c322fd10ecc692464d9d036cde2f15
HyppoliteJ <jh6441@princeton.edu>
2024-07-24 17:12:23 -0400

    rubocop

--- spec/models/mediaflux/namespace_list_request_spec.rb
+++ spec/models/mediaflux/namespace_list_request_spec.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe Mediaflux::NamespaceListRequest,connect_to_mediaflux: true, type: :model do
+RSpec.describe Mediaflux::NamespaceListRequest, connect_to_mediaflux: true, type: :model do
   let(:mediaflux_url) { "http://0.0.0.0:8888/__mflux_svc__" }
   let(:user) { FactoryBot.create(:user) }
   let(:mediaflux_response) { "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" }
 
   describe "#metadata" do
-
     it "parses a metadata response" do
       namespace_request = described_class.new(session_token: user.mediaflux_session, parent_namespace: "/td-test-001")
       namespaces = namespace_request.namespaces
