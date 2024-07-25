56c097ea1cead43f0f59a5d066fd9f1d17572fac
HyppoliteJ <jh6441@princeton.edu>
2024-07-25 15:04:26 -0400

    refactoring namespace describe request

--- spec/models/mediaflux/namespace_describe_request_spec.rb
+++ spec/models/mediaflux/namespace_describe_request_spec.rb
@@ -4,30 +4,31 @@ require "rails_helper"
 RSpec.describe Mediaflux::NamespaceDescribeRequest, connect_to_mediaflux: true, type: :model do
   let(:mediaflux_url) { "http://0.0.0.0:8888/__mflux_svc__" }
   let(:user) { FactoryBot.create(:user) }
-  let(:approved_project) { FactoryBot.create(:approved_project) }
   let(:mediaflux_response) { "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n" }
 
   describe "#metadata" do
     before do
-      approved_project.mediaflux_id = nil
-      mediaflux_id = ProjectMediaflux.create!(project: approved_project, session_id: user.mediaflux_session)
-      Mediaflux::TestAssetCreateRequest.new(session_token: user.mediaflux_session, parent_id: mediaflux_id).resolve
+      namespace_list_req = Mediaflux::NamespaceListRequest.new(session_token: user.mediaflux_session, parent_namespace: "/td-test-001")
+      namespace_response = namespace_list_req.response_body.split("<namespace id=")[1]
+      @namespace_id = namespace_response.split("leaf")[0].parameterize
     end
     it "parses a metadata response" do
-      namespace_request = described_class.new(session_token: user.mediaflux_session, id: 34732)
+      namespace_request = described_class.new(session_token: user.mediaflux_session, id: @namespace_id)
       metadata = namespace_request.metadata
-      expect(metadata[:id]).to eq("34732")
-      expect(metadata[:path]).to eq("/td-test-001")
-      expect(metadata[:name]).to eq("td-test-001")
+      expect(metadata[:id]).to eq(@namespace_id)
+      expect(metadata[:path]).to eq("/td-test-001/test")
+      expect(metadata[:name]).to eq("test")
+      expect(metadata[:description]).to eq("")
       expect(metadata[:store]).to eq("data")
     end
 
-    it "parses a metadata response with an namespace instead of an id" do
-      namespace_request = described_class.new(session_token: user.mediaflux_session, path: "/td-test-001")
+    it "parses a metadata response with an path instead of an id" do
+      namespace_request = described_class.new(session_token: user.mediaflux_session, path: "/td-test-001/test")
       metadata = namespace_request.metadata
-      expect(metadata[:id]).to eq("34732")
-      expect(metadata[:path]).to eq("/td-test-001")
-      expect(metadata[:name]).to eq("td-test-001")
+      expect(metadata[:id]).to eq(@namespace_id)
+      expect(metadata[:path]).to eq("/td-test-001/test")
+      expect(metadata[:name]).to eq("test")
+      expect(metadata[:description]).to eq("")
       expect(metadata[:store]).to eq("data")
     end
   end
