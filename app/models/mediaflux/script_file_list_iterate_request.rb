2b06ba4a9ed3281f2ee1c8f9c9f704a30589eed9
Hector Correa <hector_correa@princeton.edu>
2024-09-20 17:04:06 -0400

    Added TCL script and Ruby classes to call it

---
+++ app/models/mediaflux/script_file_list_iterate_request.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+module Mediaflux
+  class ScriptFileListIterateRequest < Request
+    # Constructor
+    # @param session_token [String] the API token for the authenticated session
+    # @param iterator [Int] the iterator returned by ScriptFileListInitRequest
+    def initialize(session_token:, iterator:, session_user: nil)
+      super(session_token: session_token, session_user: session_user)
+      @iterator = iterator
+    end
+
+    # Specifies the Mediaflux service to use
+    # @return [String]
+    def self.service
+      "asset.script.execute"
+    end
+
+    # Have we iterated over all the data?
+    def complete?
+      nested_response = response_xml.xpath("/response/reply/result").text
+      nested_xml = Nokogiri::XML.parse(nested_response)
+      nested_xml.xpath("/result/iterated/@complete").first.text == "true"
+    end
+
+    # Returns the file list
+    def result
+      nested_response = response_xml.xpath("/response/reply/result").text
+      nested_xml = Nokogiri::XML.parse(nested_response)
+      nested_xml.xpath("/result/name").map do |file|
+        {id: file["id"], name: file.text}
+      end
+    end
+
+    private
+
+      def build_http_request_body(name:)
+        super do |xml|
+          # asset.script.execute :id path="/system/scripts/fileList.tcl" :arg -name iterator 123
+          xml.args do
+            xml.id "path=/system/scripts/fileList.tcl"
+            xml.arg name: "iterator" do
+              xml.text(@iterator)
+            end
+          end
+        end
+      end
+  end
+end
---
+++ lib/assets/fileList.tcl
@@ -0,0 +1,52 @@
+# DESCRIPTION:
+#
+# ReturnS a list of files (assets) for a given path.
+#
+# This script uses iterators to handle cases where there are many files
+# in the given path. Therefore the first time you call it you give it a
+# `path` and the script will give you back an interator. Call the script
+# again with the `iterator` to start iterating over the list of files.
+#
+# Call it as many times until the return includes `iterated complete="true"`
+#
+#
+# RUNNING THE SCRIPT OUTSIDE OF MEDIAFLUX:
+#
+#   script.execute :in file:/Users/correah/src/mediaflux/fileList.tcl :arg -name path /path/to/collection
+#   script.execute :in file:/Users/correah/src/mediaflux/fileList.tcl :arg -name iterator $iterator
+#
+# INSTALLING THE SCRIPT IN MEDIAFLUX...
+#
+#   asset.namespace.create :namespace /system/scripts
+#   asset.create :namespace /system/scripts :name fileList.tcl :in file:/Users/correah/src/mediaflux/fileList.tcl
+#   asset.set.executable :id path=/system/scripts/fileList.tcl :executable true
+#
+# ...AND RUNNING IT FROM WITHIN MEDIAFLUX:
+#
+#   asset.script.execute :id path=/system/scripts/fileList.tcl :arg -name path /path/to/collection
+#   asset.script.execute :id path=/system/scripts/fileList.tcl :arg -name iterator $iterator
+#
+# TO REPLACE THE SCRIPT IN MEDIAFLUX YOU HAVE TO DESTROY IT AND RECREATE IT:
+#
+#   asset.destroy :id path=/system/scripts/fileList.tcl
+#
+
+if { [info exists "path"] } \
+{
+  # Initial query to get the iterator
+  server.log :app "filelist" :event info :msg "File list for ${path}"
+  asset.query :collection path=$path :action get-name :as iterator
+} \
+else \
+{
+  if { [info exists "iterator"] } \
+  {
+    # Iterate over the results
+    server.log :app "filelist" :event info :msg "Iterator for ${iterator}"
+    asset.query.iterate :id ${iterator}
+  } \
+  else {
+    server.log :app "filelist" :event error :msg "You must specify a path or an iterator"
+  }
+}
+
