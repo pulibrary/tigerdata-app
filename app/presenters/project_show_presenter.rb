2a3e63323736669d0bd2700964f63fb04f18c23a
Carolyn Cole <cac9@princeton.edu>
2024-09-25 08:21:32 -0400

    Move access to project metadata to the project presenter instead of diging deep in the views
    Moves all access to the metadata_model instead of some through the model and some through the hash

--- app/presenters/project_show_presenter.rb
+++ app/presenters/project_show_presenter.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 class ProjectShowPresenter
-  delegate "id", "in_mediaflux?", "mediaflux_id", "metadata", "metadata_model", "pending?", "project_directory", "status", "title", to: :project
+  delegate "id", "in_mediaflux?", "mediaflux_id", "pending?", "project_directory", "status", "title", to: :project
+  delegate "description", "project_id", "storage_capacity", "storage_performance_expectations", "project_purpose", to: :project_metadata
 
-  attr_reader :project
+  attr_reader :project, :project_metadata
 
   def initialize(project)
     @project = project
+    @project_metadata = @project.metadata_model
   end
 end
