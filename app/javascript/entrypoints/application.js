0162868cfc2fd39d6617e915618434ca87444564
Kate Lynch <katelynch@princeton.edu>
2024-11-05 09:27:58 -0500

    Fixed JS error

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -4,8 +4,8 @@ import '@popperjs/core';
 // import 'bootstrap/js/src/carousel'
 import 'bootstrap/js/src/collapse';
 import 'bootstrap/js/src/dropdown';
-import 'bootstrap/js/src/modal'
-import 'bootstrap/js/src/popover'
+import 'bootstrap/js/src/modal';
+import 'bootstrap/js/src/popover';
 import 'bootstrap/js/src/scrollspy';
 // import 'bootstrap/js/src/tab'
 // import 'bootstrap/js/src/toast'
