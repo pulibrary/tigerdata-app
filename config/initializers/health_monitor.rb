408ef7a1ac5d7097853b2249a22847061229ff58
Trey Pendragon <tpendragon@princeton.edu>
2024-11-14 08:59:24 -0800

    Still log.

--- config/initializers/health_monitor.rb
+++ config/initializers/health_monitor.rb
@@ -13,8 +13,8 @@ Rails.application.config.after_initialize do
     config.path = :health
 
     config.error_callback = proc do |e|
+      Rails.logger.error "Health check failed with: #{e.message}"
       unless e.is_a?(HealthMonitor::Providers::FileAbsenceException)
-        Rails.logger.error "Health check failed with: #{e.message}"
         Honeybadger.notify(e)
       end
     end
