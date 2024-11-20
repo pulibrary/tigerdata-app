2a09c4967ba134d1e75ac15463f7e812fc2367e8
Kate Lynch <katelynch@princeton.edu>
2024-11-20 14:31:58 -0500

    Added user roles information

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,19 @@ module ApplicationHelper
         project.metadata["storage_performance_expectations"]["approved"].nil? ? "Requested #{project.metadata["storage_performance_expectations"]["requested"]}" : project.metadata["storage_performance_expectations"]["approved"]
     end
 
+    def user_roles(user, project)
+        roles = []
+        roles << "Data Sponsor" if project["metadata_json"]["data_sponsor"] == user.uid
+        roles << "Data Manager" if project["metadata_json"]["data_manager"] == user.uid
+        roles << "Data User" if project["metadata_json"]["data_user_read_only"].include?(user.uid) || project["metadata_json"]["data_user_read_write"].include?(user.uid)
+        roles.join(", ")
+    end 
+
     def latest_file_list_time(project)
         @my_inventory_requests.where(project_id:project.id).empty? ? "Never requested" : "Prepared #{time_ago_in_words(@my_inventory_requests.where(project_id:project.id).sort_by(&:completion_time).reverse.first.completion_time)} ago"
     end 
 
     def last_activity_on_project(project)
         project.metadata["updated_on"].nil? ? "Not yet active" : "#{time_ago_in_words(project.metadata["updated_on"])} ago"
-    end 
+    end
 end
