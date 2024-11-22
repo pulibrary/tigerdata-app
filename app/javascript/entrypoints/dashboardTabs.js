9a6e35adf8bccd1afba0edc3f276e188afbb69d5
HyppoliteJ <jh6441@princeton.edu>
2024-11-22 10:35:33 -0500

    eslint

--- app/javascript/entrypoints/dashboardTabs.js
+++ app/javascript/entrypoints/dashboardTabs.js
@@ -163,13 +163,12 @@ export function dashStyle(railsSession) {
 }
 
 export function dashTab() {
-  const url = window.location.href;
   $('#dash-classic').on('click', (inv) => {
     const element = inv;
     element.preventDefault();
     $.ajax({
       type: 'POST',
-      url: `/dash_classic`,
+      url: '/dash_classic',
       data: { dashtab: 'classic' },
       success() { // on success..
         window.location.reload(); // update the DIV
@@ -182,7 +181,7 @@ export function dashTab() {
     element.preventDefault();
     $.ajax({
       type: 'POST',
-      url: `/dash_project`,
+      url: '/dash_project',
       data: { dashtab: 'project' },
       success() { // on success..
         window.location.reload(); // update the DIV
@@ -195,7 +194,7 @@ export function dashTab() {
     element.preventDefault();
     $.ajax({
       type: 'POST',
-      url: `/dash_activity`,
+      url: '/dash_activity',
       data: { dashtab: 'activity' },
       success() { // on success..
         window.location.reload(); // update the DIV
@@ -208,7 +207,7 @@ export function dashTab() {
     element.preventDefault();
     $.ajax({
       type: 'POST',
-      url: `/dash_admin`,
+      url: '/dash_admin',
       data: { dashtab: 'admin' },
       success() { // on success..
         window.location.reload(); // update the DIV
