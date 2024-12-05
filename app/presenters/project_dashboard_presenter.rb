d75921bc6c8e4e2c2bd2595e01fdb10eb3959cdb
Jaymee Hyppolite <54775395+JaymeeH@users.noreply.github.com>
2024-12-05 11:44:34 -0500

    Adding project quota to the datatable (#1077)
    * adding project quota to the datatable
    adding storage capacity and usage to the dashboard presenter
    passing the session id through the welcome controller
    
    * creating a single return value for quota usage through the presenter
    passing that value to the dashboard datatable
    returning default values for non-approved projects
    
    * re-naming variables for readability
    
    * updating quota usage text
    
    * first pass at creating css for quota progress bar
    
    * second pass at creating css for progress bar
    
    * adding progress bar to the datatable with a fixed height & width
    
    * placing progress bar inline with the quota and defaulting it to a fixed 5 percent for un-approved projects
    
    * refining table headings
    
    * passing dash session variables in the index
    
    * cleaning up unused code

--- app/presenters/project_dashboard_presenter.rb
+++ app/presenters/project_dashboard_presenter.rb
@@ -40,4 +40,13 @@ class ProjectDashboardPresenter < ProjectShowPresenter
       "Data User"
     end
   end
+
+  def quota_usage(session_id:)
+    quota_usage = "#{project.storage_usage(session_id:)} out of #{project.storage_capacity(session_id:)} used"
+    quota_usage
+  end
+
+  def quota_percentage(session_id:)
+    # figure out how to calculate percentage of storage used with different and unpredictable units
+  end
 end
