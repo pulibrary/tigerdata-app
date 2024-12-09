6c466c686ad720603e0e0bdc256d58d49a9f65f4
Kate Lynch <katelynch@princeton.edu>
2024-12-09 14:49:57 -0500

    Moving Javascript to entrypoints

---
+++ app/javascript/entrypoints/showMoreLess.js
@@ -0,0 +1,34 @@
+/* 
+
+Based on https://dev.to/justalever/how-to-code-a-show-more--show-less-toggle-with-javascript-3c7m 
+
+*/
+
+export const showMoreLess = {
+  
+    addClass: function(element, theClass) {
+      element.classList.add(theClass);
+    },
+    
+    removeClass: function(element, theClass) {
+      element.classList.remove(theClass);
+    },
+    
+    showMore: function(element, excerpt) {
+      element.addEventListener("click", event => {
+        const linkText = event.target.textContent.toLowerCase();
+        event.preventDefault();
+        
+        console.log(this);
+        if (linkText == "more...") {
+          element.textContent = "less...";
+          this.removeClass(excerpt, "truncate");
+          this.addClass(excerpt, "excerpt-visible");
+        } else {
+          element.textContent = "more...";
+          this.removeClass(excerpt, "excerpt-visible");
+          this.addClass(excerpt, "truncate");
+        }
+      });
+    } 
+  }
