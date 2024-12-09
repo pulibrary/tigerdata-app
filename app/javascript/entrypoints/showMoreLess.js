20df4a76498ae7d8b4eeb51d7f2d5d0b4c354718
Kate Lynch <katelynch@princeton.edu>
2024-12-09 16:36:28 -0500

    Addressed eslint errors
    Co-authored-by: Hector Correa <hectorcorrea@users.noreply.github.com>
    Co-authored-by: Jaymee Hyppolite <jaymeeh@users.noreply.github.com>

--- app/javascript/entrypoints/showMoreLess.js
+++ app/javascript/entrypoints/showMoreLess.js
@@ -1,34 +1,23 @@
-/* 
-
-Based on https://dev.to/justalever/how-to-code-a-show-more--show-less-toggle-with-javascript-3c7m 
-
-*/
-
+// eslint-disable-next-line import/prefer-default-export
 export const showMoreLess = {
+// eslint-disable-next-line object-shorthand, func-names
+  showMore: function (element, excerpt) {
+    const textElement = element;
 
-    addClass: function(element, theClass) {
-      element.classList.add(theClass);
-    },
+    textElement.addEventListener('click', (event) => {
+      const linkText = event.target.textContent.toLowerCase();
+      event.preventDefault();
 
-    removeClass: function(element, theClass) {
-      element.classList.remove(theClass);
-    },
-
-    showMore: function(element, excerpt) {
-      element.addEventListener("click", event => {
-        const linkText = event.target.textContent.toLowerCase();
-        event.preventDefault();
-
-        console.log(this);
-        if (linkText == "more...") {
-          element.textContent = "less...";
-          this.removeClass(excerpt, "truncate");
-          this.addClass(excerpt, "excerpt-visible");
-        } else {
-          element.textContent = "more...";
-          this.removeClass(excerpt, "excerpt-visible");
-          this.addClass(excerpt, "truncate");
-        }
-      });
-    } 
-  }
+      if (linkText === 'more...') {
+        textElement.textContent = 'less...';
+        excerpt.classList.remove('truncate');
+      } else {
+        textElement.textContent = 'more...';
+        excerpt.classList.add('truncate');
+      }
+    });
+  },
+};
+/*
+Based on https://dev.to/justalever/how-to-code-a-show-more--show-less-toggle-with-javascript-3c7m
+*/
