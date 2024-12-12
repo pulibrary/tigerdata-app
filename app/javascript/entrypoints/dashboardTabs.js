d6e6387632eaa356c323e5b6349fa259fc1f2f58
Hector Correa <hector_correa@princeton.edu>
2024-12-12 11:41:57 -0500

    Remove the Activity tab now that the sidebar displays the downloads

--- app/javascript/entrypoints/dashboardTabs.js
+++ app/javascript/entrypoints/dashboardTabs.js
@@ -1,6 +1,5 @@
 export function dashStyle(railsSession) {
   const project = document.getElementById('dash-projects');
-  const activity = document.getElementById('dash-activity');
   const admin = document.getElementById('dash-admin');
   const session = railsSession;
   const home = document.getElementById('welcome');
@@ -12,11 +11,6 @@ export function dashStyle(railsSession) {
         project.style.borderBottom = 'solid';
         project.style.borderColor = '#E77500';
         project.classList.add('active');
-        break;
-      case 'activity':
-        activity.style.borderBottom = 'solid';
-        activity.style.borderColor = '#E77500';
-        activity.classList.add('active');
         break;
       case 'admin':
         admin.style.borderBottom = 'solid';
@@ -60,37 +54,6 @@ export function dashStyle(railsSession) {
     project.style.borderBottom = 'solid';
     project.style.borderColor = '#E77500';
     project.classList.add('active');
-  });
-
-  $('#dash-activity').on('mouseenter', (el) => {
-    const element = el;
-    element.preventDefault();
-    // const tab = document.getElementById('tab-nav');
-
-    if (!activity.classList.contains('active')) {
-      activity.style.borderBottom = 'solid';
-      activity.style.borderColor = '#121212';
-    }
-  });
-
-  $('#dash-activity').on('mouseleave', (el) => {
-    const element = el;
-    element.preventDefault();
-    // const tab = document.getElementById('tab-nav');
-
-    if (!activity.classList.contains('active')) {
-      activity.style.border = 'none';
-    }
-  });
-
-  $('#dash-activity').on('click', (el) => {
-    const element = el;
-    element.preventDefault();
-    // const tab = document.getElementById('tab-nav');
-    // change background color to red
-    activity.style.borderBottom = 'solid';
-    activity.style.borderColor = '#E77500';
-    activity.classList.add('active');
   });
 
   $('#dash-admin').on('mouseenter', (el) => {
@@ -146,20 +109,6 @@ export function dashTab() {
       type: 'POST',
       url: '/dash_project',
       data: { dashtab: 'project' },
-      success() { // on success..
-        window.location.reload(); // update the DIV
-      },
-    });
-  });
-
-  $('#dash-activity').on('click', (inv) => {
-    const element = inv;
-    element.preventDefault();
-    $.ajax({
-      type: 'POST',
-
-      url: '/dash_activity',
-      data: { dashtab: 'activity' },
       success() { // on success..
         window.location.reload(); // update the DIV
       },
