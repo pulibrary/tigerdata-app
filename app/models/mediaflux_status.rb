b50125e0ab645b57599035b7b395ffe3374ff29c
carolyncole <1599081+carolyncole@users.noreply.github.com>
2024-12-18 14:39:10 -0500

    Cache the mediaflux good status for 5 minutes on the health.json page (#1150)

--- app/models/mediaflux_status.rb
+++ app/models/mediaflux_status.rb
@@ -4,15 +4,15 @@ class MediafluxStatus < HealthMonitor::Providers::Base
     # Notice that we check Mediaflux status using our TigerData account
     # (rather than the "logged in" user since there is not always a logged
     # in user for the health check)
-    domain = Rails.configuration.mediaflux["api_domain"]
-    user = Rails.configuration.mediaflux["api_user"]
-    password = Rails.configuration.mediaflux["api_password"]
-    logon_request = Mediaflux::LogonRequest.new(domain:, user:, password:)
-    session_token = logon_request.session_token
-    if logon_request.error?
-      raise logon_request.response_error[:message]
-    else
-      Mediaflux::LogoutRequest.new(session_token:)
+    Rails.cache.fetch("mediaflux_health_session", expires_in: 5.minutes) do
+      logon_request = Mediaflux::LogonRequest.new
+      session_token = logon_request.session_token
+      if logon_request.error?
+        raise logon_request.response_error[:message]
+      else
+        Mediaflux::LogoutRequest.new(session_token:)
+      end
+      session_token
     end
   end
 end
