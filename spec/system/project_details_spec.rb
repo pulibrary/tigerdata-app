bfc42915dc753d0e3499068835b0fb78b7aa20b5
HyppoliteJ <jh6441@princeton.edu>
2024-12-19 10:19:18 -0500

    updating tests

--- spec/system/project_details_spec.rb
+++ spec/system/project_details_spec.rb
@@ -99,7 +99,7 @@ RSpec.describe "Project Details Page", type: :system, connect_to_mediaflux: true
             expect(page).to have_content(pending_text)
             expect(page).to have_css ".pending"
             expect(page).to have_link("Edit")
-            expect(page).to have_selector(:link_or_button, "Approve Project")
+            expect(page).to have_selector(:link_or_button, "Approval Setting")
           end
         end
       end
@@ -238,7 +238,7 @@ RSpec.describe "Project Details Page", type: :system, connect_to_mediaflux: true
         expect(page).to have_content "1234"
         expect(page).not_to have_content "This project has not been saved to Mediaflux"
         expect(page).not_to have_content pending_text
-        expect(page).to have_selector(:link_or_button, "Approve Project")
+        expect(page).to have_selector(:link_or_button, "Approval Setting")
       end
 
       it "does not show the mediaflux id to the sponsor" do
@@ -257,7 +257,7 @@ RSpec.describe "Project Details Page", type: :system, connect_to_mediaflux: true
         visit "/projects/#{project_not_in_mediaflux.id}/details"
         expect(page).to have_content "This project has not been saved to Mediaflux"
         expect(page).to have_content pending_text
-        expect(page).to have_selector(:link_or_button, "Approve Project")
+        expect(page).to have_selector(:link_or_button, "Approval Setting")
       end
     end
   end
