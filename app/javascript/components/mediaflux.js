e3dbcce0e049f98a9cdd435c7ba8919c73c87fac
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-01-07 13:58:28 -0500

    Correcting the JS files

--- app/javascript/components/mediaflux.js
+++ app/javascript/components/mediaflux.js
@@ -1,18 +1,18 @@
 class MediafluxComponent {
   constructor(element) {
-    this.element = element;
+    this.element = element
   }
 
   setOnline(value) {
-    this.online = value;
+    this.online = value
     if (this.online) {
-      this.element.classList.add('active');
-      this.element.classList.remove('inactive');
+      this.element.classList.add("active")
+      this.element.classList.remove("inactive")
     } else {
-      this.element.classList.add('inactive');
-      this.element.classList.remove('active');
+      this.element.classList.add("inactive")
+      this.element.classList.remove("active")
     }
   }
 }
 
-export default MediafluxComponent;
+export default MediafluxComponent
--- app/javascript/entrypoints/pulDataTables.js
+++ app/javascript/entrypoints/pulDataTables.js
@@ -1,9 +1,9 @@
-import 'datatables';
+import "datatables"
 
 // Setup DataTables for the index tabs
 //   DataTables was included as a yarn package
 export function setupTable(tableId) {
-  const table = $(tableId);
+  const table = $(tableId)
   // Show sorting and pagination at this point
   // Likely will want searching soon
   const datasetOptions = {
@@ -12,21 +12,22 @@ export function setupTable(tableId) {
     searching: false,
     // use example spanish translation to know what to change in language https://cdn.datatables.net/plug-ins/2.1.8/i18n/es-ES.json
     language: {
-      info: '_START_ - _END_ out of _TOTAL_ shown',
-      paginate: { next: '>', previous: '<' },
+      info: "_START_ - _END_ out of _TOTAL_ shown",
+      paginate: { next: ">", previous: "<" },
     },
-    columnDefs: [{
-      targets: 0,
-      searchable: false,
-      orderable: false,
-    },
+    columnDefs: [
+      {
+        targets: 0,
+        searchable: false,
+        orderable: false,
+      },
     ],
-  };
+  }
 
   // If we have a table initialize it with DataTables
   if (table) {
-    table.dataTable(datasetOptions);
+    table.dataTable(datasetOptions)
   }
 }
 
-export default setupTable;
+export default setupTable
