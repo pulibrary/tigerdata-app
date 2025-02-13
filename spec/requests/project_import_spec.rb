dbbfe4fe27c7b4b88e4c31eae335d65d922d1465
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-02-13 09:38:59 -0500

    Add import projects button to the dashboard (#1309)
    Also fixes flash message so it displays nicely

--- spec/requests/project_import_spec.rb
+++ spec/requests/project_import_spec.rb
@@ -8,25 +8,43 @@ RSpec.describe "ProjectImports", type: :request do
       expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
     end
 
-    context "a signed in user" do
+    it "redirects to sign in" do
+      get project_import_path
+      expect(response).to redirect_to new_user_session_path
+      expect(flash[:alert]).to eq("You need to sign in or sign up before continuing.")
+    end
+
+      context "a signed in user" do
       let(:user) { FactoryBot.create :user }
       
       before do
         sign_in(user)
       end
 
-      it "renders a successful response" do
+      it "redirects to dashboard with access denied" do
         put project_import_path
         expect(response).to redirect_to(dashboard_path)
         expect(flash[:alert]).to eq("Access Denied")
       end
+
+      it "renders the dashboard with no message" do
+        get project_import_path
+        expect(response.body).to include("Welcome")
+        expect(flash[:alert]).to be_blank
+      end
     end
 
     context "a sysadmin user" do
-      let(:user) { FactoryBot.create :sysadmin, mediaflux_session: SystemUser.mediaflux_session}
+      let(:user) { FactoryBot.create :sysadmin, mediaflux_session: SystemUser.mediaflux_session, eligible_sponsor:true }
       
       before do
         sign_in(user)
+      end
+
+      it "renders a successful response" do
+        expect{ put project_import_path }.to change { Project.count }.by(0)
+        expect(response).to redirect_to(dashboard_path)
+        expect(flash[:notice]).to eq "Created 0 projects."
       end
 
       it "renders a successful response" do
@@ -36,8 +54,18 @@ RSpec.describe "ProjectImports", type: :request do
         new_project.destroy
 
         expect{ put project_import_path }.to change { Project.count }.by(1)
-        expect(response).not_to redirect_to(dashboard_path)
-        expect(flash[:alert]).to be_blank
+        expect(response).to redirect_to(dashboard_path)
+        expect(flash[:notice]).to eq "Created 1 project."
+      end
+
+      it "renders a successful response with any errors" do
+        new_project = FactoryBot.create(:approved_project, project_directory: "test-request")
+        new_project.mediaflux_id = nil
+        ProjectMediaflux.create!(project: new_project, user:)
+
+        expect{ put project_import_path }.to change { Project.count }.by(0)
+        expect(response).to redirect_to(dashboard_path)
+        expect(flash[:notice]).to eq "Created 0 projects. The following errors occurred: <ul><li>Skipping project 10.34770/tbd.  There are already 1 version of that project in the system</li><ul>"
       end
     end
   end
