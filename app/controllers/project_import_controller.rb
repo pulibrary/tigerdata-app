e2a8401fa4d7f9076161d17fbfd820420a3cb2ff
Carolyn Cole <cac9@princeton.edu>
2025-02-18 13:42:01 -0500

    Adding a display for report output
    Fixing code when an error happens with permissions to run the report

--- app/controllers/project_import_controller.rb
+++ app/controllers/project_import_controller.rb
@@ -2,15 +2,13 @@
 class ProjectImportController < ApplicationController
   def run
     if current_user.eligible_sysadmin?
-      output = ProjectImport.run_with_report(mediaflux_session: current_user.mediaflux_session)
-      created_count = output.count { |out| out.starts_with?("Created") }
-      error_output = output.reject { |out| out.starts_with?("Created") }
+      @results = ProjectImport.run_with_report(mediaflux_session: current_user.mediaflux_session).sort
+      created_count = @results.count { |out| out.starts_with?("Created") }
       notice = "Created #{created_count} #{'project'.pluralize(created_count)}."
-      notice += " The following errors occurred: <ul><li>#{error_output.join('</li><li>')}</li><ul>" if error_output.count > 0
       flash[:notice] = notice
     else
       flash[:alert] = I18n.t(:access_denied)
+      redirect_to dashboard_path
     end
-    redirect_to dashboard_path
   end
 end
