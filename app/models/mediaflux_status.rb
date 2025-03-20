bd27a43346321b19e317e4ef77c634a02a4173d6
leefaisonr <rl3667@princeton.edu>
2025-03-20 15:43:09 -0400

    Allow captured errors for mflux status to render custom error message for users
    Co-authored-by: Hector Correa <hectorcorrea@users.noreply.github.com>

--- app/models/mediaflux_status.rb
+++ app/models/mediaflux_status.rb
@@ -4,21 +4,18 @@ class MediafluxStatus < HealthMonitor::Providers::Base
     # Notice that we check Mediaflux status using our TigerData account
     # (rather than the "logged in" user since there is not always a logged
     # in user for the health check)
-    Rails.cache.fetch("mediaflux_health_session", expires_in: 5.minutes) do
-      byebug
-      begin 
-        logon_request = Mediaflux::LogonRequest.new
-        session_token = logon_request.session_token
-        if logon_request.error?
-          raise logon_request.response_error[:message]
-        else
-          Mediaflux::LogoutRequest.new(session_token:)
-        end
-        session_token
-      rescue StandardError => e
-        byebug
-        raise StandardError, e.message
+    Rails.cache.fetch("mediaflux_health_session", expires_in: 5.minutes) do 
+      logon_request = Mediaflux::LogonRequest.new
+      session_token = logon_request.session_token
+      if logon_request.error?
+        raise logon_request.response_error[:message]
+      else
+        Mediaflux::LogoutRequest.new(session_token:)
       end
+      session_token
+    rescue StandardError => e
+      Rails.logger.error("Mediaflux error #{e.message}")
+      raise StandardError, "Mediaflux error: Go to the server logs for details"
     end
   end
 end
