c0fb2f2e2f8e22dbf95dce83e0c35c9fd032df37
Hector Correa <hector_correa@princeton.edu>
2025-04-29 10:59:11 -0400

    Create a sample project

--- docs/schema_migrate.tcl
+++ docs/schema_migrate.tcl
@@ -1,4 +1,5 @@
-# Create a namespace and the "root" document type via a TCL script.
+# Create a namespace and the "root" document type via a TCL script
+# and a sample project ("collection asset") with some values.
 #
 # To run this script form aTerm:
 #
@@ -7,9 +8,14 @@
 # Define where our document type will be located (e.g. tigerdataX:resourceDoc)
 set namespace "tigerdataX"
 set rootDoc "resourceDoc"
-set schemaVersion "v0.9"
+set schemaVersion "v0.8"
 
-# Create the namespace
+# Define where our test document will be located (e.g. /td-demo-001/dev/tigerdataNS/april29-test)
+set assetTest "april29-test1047"
+set ns "NS"
+set assetTestNS $assetTest$ns
+
+# Create the namespace for the metadata definition
 asset.doc.namespace.update :create true :namespace $namespace
 
 # Define "resource" as defined in the TigerData schemaVersion
@@ -37,9 +43,32 @@ asset.doc.type.update :create true :description "testing doc definition via a TC
 #
 #   We can use `asset.doc.type.versions :type tigerdata:rootdoc` to get a list of the versions of a given type.
 #   We can use `asset.doc.type.describe :type tigerdata:rootdoc -version 1` to view the definition of a given version.
-
-
+#
 # Validating:
 # asset.meta.validate :id 1059
 #   :invalid -id "1059" -version "2" -nb "1" "XPath tigerdata:project is invalid: missing element 'new_req_element'"
 #    :invalid -id "1059" -version "2" -nb "2" "XPath tigerdata:project is invalid: missing element 'another_element'"
+
+
+# Create a namespace for our test project
+asset.namespace.create :namespace /td-demo-001/dev/tigerdataNS/$assetTestNS
+
+# Create the collection for the test project
+# with the values defined in github.com/pulibrary/tigerdata-app/issues/1401
+asset.create \
+    :pid path=/td-demo-001/dev/tigerdata \
+    :namespace /td-demo-001/dev/tigerdataNS/$assetTestNS \
+    :name $assetTest \
+    :collection -unique-name-index true -contained-asset-index true -cascade-contained-asset-index true true \
+    :type "application/arc-asset-collection" \
+    :meta < \
+    :tigerdataX:resourceDoc < \
+        :resource -resourceClass "Project" -resourceID "10.34770/az09-0011" -resourceIDType "DOI" < \
+          :title "Test Proof of Concept Project 1" \
+          :projectID -projectIDType "DOI" "10.34770/az09-0011" \
+          :projectProvenance < \
+            :schemaVersion "v0.8" \
+          > \
+        > \
+    > \
+  >
