744180d48529ae96803032066d44e0420472726b
Kate Lynch <katelynch@princeton.edu>
2025-05-06 17:00:37 -0400

    Added accessible role status visual indicators

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# Helper methods for users
+module UserHelper
+    def role_status_style(user_role)
+      user_role ? "td-true bi-check-circle-fill" : "td-false bi-exclamation-circle"
+    end
+  end
+  
