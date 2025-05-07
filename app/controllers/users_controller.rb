bd7e1ae91e9476bab1852a76eb6991cab593150f
Kate Lynch <katelynch@princeton.edu>
2025-05-07 13:50:29 -0400

    Rubocop fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
 class UsersController < ApplicationController
-    before_action :set_breadcrumbs
-  
-    # GET /requests
-    def index
-      if current_user.superuser || current_user.sysadmin || current_user.trainer
-        @users = User.order('family_name ASC NULLS LAST')
-        add_breadcrumb("Current Users")
-      else
-        flash[:notice] = "You do not have access to this page."
-        redirect_to dashboard_path
-      end
+  before_action :set_breadcrumbs
+  # GET /requests
+  def index
+    if current_user.superuser || current_user.sysadmin || current_user.trainer
+      @users = User.order("family_name ASC NULLS LAST")
+      add_breadcrumb("Current Users")
+    else
+      error_message = "You do not have access to this page."
+      flash[:notice] = error_message
+      redirect_to dashboard_path
     end
-  
-    private
-  
-      def set_breadcrumbs
-        add_breadcrumb("Dashboard", dashboard_path)
-      end
   end
-  
+
+  private
+
+    def set_breadcrumbs
+      add_breadcrumb("Dashboard", dashboard_path)
+    end
+end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 # Helper methods for users
 module UserHelper
-    def role_status_style(user_role)
-      user_role ? "td-true bi-check-circle-fill" : "td-false bi-exclamation-circle"
-    end
+  def role_status_style(user_role)
+    user_role ? "td-true bi-check-circle-fill" : "td-false bi-exclamation-circle"
   end
-  
+end
