9fd7bb2b2c6ac073753a7edd6494e3be224c6355
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-05-07 08:20:09 -0400

    Creating a pattern for use in generating request wizards (#1450)

---
+++ app/controllers/new_project_wizard/project_information_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+class NewProjectWizard::ProjectInformationController < RequestWizardsController
+  protected
+
+    def render_current
+      render "/new_project_wizard/project_information"
+    end
+
+    def render_next
+      redirect_to new_project_project_info_categories_path(request_model)
+    end
+
+    def render_back
+      redirect_to dashboard_path
+    end
+end
