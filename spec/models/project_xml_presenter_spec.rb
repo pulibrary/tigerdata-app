abcbe3c019e4ca58fa2ded955ee67e45a5f3b97a
Bess Sadler <bess.sadler@princeton.edu>
2025-05-08 08:36:04 -0400

    New ProjectXmlPresenter class

---
+++ spec/models/project_xml_presenter_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe ProjectXmlPresenter, type: :model, connect_to_mediaflux: false do
+  let(:project) { FactoryBot.create :project }
+  subject(:presenter) { described_class.new(project) }
+
+  it "can be instantiated" do
+    expect(presenter).to be_instance_of(described_class)
+  end
+end
