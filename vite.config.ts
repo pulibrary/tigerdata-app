d15986b2b1a6b511d546b224dfa0171ab5f9ee87
Carolyn Cole <cac9@princeton.edu>
2025-05-13 08:04:57 -0400

    Moving around the layout components to make adding another layout easier

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,27 @@ import { defineConfig } from 'vite';
 import ViteRails from 'vite-plugin-rails';
 import inject from '@rollup/plugin-inject';
 
-export default defineConfig({
-  plugins: [
-    inject({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
-    ViteRails(),
-  ],
-  include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-});
+export default ({ command, mode }) => {
+  let minifySetting;
+
+  if (mode === 'development') {
+    minifySetting = false;
+  } else {
+    minifySetting = 'esbuild';
+  }
+
+  return {
+    build: {
+      minify: minifySetting,
+      skipCompatibilityCheck: true,
+    },
+    plugins: [
+      inject({
+        $: 'jquery',
+        jQuery: 'jquery',
+      }),
+      ViteRails(),
+    ],
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+  };
+};
