bf82c0039b581f06c37bd52b7c4f0a5c4a834eaf
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-05-13 11:46:51 -0400

    Implementing support for attributes on the <resource> root node
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

--- spec/models/project_xml_presenter_spec.rb
+++ spec/models/project_xml_presenter_spec.rb
@@ -2,7 +2,7 @@
 require "rails_helper"
 
 RSpec.describe ProjectXmlPresenter, type: :model, connect_to_mediaflux: false do
-  let(:project) { FactoryBot.create :project }
+  let(:project) { FactoryBot.create :project_with_doi }
   subject(:presenter) { described_class.new(project) }
 
   it "can be instantiated" do
@@ -17,6 +17,14 @@ RSpec.describe ProjectXmlPresenter, type: :model, connect_to_mediaflux: false do
   describe "#to_xml" do
     it "generates a XML Document" do
       expect(presenter.to_xml).to be_a(Nokogiri::XML::Document)
+    end
+
+    it "ensures that the root node is a <resource> element with the required attributes" do
+      expect(presenter.to_xml.root).to be_a(Nokogiri::XML::Element)
+      expect(presenter.to_xml.root.name).to eq("resource")
+      expect(presenter.to_xml.root["resourceClass"]).to eq("Project")
+      expect(presenter.to_xml.root["resourceID"]).to eq(project.metadata_model.project_id)
+      expect(presenter.to_xml.root["resourceIDType"]).to eq("DOI")
     end
   end
 end
