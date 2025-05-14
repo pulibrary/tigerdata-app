c9dffb961cff25a9238f616c2a68f76ba2514692
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-05-14 16:30:38 -0400

    Refactoring by introducing the following: - Improving the speed for the test suites - Fixing an order-related bug with the recursive building of XML trees - Removing empty arguments from the config - Renaming `message` to `object_method` - Tracking the W3C XML schema in order to ensure that Nokogiri schema validation works properly - Enabling the XSD schema document validation test case - Ensuring that the config YAML file does not contain specify deployment environments - Extending the test suites in order to advance XSD schema validation

--- spec/models/project_xml_presenter_spec.rb
+++ spec/models/project_xml_presenter_spec.rb
@@ -2,39 +2,58 @@
 require "rails_helper"
 
 RSpec.describe ProjectXmlPresenter, type: :model, connect_to_mediaflux: false do
-  let(:project) { FactoryBot.create :project_with_doi }
   subject(:presenter) { described_class.new(project) }
+
+  let(:data_sponsor) { FactoryBot.create(:project_sponsor, uid: "abc123") }
+  let(:data_manager) { FactoryBot.create(:data_manager, uid: "bcd234") }
+  let(:read_only) { FactoryBot.create :user }
+  let(:read_write) { FactoryBot.create :user }
+  let(:department) { "77777" }
+
+  let(:metadata_model) do
+    hash = {
+      data_sponsor: data_sponsor.uid,
+      data_manager: data_manager.uid,
+      project_directory: "/tigerdata/abcd1/test-project-2",
+      title: "project 123",
+      departments: [department], # RDSS test code in fixture data
+      description: "hello world",
+      data_user_read_only: [read_only.uid],
+      data_user_read_write: [read_write.uid],
+      status: ::Project::PENDING_STATUS,
+      created_on: Time.current.in_time_zone("America/New_York").iso8601,
+      created_by: FactoryBot.create(:user).uid,
+      project_id: "10.34770/az09-0004"
+    }
+    ProjectMetadata.new_from_hash(hash)
+  end
+
+  let(:project) { FactoryBot.create(:project_with_doi, data_sponsor: data_sponsor.uid, data_manager: data_manager.uid, metadata_model: metadata_model) }
 
   let(:schema_file_path) { Rails.root.join("lib", "assets", "tigerdata_metadata", "v0.8", "TigerData_StandardMetadataSchema_v0.8.xsd") }
   let(:schema_file) { File.read(schema_file_path) }
   let(:schema) { Nokogiri::XML::Schema(schema_file) }
 
-  it "can be instantiated" do
-    expect(presenter).to be_instance_of(described_class)
-  end
-
-  context "rails XML payload" do
-    it "has an xml payload" do
-    end
-  end
-
   describe "#document" do
+    let(:document) { presenter.document }
+    let(:root) { document.root }
+
     it "generates a XML Document" do
-      expect(presenter.document).to be_a(Nokogiri::XML::Document)
+      expect(document).to be_a(Nokogiri::XML::Document)
     end
 
     it "ensures that the root node is a <resource> element with the required attributes" do
-      expect(presenter.document.root).to be_a(Nokogiri::XML::Element)
-      expect(presenter.document.root.name).to eq("resource")
-      expect(presenter.document.root["resourceClass"]).to eq("Project")
-      expect(presenter.document.root["resourceID"]).to eq(project.metadata_model.project_id)
-      expect(presenter.document.root["resourceIDType"]).to eq("DOI")
+      expect(root).to be_a(Nokogiri::XML::Element)
+      expect(root.name).to eq("resource")
+      expect(root["resourceClass"]).to eq("Project")
+      expect(root["resourceID"]).to eq(project.metadata_model.project_id)
+      expect(root["resourceIDType"]).to eq("DOI")
     end
 
     describe "<projectID>" do
-      let(:node) { presenter.document.root.at_xpath("projectID") }
+      let(:node) { root.at_xpath("projectID") }
 
-      it "builds a <projectID> element containing the title of the project" do
+      it "builds a <projectID> element containing the ID of the project" do
         expect(node).to be_a(Nokogiri::XML::Element)
         expect(node.name).to eq("projectID")
         expect(node["inherited"]).to eq("false")
@@ -44,19 +63,83 @@ RSpec.describe ProjectXmlPresenter, type: :model, connect_to_mediaflux: false do
       end
     end
 
-    describe "<departments>" do
-      let(:node) { presenter.document.root.at_xpath("departments") }
+    describe "<dataSponsor>" do
+      let(:node) { root.at_xpath("dataSponsor") }
 
-      it "builds a <departments> element containing the title of the project" do
+      it "builds a <dataSponsor> element containing the data sponsor of the project" do
         expect(node).to be_a(Nokogiri::XML::Element)
-        expect(node.name).to eq("departments")
+        expect(node.name).to eq("dataSponsor")
+        expect(node["userID"]).to eq(project.metadata_model.data_sponsor)
         expect(node["discoverable"]).to eq("true")
+        expect(node["inherited"]).to eq("true")
         expect(node["trackingLevel"]).to eq("ResourceRecord")
       end
     end
 
+    describe "<dataManager>" do
+      let(:node) { root.at_xpath("dataManager") }
+
+      it "builds a <dataManager> element containing the data manager of the project" do
+        expect(node).to be_a(Nokogiri::XML::Element)
+        expect(node.name).to eq("dataManager")
+        expect(node["userID"]).to eq(project.metadata_model.data_manager)
+        expect(node["discoverable"]).to eq("true")
+        expect(node["inherited"]).to eq("true")
+        expect(node["trackingLevel"]).to eq("ResourceRecord")
+      end
+    end
+
+    describe "<departments>" do
+      let(:node) { root.at_xpath("departments") }
+
+      it "builds a <departments> element containing the departments of the project" do
+        expect(node).to be_a(Nokogiri::XML::Element)
+        expect(node.name).to eq("departments")
+        expect(node["discoverable"]).to eq("true")
+        expect(node["trackingLevel"]).to eq("ResourceRecord")
+        expect(node.children.length).to eq(presenter.departments.length)
+      end
+
+      describe "<department>" do
+        let(:node) { root.at_xpath("departments/department") }
+
+        it "builds a <department> element for each department associated with the project" do
+          expect(node).to be_a(Nokogiri::XML::Element)
+          expect(node.name).to eq("department")
+          expect(node["inherited"]).to eq("true")
+          expect(node["departmentCode"]).to eq(presenter.departments.first)
+          expect(node.content).to eq("0#{department}")
+        end
+      end
+    end
+
+    describe "<projectDirectory>" do
+      let(:node) { root.at_xpath("projectDirectory") }
+
+      it "builds a <projectDirectory> element containing the project directory structure information of the project" do
+        expect(node).to be_a(Nokogiri::XML::Element)
+        expect(node.name).to eq("projectDirectory")
+        expect(node["approved"]).to eq("false")
+        expect(node["inherited"]).to eq("false")
+        expect(node["discoverable"]).to eq("false")
+        expect(node["trackingLevel"]).to eq("InternalUseOnly")
+        expect(node.children.length).to eq(presenter.project_directory.length)
+      end
+
+      describe "<projectDirectoryPath>" do
+        let(:node) { root.at_xpath("projectDirectory/projectDirectoryPath") }
+
+        it "builds a <projectDirectoryPath> element containing project directory information for a file-system node" do
+          expect(node).to be_a(Nokogiri::XML::Element)
+          expect(node.name).to eq("projectDirectoryPath")
+          expect(node["protocol"]).to eq("NFS")
+          expect(node.content).to eq(presenter.project_directory.first)
+        end
+      end
+    end
+
     describe "<title>" do
-      let(:node) { presenter.document.root.at_xpath("title") }
+      let(:node) { root.at_xpath("title") }
 
       it "builds a <title> element containing the title of the project" do
         expect(node).to be_a(Nokogiri::XML::Element)
@@ -69,9 +152,9 @@ RSpec.describe ProjectXmlPresenter, type: :model, connect_to_mediaflux: false do
     end
 
     describe "<description>" do
-      let(:node) { presenter.document.root.at_xpath("description") }
+      let(:node) { root.at_xpath("description") }
 
-      it "builds a <description> element containing the title of the project" do
+      it "builds a <description> element containing the description of the project" do
         expect(node).to be_a(Nokogiri::XML::Element)
         expect(node.name).to eq("description")
         expect(node["inherited"]).to eq("false")
@@ -82,44 +165,60 @@ RSpec.describe ProjectXmlPresenter, type: :model, connect_to_mediaflux: false do
     end
 
     describe "<projectPurpose>" do
-      let(:node) { presenter.document.root.at_xpath("projectPurpose") }
+      let(:node) { root.at_xpath("projectPurpose") }
 
-      it "builds a <projectPurpose> element containing the title of the project" do
+      it "builds a <projectPurpose> element containing the stated purpose for the project" do
         expect(node).to be_a(Nokogiri::XML::Element)
         expect(node.name).to eq("projectPurpose")
-        expect(node["inherited"]).to eq("false")
+        expect(node["inherited"]).to eq("true")
         expect(node["discoverable"]).to eq("true")
         expect(node["trackingLevel"]).to eq("InternalUseOnly")
         expect(node.content).to eq(project.metadata_model.project_purpose)
       end
     end
 
-    describe "<storagePerformance>" do
-      let(:node) { presenter.document.root.at_xpath("storagePerformance") }
+    describe "<storageCapacity>" do
+      let(:node) { root.at_xpath("storageCapacity") }
 
-      it "builds a <storagePerformance> element containing the title of the project" do
+      it "builds a <storageCapacity> element containing the storage capacity information for the project" do
+        expect(node).to be_a(Nokogiri::XML::Element)
+        expect(node.name).to eq("storageCapacity")
+        expect(node["approved"]).to eq("false")
+        expect(node["inherited"]).to eq("false")
+        expect(node["discoverable"]).to eq("false")
+        expect(node["trackingLevel"]).to eq("InternalUseOnly")
+      end
+    end
+
+    describe "<storagePerformance>" do
+      let(:node) { root.at_xpath("storagePerformance") }
+
+      it "builds a <storagePerformance> element containing the storage performance information for the project" do
         expect(node).to be_a(Nokogiri::XML::Element)
         expect(node.name).to eq("storagePerformance")
         expect(node["approved"]).to eq("false")
         expect(node["inherited"]).to eq("false")
         expect(node["discoverable"]).to eq("false")
         expect(node["trackingLevel"]).to eq("InternalUseOnly")
-        expect(node.children.length).to eq(1)
       end
 
       describe "<requestedValue>" do
-        let(:node) { presenter.document.root.at_xpath("storagePerformance/requestedValue") }
+        let(:node) { root.at_xpath("storagePerformance/requestedValue") }
 
-        it "builds a <requestedValue> element containing the title of the project" do
+        it "builds a <requestedValue> element containing the storage request information for the project" do
           expect(node).to be_a(Nokogiri::XML::Element)
           expect(node.name).to eq("requestedValue")
-          expect(node.content).to eq(project.metadata_model.storage_performance_expectations[:requested])
+          expect(node.content).to eq(presenter.requested_storage)
         end
       end
     end
 
-    xit "validates against the XSD schema document" do
-      expect(schema.valid?(presenter.document)).to be true
+    describe "XSD schema validation" do
+      let(:validation_errors) { schema.validate(document) }
+
+      xit "validates against the XSD schema document" do
+        expect(validation_errors).to be_empty
+      end
     end
   end
 end
