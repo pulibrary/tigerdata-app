20bd264c2734b5284c76165c5a9666df86f3a988
Kate Lynch <katelynch@princeton.edu>
2025-05-15 15:37:39 -0400

    Added simple approve button

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,6 +6,19 @@ class RequestsController < ApplicationController
   def index
     return head :forbidden unless Flipflop.new_project_request_wizard?
     add_breadcrumb("New Project Request")
+    @requests = Request.all
+  end
+
+  def show
+    @request = Request.find(params[:id])
+    add_breadcrumb("Requests", requests_path)
+    add_breadcrumb(@request.project_title, request_path)
+    render :show
+  end
+
+  def approve
+    stub_message = "Edit the 'approve' method in app/controllers/requests_controller.rb to do something when this button is clicked"
+    redirect_to dashboard_path, notice: stub_message
   end
 
   private
