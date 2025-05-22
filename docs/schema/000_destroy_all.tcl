d366a2fd423ce2257ec22b02868ae1b087332f76
Hector Correa <hector_correa@princeton.edu>
2025-05-22 10:28:42 -0400

    Force user to uncomment the destroy commands

--- docs/schema/000_destroy_all.tcl
+++ docs/schema/000_destroy_all.tcl
@@ -1,7 +1,9 @@
-# Use this to reset your development environment
+# Use this to reset your development environment.
 # (the values references on this script do not exist in production.)
 #
 # Deletes the /td-demo-001 namespace and everything underneath
 # as well as the tigerdataX document namespace definition
-asset.namespace.destroy :namespace /td-demo-001
-asset.doc.namespace.destroy :namespace tigerdataX
+
+# UNCOMMENT THESE NEXT TWO LINES
+# asset.namespace.destroy :namespace /td-demo-001
+# asset.doc.namespace.destroy :namespace tigerdataX
