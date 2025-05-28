8bcffcaa9c6c034eab1d6ebdd5b54e88db363a70
leefaisonr <rl3667@princeton.edu>
2025-05-28 13:14:47 -0400

    display button for project request link

--- spec/system/dashboard_spec.rb
+++ spec/system/dashboard_spec.rb
@@ -183,6 +183,14 @@ RSpec.describe "Dashboard", connect_to_mediaflux: true, js: true do
         expect(page).to have_content "Create new project"
       end
 
+      it "shoes the 'New Project Request' button" do
+        sign_in current_user
+        visit dashboard_path
+        expect(page).to have_content("Welcome, #{current_user.given_name}!")
+        expect(page).not_to have_content "Please log in"
+        expect(page).to have_content "New project request"
+      end
+
       it "shows the system administrator dashboard", js: true do
         sign_in current_user
         visit dashboard_path
