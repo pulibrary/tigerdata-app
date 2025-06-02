289600b7b8f682a8ca3df9393c1900201d1d956e
Carolyn Cole <cac9@princeton.edu>
2025-06-02 11:46:40 -0400

    Allow the princeton users to be loaded from ldap
    This occurs each morning via a cron job.  Users will  get updated if thier display name is blank or created if the uid is missing

--- app/services/princeton_users.rb
+++ app/services/princeton_users.rb
@@ -1,11 +1,51 @@
 # frozen_string_literal: true
 class PrincetonUsers
+  CHARS_AND_NUMS =  ('a'..'z').to_a + (0..9).to_a
+
   class << self
+
     def user_list
       Rails.cache.fetch("princeton_user_list", expires_in: 6.hours) do
-        # TODO: - pull this from ldap instead of just the database
         @user_list = User.all.map { |user| { uid: user.uid, name: user.display_name } }
       end
+    end
+
+    def create_users_from_ldap(current_uid_start: "", ldap_connection: default_ldap_connection)
+      CHARS_AND_NUMS.each do |char|
+        filter =(~ Net::LDAP::Filter.eq( "pustatus", "guest" )) & Net::LDAP::Filter.eq("uid", "#{current_uid_start}#{char}*")
+        people = ldap_connection.search(filter:, attributes: [:pudisplayname, :givenname, :sn, :uid, :edupersonprincipalname]); 
+        if ldap_connection.get_operation_result.message == "Success"
+          people.each{|person| user_from_ldap(person)}
+        else
+          create_users_from_ldap(current_uid_start: "#{current_uid_start}#{char}", ldap_connection:)
+        end
+      end
+    end
+
+    def user_from_ldap(ldap_person)
+      uid = ldap_person[:uid].first.downcase
+      current_entries = User.where(uid:)
+      if current_entries.empty?
+        User.create(uid: , display_name: ldap_person[:pudisplayname].first, 
+                    family_name: ldap_person[:sn].first, given_name: ldap_person[:givenname].first, 
+                    email: ldap_person[:edupersonprincipalname].first)
+      else
+        user = current_entries.first
+        if user.display_name.blank?
+          user.display_name = ldap_person[:pudisplayname].first
+          user.family_name = ldap_person[:sn].first
+          user.given_name = ldap_person[:givenname].first
+          user.save
+        end
+      end
+    end
+
+    def default_ldap_connection
+      @default_ldap_connection ||= Net::LDAP.new host: "ldap.princeton.edu", base: "o=Princeton University,c=US", port: 636,
+                                                  encryption: {
+                                                    method: :simple_tls,
+                                                    tls_options: OpenSSL::SSL::SSLContext::DEFAULT_PARAMS
+                                                  }
     end
   end
 end
--- lib/tasks/load_users.rake
+++ lib/tasks/load_users.rake
@@ -4,4 +4,12 @@ namespace :load_users do
   task from_registration_list: :environment do
     User.load_registration_list
   end
+
+  desc "Load user from ldap"
+  task from_ldap: :environment do
+    count_before = User.count
+    PrincetonUsers.create_users_from_ldap
+    count_after = User.count
+    puts "Create #{count_after - count_before} users from ldap"
+  end
 end
