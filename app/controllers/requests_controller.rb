d17125d00880afce171476375e52c76e0dd7fd85
Kate Lynch <katelynch@princeton.edu>
2025-06-03 15:00:12 -0400

    Refactoring and rubocop fixes

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -31,25 +31,8 @@ class RequestsController < ApplicationController
   def approve
     if current_user.superuser || current_user.sysadmin || current_user.trainer
       @request = Request.find(params[:id])
-      project_directory = "/tigerdata/#{@request[:parent_folder]}/#{@request[:project_folder]}"
-      departments = @request[:departments].map { |d| d["name"] }
-      data_users = @request[:user_roles].map { |u| u["uid"] }
-      metadata_json = {
-        title: @request[:project_title],
-        description: @request[:description],
-        status: Project::APPROVED_STATUS,
-        data_sponsor: @request[:data_sponsor],
-        data_manager: @request[:data_manager],
-        departments: departments,
-        data_user_read_only: data_users,
-        project_directory: project_directory,
-        storage_capacity: {size: { approved: @request[:quota], requested: @request[:quota]}, unit: {approved: @request[:storage_unit], requested: @request[:storage_unit]}},
-        storage_performance_expectations: { requested: "Standard", approved: "Standard" },
-        created_by: nil,
-        created_on: @request[:created_at]
-      }
-
-      project = Project.create!({:metadata_json => metadata_json })
+      project_metadata_json = parse_request_metadata(@request)
+      Project.create!({ metadata_json: project_metadata_json })
       @request.destroy
       stub_message = "Project approved and created in the TigerData web portal; request has been processed and deleted"
       redirect_to dashboard_path, notice: stub_message
@@ -61,6 +44,37 @@ class RequestsController < ApplicationController
   end
 
   private
+
+    # rubocop:disable Metrics/MethodLength
+    def parse_request_metadata(request)
+      project_directory = "/tigerdata/#{request[:parent_folder]}/#{request[:project_folder]}"
+      departments = request[:departments].map { |d| d["name"] }
+      data_users = request[:user_roles].map { |u| u["uid"] }
+      {
+        title: request[:project_title],
+        description: request[:description],
+        status: Project::APPROVED_STATUS,
+        data_sponsor: request[:data_sponsor],
+        data_manager: request[:data_manager],
+        departments: departments,
+        data_user_read_only: data_users,
+        project_directory: project_directory,
+        storage_capacity: {
+          size: {
+            approved: request[:quota].split(" ").first,
+            requested: request[:quota].split(" ").first
+          },
+          unit: {
+            approved: request[:storage_unit],
+            requested: request[:storage_unit]
+          }
+        },
+        storage_performance_expectations: { requested: "Standard", approved: "Standard" },
+        created_by: nil,
+        created_on: request[:created_at]
+      }
+    end
+    # rubocop:enable Metrics/MethodLength
 
     def set_breadcrumbs
       add_breadcrumb("Dashboard", dashboard_path)
