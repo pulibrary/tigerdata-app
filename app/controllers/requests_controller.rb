604ab66f95f047a30bc474281a91e80413f8447d
Kate Lynch <katelynch@princeton.edu>
2025-06-03 14:44:48 -0400

    First pass creating a project from request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -30,7 +30,28 @@ class RequestsController < ApplicationController
 
   def approve
     if current_user.superuser || current_user.sysadmin || current_user.trainer
-      stub_message = "Edit the 'approve' method in app/controllers/requests_controller.rb to do something when this button is clicked"
+      @request = Request.find(params[:id])
+      project_directory = "/tigerdata/#{@request[:parent_folder]}/#{@request[:project_folder]}"
+      departments = @request[:departments].map { |d| d["name"] }
+      data_users = @request[:user_roles].map { |u| u["uid"] }
+      metadata_json = {
+        title: @request[:project_title],
+        description: @request[:description],
+        status: Project::APPROVED_STATUS,
+        data_sponsor: @request[:data_sponsor],
+        data_manager: @request[:data_manager],
+        departments: departments,
+        data_user_read_only: data_users,
+        project_directory: project_directory,
+        storage_capacity: {size: { approved: @request[:quota], requested: @request[:quota]}, unit: {approved: @request[:storage_unit], requested: @request[:storage_unit]}},
+        storage_performance_expectations: { requested: "Standard", approved: "Standard" },
+        created_by: nil,
+        created_on: @request[:created_at]
+      }
+
+      project = Project.create!({:metadata_json => metadata_json })
+      @request.destroy
+      stub_message = "Project approved and created in the TigerData web portal; request has been processed and deleted"
       redirect_to dashboard_path, notice: stub_message
     else
       error_message = "You do not have access to this page."
