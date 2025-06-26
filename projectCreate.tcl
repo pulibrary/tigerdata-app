5c688ec3c6eeab496a3b5f23e3cae8213b3261b1
Hector Correa <hector_correa@princeton.edu>
2025-06-26 15:10:10 -0400

    Updated the script to populate all the required fields in the current TigerData project schema. Most of these values are still hard-coded, but some of them are now arguments to the TCL script.

--- projectCreate.tcl
+++ projectCreate.tcl
@@ -8,7 +8,7 @@
 #
 # RUNNING THE SCRIPT OUTSIDE OF MEDIAFLUX:
 #
-#   script.execute :in file:/Users/correah/src/tigerdata-app/projectCreate.tcl :arg -name doi 10.123/456
+#   script.execute :in file:/Users/correah/src/tigerdata-app/projectCreate.tcl :arg -name doi 10.123/456 :arg -name directory test-123 :arg -name title "hello world"
 #
 # INSTALLING THE SCRIPT IN MEDIAFLUX...
 #
@@ -18,30 +18,29 @@
 #
 # ...AND RUNNING IT FROM WITHIN MEDIAFLUX:
 #
-#   asset.script.execute :id path=/system/scripts/projectCreate.tcl :arg -name doi 10.123/456
+#   asset.script.execute :id path=/system/scripts/projectCreate.tcl :arg -name doi 10.123/456 :arg -name directory test-308 :arg -name title "hello world"
 #
 # TO REPLACE THE SCRIPT IN MEDIAFLUX YOU HAVE TO DESTROY IT AND RECREATE IT:
 #
 #   asset.destroy :id path=/system/scripts/projectCreate.tcl
 #
 
-if { [info exists "doi"] } \
+if { [info exists "doi"] && [info exists "directory"] && [info exists "title"] } \
 {
   server.log :app "projectCreate" :event info :msg "Creating project for DOI ${doi}"
 
-  # Project values
-  set projectDirectory "test-minimal-9"
-  set projectDescription "This is just an example description."
-  set projectDOI "10.34770/az09-0001"
-  set projectTitle "Test Project 1"
+  # Hard-coded project values for now
+  # TODO: Make these values arguments to the TCL script
   set schemaVersion "v0.8"
+  set dataSponsor "hc8719"
+  set department "LIB-RDSS"
 
   # Mediaflux paths
   set ns "NS"
   set rootCollection "/td-demo-001/dev/tigerdata"
   set rootNamespace "/td-demo-001/dev/tigerdataNS"
-  set projectCollection $rootCollection/$projectDirectory
-  set projectNamespace $rootNamespace/$projectDirectory$ns
+  set projectCollection $rootCollection/$directory
+  set projectNamespace $rootNamespace/$directory$ns
 
   # Create a namespace for our test project
   set createNS [xvalue exists [asset.namespace.exists :namespace $projectNamespace]]
@@ -50,48 +49,39 @@ if { [info exists "doi"] } \
       asset.namespace.create :namespace $projectNamespace
   }
 
-
-
-  # ==============================================
-  #
-  # TODO: Update to create the project with the existing tigerdata:project schema
-  #
-  # ==============================================
-
   # Create the collection for the test project and set the value for the TigerData schema
   asset.create \
       :pid path=$rootCollection \
       :namespace $projectNamespace \
-      :name $projectDirectory \
+      :name $directory \
       :collection -unique-name-index true -contained-asset-index true -cascade-contained-asset-index true true \
       :type "application/arc-asset-collection" \
       :meta < \
-          :tigerdataX:resourceDoc < \
-              :resource -resourceClass "Project" -resourceID $projectDOI -resourceIDType "DOI" < \
-                  :title -lang "en" $projectTitle \
-                  :description -lang "en" -inherited false -discoverable true -trackingLevel "ResourceRecord" $projectDescription \
-                  :projectID -projectIDType "DOI" -inherited true -discoverable true -trackingLevel "ResourceRecord" $projectDOI \
-                  :projectDirectory -inherited false -discoverable false -trackingLevel "InternalUseOnly" -approved true \
-                  < \
-                      :projectDirectoryPath -protocol "NFS" $projectDirectory \
-                      :requestedValue -protocol "NFS" $projectDirectory \
-                      :approvedValue -protocol "NFS" $projectDirectory \
-                  > \
-                  :dataSponsor -userID "mjc12" -userIDType "NetID" -discoverable true -inherited true -trackingLevel "ResourceRecord" \
-                  :dataManager -userID "mjc12" -userIDType "NetID" -discoverable true -inherited true -trackingLevel "ResourceRecord" \
-                  :departments -discoverable true -trackingLevel "ResourceRecord" < \
-                      :department -departmentCode RDSS -inherited false "Research Data and Scholarly Services" \
-                  > \
-                  :projectProvenance < \
-                      :schemaVersion $schemaVersion \
-                  > \
-              > \
+          :tigerdata:project < \
+            :ProjectDirectory $directory \
+            :Title $title \
+            :Status "status" \
+            :DataSponsor $dataSponsor \
+            :DataManager $dataSponsor \
+            :Department $department \
+            :CreatedOn "NOW" \
+            :CreatedBy $dataSponsor \
+            :ProjectID $doi \
+            :StorageCapacity < \
+              :Size -Requested "123" -Approved "123" 123 \
+              :Unit -Requested "KB" -Approved "KB" "KB" \
+            > \
+            :Performance -Requested "Standard" -Approved "Standard" "Standard" \
+            :ProjectPurpose "testing" \
+            :Submission < \
+              :RequestedBy $dataSponsor \
+              :RequestDateTime "NOW" \
+            > \
+            :SchemaVersion $schemaVersion \
           > \
       >
-
-
 } \
 else \
 {
-  server.log :app "projectCreate" :event error :msg "You must provide a DOI"
+  server.log :app "projectCreate" :event error :msg "You must provide: doi, directory, and title"
 }
