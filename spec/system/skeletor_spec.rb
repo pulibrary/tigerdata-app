7637e1d27cf80d760375d63d16d1e428d0a0c5c3
Precilla Prempeh <pprempeh@princeton.edu>
2025-07-07 15:53:31 -0400

    Created an integration test for skeletor Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

---
+++ spec/system/skeletor_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "WelcomeController", connect_to_mediaflux: true, js: true do
+  context "unauthenticated user" do
+    it "shows the 'Log In' button" do
+      visit "/"
+      expect(page).to have_content "TigerData Web Portal"
+      expect(page).to have_content "Log in"
+      expect(page).to have_link "Accessibility", href: "https://accessibility.princeton.edu/help"
+    end
+end
+
+    #Authenticated user logging in
+    context "authenticated user" do
+        let(:current_user) { FactoryBot.create(:user, uid: "pul123", mediaflux_session: SystemUser.mediaflux_session) }
+        it "redirects to the user's dashboard and shows the logout button",
+          :integration do 
+        sign_in current_user
+        visit "/"
+        expect(page).to have_content("Welcome, #{current_user.given_name}!")
+        click_link current_user.uid.to_s
+        expect(page).to have_content "Log out"
+        end
+    end
+end
