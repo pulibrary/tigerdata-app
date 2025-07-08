fc35f936a73295a90be6d8a4273740de82b9bb19
Precilla Prempeh <pprempeh@princeton.edu>
2025-07-08 11:52:23 -0400

    Added the link to the Skeletor Epic and updated the description
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

--- spec/system/skeletor_spec.rb
+++ spec/system/skeletor_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-
-RSpec.describe "WelcomeController", connect_to_mediaflux: true, js: true, integration: true do
+# This is the automated test to the Skeletor epic https://github.com/pulibrary/tigerdata-app/issues/1478
+RSpec.describe "The Skeletor Epic", connect_to_mediaflux: true, js: true, integration: true do
   context "unauthenticated user" do
     it "shows the 'Log In' button" do
       visit "/"
