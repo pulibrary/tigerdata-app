648fff9b0b7703d1aebbe6e9c030329454fdc7a5
Precilla Prempeh <prescillaprempeh@gmail.com>
2025-07-16 16:16:33 -0400

    Skeletor spec (#1603)
    * Created an integration test for skeletor
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * Included skeletor test in integration suite
    
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * Used rubocop to fix format
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * Added the link to the Skeletor Epic and updated the description
    
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * Updated skeletor spec file to fill in the form fields
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * Updated file to fill in data manager and data sponsor
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * Updated skeletor_spec to test if a sysadmin or a superuser can fill out the form and submit. Also added a test to confirm  if a sysadmin or a superuser can approve a project.
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * rubocop
    
    * Removed commented lines from skeletor spec
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Precilla Prempeh <pprempeh@princeton.edu>
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

--- spec/system/skeletor_spec.rb
+++ spec/system/skeletor_spec.rb
@@ -23,4 +23,82 @@ RSpec.describe "The Skeletor Epic", connect_to_mediaflux: true, js: true, integr
       expect(page).to have_content "Log out"
     end
   end
+
+  context "sysadmin" do
+    let(:current_sysadmin) { FactoryBot.create(:sysadmin, uid: "sys123", mediaflux_session: SystemUser.mediaflux_session) }
+    let(:datasponsor) { FactoryBot.create(:project_sponsor) } # we grabbed this from project.rb
+    let(:datamanager) { FactoryBot.create(:data_manager) }
+    before do
+      datasponsor
+    end
+    it "allows the sysadmin to fill out the project" do
+      # this is the feature flipper
+      test_strategy = Flipflop::FeatureSet.current.test!
+      test_strategy.switch!(:new_project_request_wizard, true)
+      Affiliation.load_from_file(Rails.root.join("spec", "fixtures", "departments.csv"))
+
+      sign_in current_sysadmin
+      visit "/"
+      click_on "New Project Request"
+      expect(page).to have_content "Basic Details"
+      fill_in :project_title, with: "She was a Fairy"
+      expect(page).to have_content "15/200 characters"
+      fill_in :parent_folder, with: "Fairy"
+      fill_in :project_folder, with: "Pixie Dust"
+      fill_in :description, with: "An awesome project to show the wizard is magic"
+      expect(page).to have_content "46/1000 characters"
+      expect(page).not_to have_content("(77777) RDSS-Research Data and Scholarship Services")
+      # Non breaking space `u00A0` is at the end of every option to indicate an option was selected
+      select "(77777) RDSS-Research Data and Scholarship Services\u00A0", from: "department_find"
+      # This is triggering the html5 element like it would normally if the page has focus
+      page.find(:datalist_input, "department_find").execute_script("document.getElementById('department_find').dispatchEvent(new Event('input'))")
+      expect(page).to have_content("(77777) RDSS-Research Data and Scholarship Services")
+      expect(page).to have_field("request[departments][]", type: :hidden, with: "{\"code\":\"77777\",\"name\":\"RDSS-Research Data and Scholarship Services\"}")
+      click_on "Roles and People"
+      fill_in :request_data_sponsor, with: datasponsor.uid
+      fill_in :request_data_manager, with: datamanager.uid
+      click_on "Review and Submit"
+      click_on "Next"
+      click_on "Approve request"
+    end
+  end
+
+  context "superuser" do
+    let(:current_superuser) { FactoryBot.create(:superuser, uid: "superuser1", mediaflux_session: SystemUser.mediaflux_session) }
+    let(:datasponsor) { FactoryBot.create(:project_sponsor) } # we grabbed this from project.rb
+    let(:datamanager) { FactoryBot.create(:data_manager) }
+    before do
+      datasponsor
+    end
+    it "allows the superuser to fill out the project" do
+      # this is the feature flipper
+      test_strategy = Flipflop::FeatureSet.current.test!
+      test_strategy.switch!(:new_project_request_wizard, true)
+      Affiliation.load_from_file(Rails.root.join("spec", "fixtures", "departments.csv"))
+
+      sign_in current_superuser
+      visit "/"
+      click_on "New Project Request"
+      expect(page).to have_content "Basic Details"
+      fill_in :project_title, with: "She was a Fairy"
+      expect(page).to have_content "15/200 characters"
+      fill_in :parent_folder, with: "Fairy"
+      fill_in :project_folder, with: "Pixie Dust"
+      fill_in :description, with: "An awesome project to show the wizard is magic"
+      expect(page).to have_content "46/1000 characters"
+      expect(page).not_to have_content("(77777) RDSS-Research Data and Scholarship Services")
+      # Non breaking space `u00A0` is at the end of every option to indicate an option was selected
+      select "(77777) RDSS-Research Data and Scholarship Services\u00A0", from: "department_find"
+      # This is triggering the html5 element like it would normally if the page has focus
+      page.find(:datalist_input, "department_find").execute_script("document.getElementById('department_find').dispatchEvent(new Event('input'))")
+      expect(page).to have_content("(77777) RDSS-Research Data and Scholarship Services")
+      expect(page).to have_field("request[departments][]", type: :hidden, with: "{\"code\":\"77777\",\"name\":\"RDSS-Research Data and Scholarship Services\"}")
+      click_on "Roles and People"
+      fill_in :request_data_sponsor, with: datasponsor.uid
+      fill_in :request_data_manager, with: datamanager.uid
+      click_on "Review and Submit"
+      click_on "Next"
+      click_on "Approve request"
+    end
+  end
 end
