acebfc1bf83a82710b444c087863c873fa5c215f
Hector Correa <hector_correa@princeton.edu>
2025-07-23 14:37:08 -0400

    Fixes another test to use the new default namespaces

--- spec/models/mediaflux/asset_exist_request_spec.rb
+++ spec/models/mediaflux/asset_exist_request_spec.rb
@@ -2,26 +2,26 @@
 require "rails_helper"
 
 RSpec.describe Mediaflux::AssetExistRequest, type: :model, connect_to_mediaflux: true do
+  let!(:sponsor_and_data_manager_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "hc8719", mediaflux_session: SystemUser.mediaflux_session) }
   let(:session_token) { Mediaflux::LogonRequest.new.session_token }
-  let(:namespace_root) { Rails.configuration.mediaflux["api_root_collection_namespace"] }
-  let(:name) { FFaker::InternetSE.company_name_single_word }
+  let(:user) { FactoryBot.create(:user, mediaflux_session: SystemUser.mediaflux_session) }
+  let(:project) { FactoryBot.build :project_with_doi }
 
   before do
-    create_req = Mediaflux::CollectionAssetCreateRootRequest.new(session_token: session_token, namespace: namespace_root, name: name)
-    create_req.resolve
+    project.approve!(current_user: user)
   end
 
   describe "#exist" do
     it "validates that an asset exist or not" do
-      valid_path = "/#{namespace_root}/#{name}"
+      valid_path = "princeton/#{project.metadata_model.project_directory}"
       subject = described_class.new(session_token: session_token, path: valid_path)
       expect(subject.exist?).to be true
 
-      non_existing_path = "/#{namespace_root}/#{name}-not-existing"
-      subject = described_class.new(session_token: session_token, path: non_existing_path)
+      nonexisting_path = "princeton/#{project.metadata_model.project_directory}-nope"
+      subject = described_class.new(session_token: session_token, path: nonexisting_path)
       expect(subject.exist?).to be false
 
-      non_existing_namespace = "/#{namespace_root}-not-existing/#{name}"
+      non_existing_namespace = "princeton-non-existing/#{project.metadata_model.project_directory}"
       subject = described_class.new(session_token: session_token, path: non_existing_namespace)
       expect(subject.exist?).to be false
     end
