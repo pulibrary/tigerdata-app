d44fe43c9e3e7ae14647abf9cc40121db9ebaa10
Precilla Prempeh <pprempeh@princeton.edu>
2025-07-25 14:18:55 -0400

    A user can see a project they are affiliated with Co-authored-by: Bess Sadler <bess@users.noreply.github.com> Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

--- spec/system/skeletor_spec.rb
+++ spec/system/skeletor_spec.rb
@@ -100,9 +100,52 @@ RSpec.describe "The Skeletor Epic", connect_to_mediaflux: true, js: true, integr
       click_on "Review and Submit"
       click_on "Next"
       click_on "Approve request"
-      expect(Project.last.metadata_json["project_id"]).to eq "10.34770/tbd"
+      expect(Project.last.metadata_json["project_id"]).to eq "10.34770/tbd" # once a sysadmin or superuser click on approve request then it should take us to the details page and display the project ID. This is the fake DOI (10.34770/tbd)
     end
+  end
+
+  context "User Affiliated with Project" do
+    let(:current_sysadmin) { FactoryBot.create(:sysadmin, uid: "sys123", mediaflux_session: SystemUser.mediaflux_session) }
+    let(:current_user) { FactoryBot.create(:user, uid: "user4", mediaflux_session: SystemUser.mediaflux_session) }
+    let(:datasponsor) { FactoryBot.create(:project_sponsor, uid: "ds123", mediaflux_session: SystemUser.mediaflux_session) }
+    let(:datamanager) { FactoryBot.create(:data_manager) }
+    it "allows the user affliated with the project to see their project" do
+      # this is the feature flipper
+      test_strategy = Flipflop::FeatureSet.current.test!
+      test_strategy.switch!(:new_project_request_wizard, true)
+      Affiliation.load_from_file(Rails.root.join("spec", "fixtures", "departments.csv"))
+      expect(Project.count).to eq 0
+      sign_in current_sysadmin
+      visit "/"
+      click_on "New Project Request"
+      expect(page).to have_content "Basic Details"
+      fill_in :project_title, with: "She was a Fairy"
+      expect(page).to have_content "15/200 characters"
+      fill_in :parent_folder, with: "Fairy"
+      fill_in :project_folder, with: "Pixie Dust"
+      fill_in :description, with: "An awesome project to show the wizard is magic"
+      expect(page).to have_content "46/1000 characters"
+      expect(page).not_to have_content("(77777) RDSS-Research Data and Scholarship Services")
+      # Non breaking space `u00A0` is at the end of every option to indicate an option was selected
+      select "(77777) RDSS-Research Data and Scholarship Services\u00A0", from: "department_find"
+      # This is triggering the html5 element like it would normally if the page has focus
+      page.find(:datalist_input, "department_find").execute_script("document.getElementById('department_find').dispatchEvent(new Event('input'))")
+      expect(page).to have_content("(77777) RDSS-Research Data and Scholarship Services")
+      expect(page).to have_field("request[departments][]", type: :hidden, with: "{\"code\":\"77777\",\"name\":\"RDSS-Research Data and Scholarship Services\"}")
+      click_on "Roles and People"
+      fill_in :request_data_sponsor, with: datasponsor.uid
+      fill_in :request_data_manager, with: datamanager.uid
+      click_on "Review and Submit"
+      click_on "Next"
+      click_on "Approve request"
+      page.find(:css, "#dropdownMenuButton").click
+      click_on "Log out"
+      sign_in datasponsor
+      visit "/"
+      expect(page).to have_content("She was a Fairy")
+    end  
   end
 end
 
-# once a sysadmin or superuser click on approve request then it should take us to the details page and display the project ID. This is the fake DOI (10.34770/tbd)
+
+
