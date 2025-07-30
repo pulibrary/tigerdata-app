c1b5c9a5f02b0ab542e5e7723f59f8e8c3d8b3da
HyppoliteJ <jh6441@princeton.edu>
2025-07-30 16:13:09 -0400

    adding project and user steps to dry operations

--- app/operations/project_create.rb
+++ app/operations/project_create.rb
@@ -9,7 +9,7 @@ class CreateProject < Dry::Operation
 
   private
 
-    def create_project_from_request(request)
+    def create_project_from_request(_request)
       project_metadata_json = RequestProjectMetadata.convert(self)
       # Create the project in the Rails database
       project = Project.create!({ metadata_json: project_metadata_json })
@@ -20,11 +20,28 @@ class CreateProject < Dry::Operation
     end
 
     def persist_in_mediaflux(project)
-      result = create_in_mediaflux(project:, approver:)
+      # Create the project in Mediaflux
+      request = Mediaflux::ProjectCreateServiceRequest.new(session_token: current_user.mediaflux_session, project: self)
+      request.resolve
+
+      if request.mediaflux_id.to_i == 0
+        raise ProjectCreateError, "Error saving project #{id} to Mediaflux: #{request.response_error}. Debug output: #{request.debug_output}"
+      end
+
+      project.mediaflux_id = request.mediaflux_id
+      project.metadata_model.status = Project::APPROVED_STATUS
+      project.save!
+      Success project.mediaflux_id
       # Return Success(user) or Failure(error)
     end
 
-    def persist_users_in_mediaflux(project, mediaflux_id)
+    def persist_users_in_mediaflux(_project, _mediaflux_id)
+      # Add the data users to the project in Mediaflux
+      add_users_request = Mediaflux::ProjectUserAddRequest.new(session_token: current_user.mediaflux_session, project: self)
+      add_users_request.resolve
+
+      user_debug = add_users_request.debug_output.to_s
+      Rails.logger.error "Project #{id} users have been added to MediaFlux: #{user_debug}"
       Success true
       # Return Success(true) or Failure(error)
     end
