578302a3b522fa29c5775d58cf3b8de3a0f3d94c
HyppoliteJ <jh6441@princeton.edu>
2025-08-01 13:22:43 -0400

    adding desired success and failure outcomes

--- app/operations/project_create.rb
+++ app/operations/project_create.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 class CreateProject < Dry::Operation
-  def call(request)
+  def call(request:)
     project = step create_project_from_request(request)
-    mediaflux_id = step persist_in_mediaflux(project)
+    mediaflux_id = step persist_in_mediaflux(project, request.current_user)
     step persist_users_in_mediaflux(project, mediaflux_id)
     Success project
   end
 
   private
 
-    def create_project_from_request(_request)
-      project_metadata_json = RequestProjectMetadata.convert(self)
+    def create_project_from_request(request)
       # Create the project in the Rails database
+      project_metadata_json = RequestProjectMetadata.convert(request)
       project = Project.create!({ metadata_json: project_metadata_json })
       project.draft_doi
       project.save!
-      Success project
+
       # Return Success(attrs) or Failure(error)
+      Success project
     end
 
-    def persist_in_mediaflux(project)
+    # rubocop:disable Metrics/MethodLength
+    def persist_in_mediaflux(project, current_user)
       # Create the project in Mediaflux
-      request = Mediaflux::ProjectCreateServiceRequest.new(session_token: current_user.mediaflux_session, project: self)
+      request = Mediaflux::ProjectCreateServiceRequest.new(session_token: current_user.mediaflux_session, project: project)
       request.resolve
 
       if request.mediaflux_id.to_i == 0
@@ -31,17 +33,29 @@ class CreateProject < Dry::Operation
       project.mediaflux_id = request.mediaflux_id
       project.metadata_model.status = Project::APPROVED_STATUS
       project.save!
+
+      debug_output = if request.mediaflux_id == 0
+                       "Error saving project #{id} to Mediaflux: #{request.response_error}. Debug output: #{request.debug_output}"
+                     else
+                       request.debug_output.to_s
+                     end
+      Rails.logger.error debug_output
+
+      ProvenanceEvent.generate_approval_events(project: project, user: current_user, debug_output: debug_output)
+
       Success project.mediaflux_id
       # Return Success(user) or Failure(error)
     end
+    # rubocop:enable Metrics/MethodLength
 
     def persist_users_in_mediaflux(_project, _mediaflux_id)
       # Add the data users to the project in Mediaflux
-      add_users_request = Mediaflux::ProjectUserAddRequest.new(session_token: current_user.mediaflux_session, project: self)
+      add_users_request = Mediaflux::ProjectUserAddRequest.new(session_token: current_user.mediaflux_session, project: project)
       add_users_request.resolve
 
       user_debug = add_users_request.debug_output.to_s
       Rails.logger.error "Project #{id} users have been added to MediaFlux: #{user_debug}"
+
       Success true
       # Return Success(true) or Failure(error)
     end
