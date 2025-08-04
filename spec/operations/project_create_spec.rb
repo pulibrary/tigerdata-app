34c994a7745986149530c97e919d95f831f8e75f
Jaymee Hyppolite <54775395+JaymeeH@users.noreply.github.com>
2025-08-04 15:42:45 -0400

    Add data users to a project after it is created in mediaflux (#1657)
    * Add data users to a project after it is created in mediaflux
    
    * only send the mediaflux id and data users in the request
    
    * fixing multiple users command
    
    * setup dry operation
    
    * setup dry operation
    
    * revert request model
    
    * adding project and user steps to dry operations
    
    * adding desired success and failure outcomes
    
    * calling the dry operation in the request model
    
    * Flattening successful results until we reach the project
    
    * Raising and logging a project create error within the project create dry-operation
    
    * Creating tests for project create dry operations
    
    * spec file for operations
    
    * testing success and failure cases for the dry operation
    
    * finalizing tests
    
    * tagging operation spec as integration
    
    * removeing require relative statement

---
+++ spec/operations/project_create_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe ProjectCreate, type: :operation, integration: true do
+  let!(:approver) { FactoryBot.create(:sponsor_and_data_manager, uid: "hc8719", mediaflux_session: SystemUser.mediaflux_session) }
+  let(:valid_request) do
+    Request.create(request_type: "new_project_request", request_title: "Request for Example Project", project_title: "Example Project",
+                   data_sponsor: approver.uid, data_manager: approver.uid,
+                   departments: [{ code: "dept", name: "department" }],
+                   description: "description", parent_folder: random_project_directory,
+                   project_folder: "project", project_id: "doi", quota: "500 GB",
+                   requested_by: "uid", user_roles: [])
+  end
+
+  let(:invalid_request) do
+    Request.create(request_type: "new_project_request", request_title: "Request for Example Project", project_title: "Example Project",
+                   data_sponsor: approver.uid, data_manager: approver.uid,
+                   departments: [{ code: "dept", name: "department" }],
+                   description: "description", parent_folder: random_project_directory,
+                   project_folder: "project", project_id: "doi", quota: "not-valid", # quota is not valid
+                   requested_by: "uid", user_roles: [])
+  end
+  subject { described_class.new } # Or initialize with dependencies if any
+
+  describe "#call" do
+    context "Success case" do
+      it "creates a project and persists it in Mediaflux" do
+        result = described_class.new.call(request: valid_request, approver: approver)
+        expect(result).to be_success
+
+        result = result.flatten while result.class != Project
+        expect(result.mediaflux_id).not_to eq(0)
+      end
+    end
+
+    context "Failure case" do
+      it "raises an error if the project cannot be saved to Mediaflux" do
+        expect { described_class.new.call(request: invalid_request, approver: approver) }.to raise_error
+        expect do
+          described_class.new.call(request: invalid_request, approver: approver)
+        end.to raise_error(ProjectCreate::ProjectCreateError, /Error saving project/)
+      end
+    end
+  end
+end
