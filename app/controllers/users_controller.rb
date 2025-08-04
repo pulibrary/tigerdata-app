beff7f41792971f325671c4774f300ccec5dc18a
Hector Correa <hector_correa@princeton.edu>
2025-08-04 17:22:51 -0400

    First pass at getting the controllers and views for the edit process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,38 @@
 # frozen_string_literal: true
 class UsersController < ApplicationController
   before_action :set_breadcrumbs
-  # GET /requests
+  before_action :check_user_access
+
   def index
-    if current_user.superuser || current_user.sysadmin || current_user.trainer
-      @users = User.order("uid ASC NULLS LAST").page params[:page]
-      add_breadcrumb("Current Users")
-    else
-      error_message = "You do not have access to this page."
-      flash[:notice] = error_message
-      redirect_to dashboard_path
-    end
+    @users = User.order("uid ASC NULLS LAST").page params[:page]
+  end
+
+  def show
+    add_breadcrumb("User")
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    add_breadcrumb("Edit User")
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    byebug
+    # TODO implement update
+    redirect_to user_path(@user)
   end
 
   private
 
     def set_breadcrumbs
-      add_breadcrumb("Dashboard", dashboard_path)
+      add_breadcrumb("Users", users_path)
+    end
+
+    def check_user_access
+      return if current_user.superuser || current_user.sysadmin || current_user.trainer
+      flash[:notice] = "You do not have access to this page."
+      redirect_to dashboard_path
     end
 end
