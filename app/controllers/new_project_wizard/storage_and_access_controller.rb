bb6d654e7c60bb425d0a639557ff8178b3513005
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-08-04 16:09:24 -0400

    Change the next and back buttons to skip the unused steps in the wizard (#1677)
    Co-authored-by: Hector Correa <hector_correa@princeton.edu>

--- app/controllers/new_project_wizard/storage_and_access_controller.rb
+++ app/controllers/new_project_wizard/storage_and_access_controller.rb
@@ -8,10 +8,14 @@ class NewProjectWizard::StorageAndAccessController < RequestWizardsController
     end
 
     def render_next
-      redirect_to new_project_additional_information_grants_and_funding_path(request_model)
+      # TODO: redirect to additional information when that screen is implemented
+      # redirect_to new_project_additional_information_grants_and_funding_path(request_model)
+      redirect_to new_project_review_and_submit_path(request_model)
     end
 
     def render_back
-      redirect_to new_project_project_type_path(request_model)
+      # TODO: redirect back to project type when that screen is implemented
+      # redirect_to new_project_project_type_path(request_model)
+      redirect_to new_project_roles_and_people_path(request_model)
     end
 end
