dc39e6addcc694fa1c7d88bcbdbd2e6e0227ad8e
Hector Correa <hector_correa@princeton.edu>
2025-08-04 15:04:28 -0400

    Adjust test to not use td-test-001 as the root namespace when running rspec and use the new /princeton namespace that comes in the Docker container with Mediaflux

--- spec/models/mediaflux/namespace_exist_request_spec.rb
+++ spec/models/mediaflux/namespace_exist_request_spec.rb
@@ -3,17 +3,15 @@ require "rails_helper"
 
 RSpec.describe Mediaflux::NamespaceExistRequest, type: :model, connect_to_mediaflux: true do
   let(:session_token) { Mediaflux::LogonRequest.new.session_token }
-  let(:namespace_root) { "#{Mediaflux::Connection.root_namespace}/#{FFaker::InternetSE.company_name_single_word}" }
+  let(:namespace_root) { "/princeton" } # this namespace is already created in the Docker container
+  let(:namespace_non_existing) { "/princeton/#{random_project_directory}" }
 
   describe "#exist" do
     it "detects whether a namespace exists or not" do
-      subject = described_class.new(session_token: session_token, namespace: namespace_root)
+      subject = described_class.new(session_token: session_token, namespace: namespace_non_existing)
       expect(subject.exist?).to be false
 
-      # create the namespace
-      Mediaflux::NamespaceCreateRequest.new(session_token: session_token, namespace: namespace_root).resolve
-
-      subject = described_class.new(session_token: session_token, namespace: namespace_root)
+      subject = described_class.new(session_token: session_token, namespace: namespace_root )
       expect(subject.exist?).to be true
     end
   end
