9b2d1d762d8dbd112427f948ef21b27c664ce67f
HyppoliteJ <jh6441@princeton.edu>
2025-08-04 13:14:30 -0400

    testing success and failure cases for the dry operation

--- spec/operations/project_create_spec.rb
+++ spec/operations/project_create_spec.rb
@@ -11,25 +11,34 @@ RSpec.describe ProjectCreate, type: :operation do
                    project_folder: "project", project_id: "doi", quota: "500 GB",
                    requested_by: "uid", user_roles: [])
   end
-  # let(:approver) { FactoryBot.create(:user, mediaflux_session: SystemUser.mediaflux_session) }
+
+  let(:invalid_request) do
+    described_class.create(request_type: "new_project_request", request_title: "Request for Example Project", project_title: "Example Project",
+                           data_sponsor: sponsor_and_data_manager_user.uid, data_manager: sponsor_and_data_manager_user.uid,
+                           departments: [{ code: "dept", name: "department" }],
+                           description: "description", parent_folder: random_project_directory,
+                           project_folder: "project", project_id: "doi", quota: "not-valid", # quota is not valid
+                           requested_by: "uid", user_roles: [])
+  end
   subject { described_class.new } # Or initialize with dependencies if any
 
   describe "#call" do
     context "Success case" do
       it "creates a project and persists it in Mediaflux" do
         result = described_class.new.call(request: valid_request, approver: approver)
-
         expect(result).to be_success
-        expect(result.value.mediaflux_id).not_to eq(0)
+
+        result = result.flatten while result.class != Project
+        expect(result.mediaflux_id).not_to eq(0)
       end
     end
 
     context "Failure case" do
       it "raises an error if the project cannot be saved to Mediaflux" do
-        allow_any_instance_of(Mediaflux::ProjectCreateServiceRequest).to receive(:resolve).and_return(double(mediaflux_id: 0, response_error: "Error saving project", debug_output: "Debug info"))
-
+        result = described_class.new.call(request: invalid_request, approver: approver)
+        expect(result).to be_failure
         expect do
-          described_class.new.call(request: request, approver: approver)
+          described_class.new.call(request: invalid_request, approver: approver)
         end.to raise_error(ProjectCreate::ProjectCreateError, /Error saving project/)
       end
     end
