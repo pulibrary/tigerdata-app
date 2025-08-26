52f6314de4975781c92bcb8bf72015362ac7c96f
Hector Correa <hector_correa@princeton.edu>
2025-08-26 11:37:04 -0400

    Use the same structure as before for the users

--- app/javascript/entrypoints/userRolesAutocomplete.js
+++ app/javascript/entrypoints/userRolesAutocomplete.js
@@ -30,7 +30,7 @@ function addNewUser(value, uid) {
   li.appendChild(newDiv);
   const input = document.createElement('input');
   input.type = 'hidden';
-  input.value = uid;
+  input.value = JSON.stringify({uid: uid, name: value});
   input.name = 'request[user_roles][]';
   li.appendChild(input);
   ul.appendChild(li);
@@ -40,11 +40,11 @@ function addNewUser(value, uid) {
 // eslint-disable-next-line import/prefer-default-export
 export function userRolesAutocomplete() {
   $('#user_find').on('input', (event) => {
-    // We add a non-breaking back space to each option to see when a option has been entered into the text input as
-    //  no user would type a &nbsp; into the input.  When the HTML5 option is selected we can see clearly that it has;
-    //  This solution was grabbed from https://stackoverflow.com/a/74598110/16862920
+    // We add a non-breaking space (HTML &nbsp; HEX A0) to each option to detect when a option has been entered
+    // into the textbox. We use this character as no user will input this character into the textbox.
+    // This solution was grabbed from https://stackoverflow.com/a/74598110/16862920
     const { value } = event.currentTarget;
-    if (value.slice(-1) === '\xa0') {
+    if (value.slice(-1) === '\xA0') {
       const userRoleList = document.getElementById('request-user-roles');
       const cleanValue = value.slice(0, -1);
       if (!userRoleList.value.includes(cleanValue)) {
