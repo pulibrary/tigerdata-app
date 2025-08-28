f2bb3b60d6b52884f622dfd2f464c907e7027adc
Hector Correa <hector_correa@princeton.edu>
2025-08-28 12:09:14 -0400

    Got the autocomplete to work with partial matches

--- app/javascript/entrypoints/userRolesAutocomplete.js
+++ app/javascript/entrypoints/userRolesAutocomplete.js
@@ -39,6 +39,49 @@ function addNewUser(uid, name) {
 
 // eslint-disable-next-line import/prefer-default-export
 export function userRolesAutocomplete(usersLookupUrl) {
+  // Debounce is used to limit the number of times a function is called in a short period of time.
+  // source: https://github.com/jrrio/datalist-autocomplete-ajax/blob/main/main.js#L159C3-L166C5
+  // see also: https://www.geeksforgeeks.org/javascript/debouncing-in-javascript/
+  const debounce = (func, delay = 250) => {
+    let timeout;
+    return function (...args) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  };
+
+  // Performs a user lookup via AJAX
+  const search = (searchValue) => {
+    $.ajax({
+      url: `${usersLookupUrl}?query=${searchValue}`,
+      success: (result) => {
+        // Re-populate the dataList with the results from the AJAX call
+        const dataList = $('#princeton_users');
+        dataList.empty();
+        for (let i = 0; i < result.suggestions.length; i += 1) {
+          const uid = result.suggestions[i].data;
+          const userName = result.suggestions[i].value;
+          // remove punctuation from the value displayed in the datalist so that
+          // matches can be made without having to be super exact (makes the search
+          // more forgiving in FireFox)
+          const cleanUserName = userName.replace(/[\,\.]/g, " ").replace(/\s\s/g, " ");
+          dataList.append(
+            `<option data-uid="${uid}" data-name="${userName}" value="(${uid}) ${cleanUserName}\xA0"></option>`,
+          );
+        }
+      },
+    });
+  }
+
+  // Wraps the search function so that it is not called too frequently
+  // if the user types the search value quickly. For example if the user
+  // enters "abc" quickly we call the search once with "abc" rather than
+  // three times ("a", then "ab", and then "abc").
+  const debouncedSearch = debounce(search);
+
+  // Wire the textbox to work as an autocomple textbox
   $('#user_find').on('input', (event) => {
     // When populating the dataList for the user list we add a non-breaking space (HTML &nbsp; HEX A0)
     // to each option. We use this special character here to detect when a user has selected an option
@@ -57,22 +100,7 @@ export function userRolesAutocomplete(usersLookupUrl) {
       current.value = '';
     } else {
       // User is typing, fetch users that match the value entered so far
-      $.ajax({
-        url: `${usersLookupUrl}?query=${value}`,
-        success: (result) => {
-          // Clear the current list of values in the dataList...
-          const dataList = $('#princeton_users');
-          dataList.empty();
-          // ...and repopulate the dataList with the results from the AJAX call
-          for (let i = 0; i < result.suggestions.length; i += 1) {
-            const uid = result.suggestions[i].data;
-            const userName = result.suggestions[i].value;
-            dataList.append(
-              `<option data-uid="${uid}" data-name="${userName}" value="(${uid}) ${userName}\xA0"></option>`,
-            );
-          }
-        },
-      });
+      debouncedSearch(value);
     }
   });
 
