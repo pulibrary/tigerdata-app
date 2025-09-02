dc9f018400cdb0b1896506cd15428ee32ac73690
Bess Sadler <bess.sadler@princeton.edu>
2025-09-02 15:58:59 -0400

    Style fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
 
   # Render a 404 page for any undefined route
   def render_not_found
-    render file: "#{Rails.root.join('public', '404.html')}", layout: false, status: :not_found
+    render file: Rails.public_path.join("404.html").to_s, layout: false, status: :not_found
   end
 
   private
