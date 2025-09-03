8f8172b6e3da741791b8af2db24abbac509181ac
HyppoliteJ <jh6441@princeton.edu>
2025-09-03 14:52:12 -0400

    update migration

--- db/migrate/20250903182114_add_developers.rb
+++ db/migrate/20250903182114_add_developers.rb
@@ -1,6 +1,6 @@
 class AddDevelopers < ActiveRecord::Migration[7.2]
   def change
-    remove_column :users, :developer, :boolean
+    remove_column :users, :superuser, :boolean
     add_column :users, :developer, :boolean, default: false
   end
 end
