7ab624d5c4827107f9a414d0746ff928e8b569b7
HyppoliteJ <jh6441@princeton.edu>
2025-09-03 14:24:48 -0400

    revert old migration and add new migration

--- db/migrate/20240209131952_add_superuser_to_user.rb
+++ db/migrate/20240209131952_add_superuser_to_user.rb
@@ -1,11 +1,11 @@
-class AddDeveloperToUser < ActiveRecord::Migration[7.0]
+class AddSuperuserToUser < ActiveRecord::Migration[7.0]
   def up
-    add_column :users, :developer, :boolean, default: false
+    add_column :users, :superuser, :boolean, default: false
     add_column :users, :sysadmin, :boolean, default: false
   end
 
   def down
-    remove_column :users, :developer, :boolean
+    remove_column :users, :superuser, :boolean
     remove_column :users, :sysadmin, :boolean
   end
 end
