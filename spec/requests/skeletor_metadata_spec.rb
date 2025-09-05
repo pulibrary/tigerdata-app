4465cfb9a09344db89e26bce83b0429cd83e766b
leefaisonr <rl3667@princeton.edu>
2025-09-05 15:52:37 -0400

    load in users to access project xml

--- spec/requests/skeletor_metadata_spec.rb
+++ spec/requests/skeletor_metadata_spec.rb
@@ -3,18 +3,18 @@
 require "rails_helper"
 
 RSpec.describe "The Skeletor Metadata", connect_to_mediaflux: true, metadata: true do
-  context "retrieves xml for a project" do
-    let!(:sponsor_and_data_manager_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
-    let(:project) { FactoryBot.create(:approved_project, sponsor_and_data_manager_user: sponsor_and_data_manager_user.uid) }
+  let!(:sponsor_and_data_manager_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
+  let(:data_manager) { FactoryBot.create(:user, mediaflux_session: SystemUser.mediaflux_session) }
+  let(:project) { FactoryBot.create(:approved_project, data_manager: data_manager.uid) }
 
+  context "retrieves xml for a project" do
     before do
-      project.approve!(current_user: sponsor_and_data_manager_user)
-      sign_in sponsor_and_data_manager_user
+      sign_in data_manager
     end
 
-    # it "provides the xml metadata for a project" do
-    # get project_show_mediaflux_url(project), params: { format: :xml }
-    # byebug
-    # end
+    it "provides the xml metadata for a project" do
+      get project_show_mediaflux_url(project), params: { format: :xml }
+      expect(response.content_type).to match "xml"
+    end
   end
 end
