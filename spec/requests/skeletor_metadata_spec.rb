a3bbe2f48c975eed595edb2fc74429479a76d931
Precilla Prempeh <pprempeh@princeton.edu>
2025-09-05 09:27:33 -0400

    testing that We can get the xml returned by rails and check the metadata values Co-authored-by: Bess Sadler <bess@users.noreply.github.com> Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

--- spec/requests/skeletor_metadata_spec.rb
+++ spec/requests/skeletor_metadata_spec.rb
@@ -4,8 +4,12 @@ require "rails_helper"
 require "open-uri"
 
 RSpec.describe "The Skeletor Metadata", connect_to_mediaflux: true, metadata: true do
-  it "recognizes a test" do
-    x = "metadata"
-    expect(x).to eq "metadata"
+  context "retrieves xml for a project" do
+  let(:current_sysadmin) { FactoryBot.create(:sysadmin, uid: "sys123", mediaflux_session: SystemUser.mediaflux_session) }
+    it "provides the xml metadata for a project" do
+        get project_show_mediaflux_url(project), params: { format: :xml }
+        expect(response.code).to eq "200"
+        expect(response.content_type).to match "xml"
+    end
   end
 end
