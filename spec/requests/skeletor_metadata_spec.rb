97149b09ccd72730ca2dc43b3d4435f6dea5a1ad
Precilla Prempeh <prescillaprempeh@gmail.com>
2025-09-08 15:02:42 -0400

    testing that We can get the xml returned by rails and check the metadata values (#1819)
    * testing that We can get the xml returned by rails and check the metadata values
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    * continued work on xml metadata
    
    * load in users to access project xml
    
    * adjust project resource url
    
    * test that we can get the xml returned by rails
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Precilla Prempeh <pprempeh@princeton.edu>
    Co-authored-by: leefaisonr <rl3667@princeton.edu>
    Co-authored-by: Robert-Anthony Lee-Faison <108823963+leefaisonr@users.noreply.github.com>

--- spec/requests/skeletor_metadata_spec.rb
+++ spec/requests/skeletor_metadata_spec.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "open-uri"
 
-RSpec.describe "The Skeletor Metadata", connect_to_mediaflux: true, metadata: true do
-  it "recognizes a test" do
-    x = "metadata"
-    expect(x).to eq "metadata"
+RSpec.describe "The Skeletor Metadata", connect_to_mediaflux: true, metadata: true, type: :request do
+  let(:data_manager) { FactoryBot.create(:data_manager, uid: "pp9425", mediaflux_session: SystemUser.mediaflux_session) }
+  let(:data_sponsor) { FactoryBot.create(:project_sponsor, uid: "rl3667", mediaflux_session: SystemUser.mediaflux_session) }
+  let(:project) { FactoryBot.create(:approved_project, data_manager: data_manager.uid, data_sponsor: data_sponsor.uid) }
+
+  context "retrieves xml for a project" do
+    before do
+      sign_in data_manager
+    end
+
+    it "provides the xml metadata for a project" do
+      get project_url(project), params: { format: :xml }
+      expect(response.content_type).to match "xml"
+      expect(response.code).to eq "200"
+    end
   end
 end
