3fd9a57846f8b4cc1d7b082d7d91d42e846ca874
leefaisonr <rl3667@princeton.edu>
2025-09-08 11:39:58 -0400

    adjust project resource url

--- spec/requests/skeletor_metadata_spec.rb
+++ spec/requests/skeletor_metadata_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe "The Skeletor Metadata", connect_to_mediaflux: true, metadata: true do
+RSpec.describe "The Skeletor Metadata", connect_to_mediaflux: true, metadata: true, type: :request do
   let!(:sponsor_and_data_manager_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
   let(:data_manager) { FactoryBot.create(:user, mediaflux_session: SystemUser.mediaflux_session) }
   let(:project) { FactoryBot.create(:approved_project, data_manager: data_manager.uid) }
@@ -13,8 +13,9 @@ RSpec.describe "The Skeletor Metadata", connect_to_mediaflux: true, metadata: tr
     end
 
     it "provides the xml metadata for a project" do
-      get project_show_mediaflux_url(project), params: { format: :xml }
+      get project_url(project), params: { format: :xml }
       expect(response.content_type).to match "xml"
+      expect(response.code).to eq "200"
     end
   end
 end
