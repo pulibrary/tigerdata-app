dcd129899431f62ba3701444d660549733416d16
Hector Correa <hector_correa@princeton.edu>
2025-09-10 12:41:49 -0400

    Updated test to look for new values

--- app/controllers/mediaflux_info_controller.rb
+++ app/controllers/mediaflux_info_controller.rb
@@ -4,6 +4,7 @@ class MediafluxInfoController < ApplicationController
   def index
     version_request = Mediaflux::VersionRequest.new(session_token: current_user.mediaflux_session)
     @mf_version = version_request.version
+    @current_user_mediaflux_roles = current_user.current_user_mediaflux_roles(session_token: current_user.mediaflux_session)
     respond_to do |format|
       format.html
       format.json { render json: @mf_version }
