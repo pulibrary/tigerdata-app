f4de68b254479df74955cb7070894942621001d2
Hector Correa <hector_correa@princeton.edu>
2025-09-10 14:35:11 -0400

    Sets a user developer status based on group information (#1825)
    * Display user's roles in mediaflux in the mediaflux info page
    
    * Added new class
    
    * Rubocop nitpicking
    
    * Update the code to fetch the developer status from Mediaflux and update the record in the Rails db
    
    * Update comment
    
    * Updated test to look for new values
    
    * Test the role too
    
    * Added a couple of tests

--- spec/views/mediaflux_infos/index.html.erb_spec.rb
+++ spec/views/mediaflux_infos/index.html.erb_spec.rb
@@ -4,10 +4,13 @@ require "rails_helper"
 RSpec.describe "mediaflux_info/index", type: :view do
   before(:each) do
     assign(:mf_version, { version: "1001" })
+    assign(:current_user_mediaflux_roles, ["role1", "role2"])
   end
 
   it "renders a mediaflux information" do
     render
     assert_select "p", "Connected to MediaFlux 1001 at #{Mediaflux::Connection.host}"
+    assert_select "li", "role1"
+    assert_select "li", "role2"
   end
 end
