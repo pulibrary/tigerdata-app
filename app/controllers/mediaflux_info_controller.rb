16ccaf1ca8db74b07a2566dc3980ccf1de91d8b0
Hector Correa <hector_correa@princeton.edu>
2025-09-12 11:43:51 -0400

    Use the system user to fetch Mediaflux version. Added error handling to the MF request.

--- app/controllers/mediaflux_info_controller.rb
+++ app/controllers/mediaflux_info_controller.rb
@@ -2,8 +2,7 @@
 class MediafluxInfoController < ApplicationController
   # GET /mediaflux_infos or /mediaflux_infos.json
   def index
-    version_request = Mediaflux::VersionRequest.new(session_token: current_user.mediaflux_session)
-    @mf_version = version_request.version
+    @mf_version = mediaflux_version_info
     @mediaflux_roles = User.mediaflux_roles(user: current_user)
     @sysadmin = current_user.sysadmin
     @developer = current_user.developer
@@ -12,4 +11,18 @@ class MediafluxInfoController < ApplicationController
       format.json { render json: @mf_version }
     end
   end
+
+  private
+
+    def mediaflux_version_info
+      # Notice that we use the system user (instead of the current user) in this request to Mediaflux
+      # because the average user does not have access to execute server.version.
+      version_request = Mediaflux::VersionRequest.new(session_token: SystemUser.mediaflux_session)
+      version_request.resolve
+      raise version_request.response_error[:message] if version_request.error?
+      version_request.version
+    rescue => ex
+      Rails.logger.error("Error fetching Mediaflux version: #{ex.message}")
+      { vendor: "N/A", version: "N/A" }
+    end
 end
