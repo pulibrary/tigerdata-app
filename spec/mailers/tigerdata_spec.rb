8be9f4354dbc77b9b62695ba09bb0ba22927f694
Jaymee Hyppolite <54775395+JaymeeH@users.noreply.github.com>
2025-09-15 13:28:28 -0400

    mail the admin who approved the project (#1842)
    * mail the admin who approved the project
    
    * edit html mailer layout, and update specs
    
    * remove comments, and return old approve method
    
    * cop

--- spec/mailers/tigerdata_spec.rb
+++ spec/mailers/tigerdata_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 
 RSpec.describe TigerdataMailer, type: :mailer do
   let!(:sponsor_and_data_manager_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
-  let(:project) { FactoryBot.create :project, project_id: "abc123/def" }
+  let(:project) { FactoryBot.create :project, project_id: "abc123/def" , mediaflux_id: 123}
   let(:project_id) { project.id }
 
   let(:valid_request) do
@@ -16,73 +16,26 @@ RSpec.describe TigerdataMailer, type: :mailer do
 context "When a project is created" do
 
   it "Sends project creation requests" do
-    expect { described_class.with(project_id:).project_creation.deliver }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    expect { described_class.with(project_id:, approver: sponsor_and_data_manager_user).project_creation.deliver }.to change { ActionMailer::Base.deliveries.count }.by(1)
     mail = ActionMailer::Base.deliveries.last
 
-    expect(mail.subject).to eq "New Project Request Ready for Review"
-    expect(mail.to).to eq ["test@example.com"]
+    expect(mail.subject).to eq ("Project: '#{project.title}' has been approved")
+    expect(mail.to).to eq [sponsor_and_data_manager_user.email]
     expect(mail.cc).to eq ["test_to@example.com"]
     expect(mail.from).to eq ["no-reply@princeton.edu"]
+
     html_body = mail.html_part.body.to_s
-    expect(html_body).to have_content(project.metadata_model.title)
-    project.metadata.keys.each do |field|
-      next if ["updated_on", "created_on", "created_by", "updated_by", "departments", "submission"].include?(field)
-      # NOTE: These are not included for 0.6 schema project creation requests
-      next if [
-        "hpc",
-        "data_use_agreement",
-        "project_visibility",
-        "resource_type",
-        "globus_request",
-        "smb_request",
-        "provisional"
-      ].include?(field)
-
-      value = project.metadata[field]
-      value = value.sort.join(", ") if value.is_a? Array
-      # TODO: Uncomment when the fields are available in the tigerdata:project schema
-      # if field == "storage_capacity"
-      #   expect(html_body).to have_content(value[:size][:requested])
-      #   expect(html_body).to have_content(value[:unit][:requested])
-      # elsif field == "storage_performance_expectations"
-      #   expect(html_body).to have_content(value[:requested])
-      # else
-      #   expect(html_body).to have_content(value)
-      # end
-    end
-    expect(mail.attachments.count).to be_positive
-    # testing the json response
-    expect(mail.attachments.first.filename).to eq "abc123_def.json"
-    parsed_body = JSON.parse(mail.attachments.first.body.raw_source)
-    expect(parsed_body).to eq project.metadata
-    expect(mail.attachments.first.mime_type).to eq "application/json"
-    # testing the xml response
-    expect(mail.attachments.second.filename).to eq "abc123_def.xml"
-    expect(mail.attachments.second.body.raw_source).to eq project.to_xml.gsub("\n", "\r\n")
-    expect(mail.attachments.second.mime_type).to eq "application/xml"
-  end
-
-  it "creates a provenance entry for the project creation" do
-
-    expect { described_class.with(project_id:).project_creation.deliver }.to change { ActionMailer::Base.deliveries.count }.by(1)
-    mail = ActionMailer::Base.deliveries.last
-
-    project.reload
-    expect(project.metadata_model.submission).to include("requested_by")
-    expect(project.metadata_model.submission).to include("request_date_time")
-
-    expect(mail.subject).to eq "New Project Request Ready for Review"
-    expect(mail.to).to eq ["test@example.com"]
-    expect(mail.cc).to eq ["test_to@example.com"]
-    expect(mail.from).to eq ["no-reply@princeton.edu"]
-    html_body = mail.html_part.body.to_s
-
     expect(html_body).not_to be_empty
+    expect(html_body).to have_content(project.id)
 
-    expect(html_body).to include("The following project request is ready for review:")
-    expect(html_body).to include("Please log in to the Presentation Layer to view more details and approve the project.")
-    expect(html_body).to include("Gratefully, the RDSS Team")
-    expect(html_body).to include(project.to_xml)
+    expect(html_body).to have_content(project.mediaflux_id)
+    expect(html_body).to have_content(project.title)
+    expect(html_body).to have_content(project.project_directory)
+    expect(html_body).to have_content(project.metadata_json["data_sponsor"])
+    expect(html_body).to have_content(project.metadata_json["data_manager"])
+
+    expect(html_body).to include("The following project has been approved and created:")
+    expect(html_body).to include("For more details, see #{project_url(project)}")
   end
 
   context "when the project ID is invalid or nil" do
