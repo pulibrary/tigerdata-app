814ef6b2b2ff382ab0a258ed8c42d9ec5af8783d
Precilla Prempeh <pprempeh@princeton.edu>
2025-09-19 14:33:25 -0400

    Created a Space Ghost system spec and tested that a user can see the project request wizard button Co-authored-by: Bess Sadler <bess@users.noreply.github.com> Co-authored-by: Robert-Anthony Lee-Faison <leefaisonr@users.noreply.github.com>

---
+++ spec/system/space_ghost_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+require "open-uri"
+
+RSpec.describe "The Space Ghost Epic", connect_to_mediaflux: true, js: true, integration: true do
+  context "user" do
+    let(:user) { FactoryBot.create(:user, uid: "pul123", mediaflux_session: SystemUser.mediaflux_session) }
+    it "displays the new project wizard on the dashboard" do
+      test_strategy = Flipflop::FeatureSet.current.test!
+      test_strategy.switch!(:allow_all_users_wizard_access, true)
+      sign_in user
+      visit "/"
+      expect(page).to have_content("Welcome, #{user.given_name}!")
+      byebug
+    end
+  end
+end
