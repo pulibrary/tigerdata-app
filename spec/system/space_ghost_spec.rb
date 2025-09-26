c671a1d6afc1b3fce24bc2429009d0e5a8d40376
leefaisonr <rl3667@princeton.edu>
2025-09-26 11:21:39 -0400

    Allow users to select and add users to the Data Users table in wizard
    Adjust user lookup controller for non-elevated users
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>
    Co-authored-by: Carolyn Cole <carolyncole@users.noreply.github.com>
    Co-authored-by: Precilla Prempeh <precillap@users.noreply.github.com>

--- spec/system/space_ghost_spec.rb
+++ spec/system/space_ghost_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 require "open-uri"
 
-RSpec.describe "The Space Ghost Epic", type: :system, connect_to_mediaflux: true, js: true, integration: true do
+RSpec.describe "The Space Ghost Epic", type: :system, connect_to_mediaflux: false, js: true do
   context "user" do
     let(:user) { FactoryBot.create(:user, uid: "pul123", mediaflux_session: SystemUser.mediaflux_session) }
     let(:datasponsor) { FactoryBot.create(:project_sponsor, uid: "kl37") } # must be a valid netid
@@ -24,6 +24,7 @@ RSpec.describe "The Space Ghost Epic", type: :system, connect_to_mediaflux: true
       test_strategy.switch!(:allow_all_users_wizard_access, true)
       Affiliation.load_from_file(Rails.root.join("spec", "fixtures", "departments.csv"))
       expect(Project.count).to eq 0
+      another_user = FactoryBot.create(:user)
       sign_in user
       visit "/"
       expect(page).to have_content("Welcome, #{user.given_name}!")
@@ -43,17 +44,17 @@ RSpec.describe "The Space Ghost Epic", type: :system, connect_to_mediaflux: true
       page.find(:datalist_input, "department_find").execute_script("document.getElementById('department_find').dispatchEvent(new Event('input'))")
       expect(page).to have_content("(77777) RDSS-Research Data and Scholarship Services")
       expect(page).to have_field("request[departments][]", type: :hidden, with: "{\"code\":\"77777\",\"name\":\"RDSS-Research Data and Scholarship Services\"}")
-      click_on "Roles and People"
+      click_on "Next"
+      expect(page).to have_content("Assign roles for your project")
       fill_in :request_data_sponsor, with: datasponsor.uid
       fill_in :request_data_manager, with: datamanager.uid
-      expect(page).to have_content("Assign roles for your project")
-      another_user = FactoryBot.create(:user)
       click_on "Add User(s)"
       fill_in :user_find, with: another_user.uid
       sleep(1.2)
       # Non breaking space `u00A0` is at the end of every option to indicate an option was selected
-      # select another_user_str + "\u00A0", from: "user_find"
-      # click_on "Add Users"
+      another_user_str = "(#{another_user.uid}) #{another_user.display_name}"
+      select another_user_str + "\u00A0", from: "user_find"
+      click_on "Add Users"
 
       # next step is to click on "add user" then figure out how to load in the user for the druple
       test_strategy.switch!(:allow_all_users_wizard_access, false)
