00676981257faea099d0ec84a9803b0b95f82b86
Bess Sadler <bess.sadler@princeton.edu>
2025-10-07 16:13:10 -0400

    Attach a plausible event to each form submission

--- spec/system/plausible_spec.rb
+++ spec/system/plausible_spec.rb
@@ -8,32 +8,29 @@ require "rails_helper"
 describe "Plausible custom properties", type: :system, connect_to_mediaflux: false, js: true do
   context "authenticated user" do
     let(:current_user) { FactoryBot.create(:user, uid: "pul123", sysadmin: true) }
-    it "notifies plausible when a project request is begun" do
+    it "notifies plausible when a request is begun" do
       sign_in current_user
       visit "/"
       expect(page.find("#new-project-request-wizard-button")[:class]).to eq "plausible-event-name=New+Project+Request"
     end
 
-    it "notifies plausible when a request is submitted" do
+    it "notifies plausible for each step in the wizard" do
       Affiliation.load_from_file(Rails.root.join("spec", "fixtures", "departments.csv"))
       sign_in current_user
 
       visit "/"
       click_on "New Project Request"
       expect(page).to have_content "Tell us a little about your project!"
-      byebug
+      expect(page.find("#next-button")[:class]).to match(/plausible-event-name=new_project_wizard\/project_information/)
       click_on "Next"
       expect(page).to have_content "Assign roles for your project"
+      expect(page.find("#next-button")[:class]).to match(/plausible-event-name=new_project_wizard\/roles_and_people/)
       click_on "Next"
       expect(page).to have_content "Enter the storage and access needs"
+      expect(page.find("#next-button")[:class]).to match(/plausible-event-name=new_project_wizard\/storage_and_access/)
       click_on "Next"
       expect(page).to have_content "Take a moment to review"
-      click_on "Back"
-      expect(page).to have_content "Enter the storage and access needs"
-      click_on "Back"
-      expect(page).to have_content "Assign roles for your project"
-      click_on "Back"
-      expect(page).to have_content "Tell us a little about your project!"
+      expect(page.find("#next-button")[:class]).to match(/plausible-event-name=new_project_wizard\/review_and_submit/)
     end
   end
 end
