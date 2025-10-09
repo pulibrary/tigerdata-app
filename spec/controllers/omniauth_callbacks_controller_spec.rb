7215c538df4ed182a79da1a34cff61fd01c78614
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-10-09 13:54:44 -0400

    Apply suggestions from code review

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe Users::OmniauthCallbacksController do
   end
 
   context "unknown user" do
-    it "redirects to the homepage with alert" do
+    it "redirects to the help page with alert" do
       controller.request.env["omniauth.auth"] = double(OmniAuth::AuthHash, provider: "cas", uid: nil)
       allow(User).to receive(:from_cas) { nil }
       get :cas
-      expect(response).to redirect_to(root_path)
+      expect(response).to redirect_to(help_path)
       expect(flash.notice).to eq("You can not be signed in at this time.")
     end
   end
@@ -28,7 +28,7 @@ RSpec.describe Users::OmniauthCallbacksController do
       controller.request.env["omniauth.auth"] = double(OmniAuth::AuthHash, provider: "other", uid: nil)
       allow(User).to receive(:from_cas) { nil }
       get :cas
-      expect(response).to redirect_to(help_path)
+      expect(response).to redirect_to(root_path)
       expect(flash.alert).to eq("You are not a recognized CAS user.")
     end
   end
