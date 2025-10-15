c28a0c29edd55128f70cae5a575f5d95eac492f1
Hector Correa <hector_correa@princeton.edu>
2025-10-15 17:35:42 -0400

    Declare the request_presenter for a few more controllers

--- app/controllers/new_project_wizard/project_type_controller.rb
+++ app/controllers/new_project_wizard/project_type_controller.rb
@@ -3,6 +3,7 @@ class NewProjectWizard::ProjectTypeController < RequestWizardsController
   protected
 
     def render_current
+      @request_presenter = RequestPresenter.new(@request_model)
       @form_url = new_project_project_type_save_path(@request_model)
       render "/new_project_wizard/project_type"
     end
