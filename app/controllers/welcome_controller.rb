d8fa5fad12c68b2097b4d0bb7095e88a87942e30
carolyncole <1599081+carolyncole@users.noreply.github.com>
2025-10-16 10:31:44 -0400

    Allow the welcome page to load when the system is disabled (#2034)
    fixes #2027

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
+  skip_before_action :downtime_check
   skip_before_action :verify_authenticity_token
 
   def index
