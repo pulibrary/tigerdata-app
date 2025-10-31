be1dc2199cd91549050ed4d086225a2aefba0d08
Hector Correa <hector_correa@princeton.edu>
2025-10-31 15:46:30 -0400

    Fix path display in dashboard

--- app/presenters/project_dashboard_presenter.rb
+++ app/presenters/project_dashboard_presenter.rb
@@ -42,7 +42,7 @@ class ProjectDashboardPresenter < ProjectShowPresenter
   end
 
   def path
-    "/#{project_metadata.project_directory}"
+    project_directory
   end
 
   # Removes "about" (as in "about 1 month ago") from time_ago_in_words
