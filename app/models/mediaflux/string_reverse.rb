3ed25e857f99337a35c8ae8af7ae24939440d3f0
Bess Sadler <bess.sadler@princeton.edu>
2025-11-07 07:24:01 -0800

    Java plugin proof of concept (#2160)
    Ref #2100

---
+++ app/models/mediaflux/string_reverse.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+module Mediaflux
+  class StringReverse < Request
+    # This is only here to prove that we can use a mediaflux service that is provided by
+    # a java plugin.
+
+    # Constructor
+    # @param session_token [String] the API token for the authenticated session
+    def initialize(string:, session_token:)
+      super(session_token: session_token)
+      @string = string
+    end
+
+    # Specifies the Mediaflux service to use when querying Mediaflux.
+    # @return [String]
+    def self.service
+      "tigerdata.trivial"
+    end
+
+    private
+
+      def build_http_request_body(name:)
+        super do |xml|
+          xml.args do
+            xml.string @string
+          end
+        end
+      end
+  end
+end
