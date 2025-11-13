b82c1f2d6f4d4aa7b8c03e3dc30228c455535d18
Bess Sadler <bess.sadler@princeton.edu>
2025-11-13 10:42:10 -0500

    Use the current_user's session for mediaflux_info page

--- app/controllers/mediaflux_info_controller.rb
+++ app/controllers/mediaflux_info_controller.rb
@@ -16,7 +16,7 @@ class MediafluxInfoController < ApplicationController
   private
 
     def java_plugin_check
-      trivial_attempt = Mediaflux::StringReverse.new(string: "Hello, Mediaflux!", session_token: SystemUser.mediaflux_session)
+      trivial_attempt = Mediaflux::StringReverse.new(string: "Hello, Mediaflux!", session_token: current_user.mediaflux_session)
       trivial_attempt.resolve
       trivial_attempt.response_body
     rescue => ex
@@ -27,7 +27,7 @@ class MediafluxInfoController < ApplicationController
     def mediaflux_version_info
       # Notice that we use the system user (instead of the current user) in this request to Mediaflux
       # because the average user does not have access to execute server.version.
-      version_request = Mediaflux::VersionRequest.new(session_token: SystemUser.mediaflux_session)
+      version_request = Mediaflux::VersionRequest.new(session_token: current_user.mediaflux_session)
       version_request.resolve
       raise version_request.response_error[:message] if version_request.error?
       version_request.version
