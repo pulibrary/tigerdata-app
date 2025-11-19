36877744a69cde441f09f12cf6a91a1c1c4966a9
Hector Correa <hector_correa@princeton.edu>
2025-11-19 08:20:54 -0500

    Automatically log Mediaflux errors (#2215)
    Automatically log to the Rails error log when calls to Mediaflux return
    errors.
    
    Before this PR errors were only logged if we called
    `request.response_error` which we might or not do, but with this PR
    errors will be logged as soon as the Mediaflux request is `resolved` and
    that guarantees that errors will be logged at all times. I am not
    sending the errors to Honeybadger because I have no idea how many errors
    we will see in the logs. Once we see how our logs look like we could
    decided what to send to Honeybadger.
    
    Closes #2167

--- spec/models/mediaflux/service_execute_request_spec.rb
+++ spec/models/mediaflux/service_execute_request_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe Mediaflux::ServiceExecuteRequest, connect_to_mediaflux: true, typ
   let!(:user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
   subject(:request) { described_class.new(session_token: user.mediaflux_session, service_name: "asset.namespace.list") }
   let(:approved_project) { create_project_in_mediaflux(current_user: user) }
-  let(:session_token) { "test-session-token" }
-  let(:identity_token) { "test-identity-token" }
+  let(:session_token) { SystemUser.mediaflux_session }
   let(:mediaflux_url) { Mediaflux::Request.uri.to_s }
 
   describe "#resolve" do
