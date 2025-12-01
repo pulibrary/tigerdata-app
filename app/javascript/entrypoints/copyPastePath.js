17fe14b746f36a97d32d38fb67d792179b863457
Kate Lynch <katelynch@princeton.edu>
2025-12-01 16:32:55 -0500

    Added behavior and language to match Figma

--- app/javascript/entrypoints/copyPastePath.js
+++ app/javascript/entrypoints/copyPastePath.js
@@ -3,31 +3,30 @@ export function copyPastePath() {
 
   // Sets the elements to the proper CSS classes once a value has been copied to the clipboard.
   function setCopiedToClipboard(iconEl, labelEl, normalClass, copiedClass) {
-    $(iconEl).removeClass('bi-clipboard');
-    $(iconEl).addClass('bi-clipboard-check');
-    $(labelEl).text('COPIED');
+    $(iconEl).removeClass('copy-paste-frames');
+    $(iconEl).addClass('copy-paste-check');
+    $(labelEl).text('Copied!');
     $(labelEl).removeClass(normalClass);
     $(labelEl).addClass(copiedClass);
   }
 
   // Resets the elements to the proper CSS classes (e.g. displays as if the copy has not happened)
   function resetCopyToClipboard(iconEl, labelEl, normalClass, copiedClass) {
-    $(labelEl).text('COPY');
+    $(labelEl).text('Copy');
     $(labelEl).removeClass(copiedClass);
     $(labelEl).addClass(normalClass);
-    $(iconEl).addClass('bi-clipboard');
-    $(iconEl).removeClass('bi-clipboard-check');
+    $(iconEl).addClass('copy-paste-frames');
+    $(iconEl).removeClass('copy-paste-check');
   }
 
   // Sets icon and label to indicate that an error happened when copying a value to the clipboard
   function errorCopyToClipboard(iconEl, errorMsg) {
-    $(iconEl).removeClass('bi-clipboard');
-    $(iconEl).addClass('bi-clipboard-minus');
+    $(iconEl).removeClass('copy-paste-frames');
+    $(iconEl).addClass('copy-paste-error');
     console.error(errorMsg);
   }
 
   function copyPath(value, iconEl, labelEl, normalClass, copiedClass) {
-    alert(value);
     // Copy value to the clipboard....
     navigator.clipboard.writeText(value).then(
       () => {
@@ -47,10 +46,14 @@ export function copyPastePath() {
     const project_path = $('#copy-project-path-button').data('url');
     copyPath(
       project_path,
-      '#copy-project-path-icon',
+      '#copy-project-path-button',
       '#copy-project-path-label',
       'copy-project-path-label-normal',
       'copy-project-path-label-copied',
     );
+  });
+
+  $(function() {
+    $("#copy-project-path-button").addClass("copy-paste-frames");
   });
 }
