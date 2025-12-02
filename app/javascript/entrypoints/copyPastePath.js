3d2a02fe4126a85c1cf5cf8966bd44657281f3fc
Kate Lynch <katelynch@princeton.edu>
2025-12-02 10:18:41 -0500

    Copy/paste functionality established (#2257)
    Closes #2254

---
+++ app/javascript/entrypoints/copyPastePath.js
@@ -0,0 +1,58 @@
+// eslint-disable-next-line import/prefer-default-export
+export function copyPastePath() {
+  // Sets the elements to the proper CSS classes once a value has been copied to the clipboard.
+  function setCopiedToClipboard(iconEl, labelEl, normalClass, copiedClass) {
+    $(iconEl).removeClass('copy-paste-frames');
+    $(iconEl).addClass('copy-paste-check');
+    $(labelEl).text('Copied!');
+    $(labelEl).removeClass(normalClass);
+    $(labelEl).addClass(copiedClass);
+  }
+
+  // Resets the elements to the proper CSS classes (e.g. displays as if the copy has not happened)
+  function resetCopyToClipboard(iconEl, labelEl, normalClass, copiedClass) {
+    $(labelEl).text('Copy');
+    $(labelEl).removeClass(copiedClass);
+    $(labelEl).addClass(normalClass);
+    $(iconEl).addClass('copy-paste-frames');
+    $(iconEl).removeClass('copy-paste-check');
+  }
+
+  // Sets icon and label to indicate that an error happened when copying a value to the clipboard
+  function errorCopyToClipboard(iconEl, errorMsg) {
+    $(iconEl).removeClass('copy-paste-frames');
+    $(iconEl).addClass('copy-paste-error');
+    console.error(errorMsg);
+  }
+
+  function copyPath(value, iconEl, labelEl, normalClass, copiedClass) {
+    // Copy value to the clipboard....
+    navigator.clipboard.writeText(value).then(
+      () => {
+        // ...and notify the user
+        setCopiedToClipboard(iconEl, labelEl, normalClass, copiedClass);
+        setTimeout(() => {
+          resetCopyToClipboard(iconEl, labelEl, normalClass, copiedClass);
+        }, 20000);
+      },
+      () => {
+        errorCopyToClipboard(iconEl, 'Copy to clipboard failed');
+      },
+    );
+  }
+
+  $('#copy-project-path-button').on('click', () => {
+    const projectPath = $('#copy-project-path-button').data('url');
+    copyPath(
+      projectPath,
+      '#copy-project-path-button',
+      '#copy-project-path-label',
+      'copy-project-path-label-normal',
+      'copy-project-path-label-copied',
+    );
+  });
+
+  $(() => {
+    $('#copy-project-path-button').addClass('copy-paste-frames');
+  });
+}
