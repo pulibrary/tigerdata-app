dd3fdbd258f9f3124110b881d10c18b468830376
Jaymee Hyppolite <54775395+JaymeeH@users.noreply.github.com>
2026-01-07 14:26:20 -0500

    Update spec/system/new_project_request_wizard_spec.rb
    Co-authored-by: carolyncole <1599081+carolyncole@users.noreply.github.com>

--- spec/system/new_project_request_wizard_spec.rb
+++ spec/system/new_project_request_wizard_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "rails_helper"
-require "open-uri"
 
 describe "New Project Request page", type: :system, connect_to_mediaflux: false, js: true do
   context "unauthenticated user" do
