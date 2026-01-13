5b684d250e31e75042919c5a21fac5b2bc6281b7
HyppoliteJ <jh6441@princeton.edu>
2026-01-13 16:47:30 -0500

    project mediaflux spec

--- spec/models/project_mediaflux_spec.rb
+++ spec/models/project_mediaflux_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe ProjectMediaflux, type: :model do
     request.smb = "Yes"
     request.globus = "Yes"
     request.project_purpose = "administrative"
-    request.approve(current_user)
+    request.approve(sponsor_and_data_manager_user)
   end
 
-  let!(:current_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
+  let!(:sponsor_and_data_manager_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
 
   describe "#create!", connect_to_mediaflux: true do
     context "Using test data" do
@@ -20,12 +20,12 @@ RSpec.describe ProjectMediaflux, type: :model do
       :integration do
         mediaflux_id = project.mediaflux_id
         mediaflux_metadata = Mediaflux::AssetMetadataRequest.new(
-                              session_token: current_user.mediaflux_session,
+                              session_token: sponsor_and_data_manager_user.mediaflux_session,
                               id: mediaflux_id
                             ).metadata
         namespace_path = mediaflux_metadata[:namespace]
         namespace_metadata = Mediaflux::NamespaceDescribeRequest.new(
-                              session_token: current_user.mediaflux_session,
+                              session_token: sponsor_and_data_manager_user.mediaflux_session,
                               path: namespace_path
                             ).metadata
 
@@ -37,7 +37,7 @@ RSpec.describe ProjectMediaflux, type: :model do
         it "adds a quota and other storage options when it creates a project in mediaflux",
         :integration do
           metadata = Mediaflux::AssetMetadataRequest.new(
-            session_token: current_user.mediaflux_session,
+            session_token: sponsor_and_data_manager_user.mediaflux_session,
             id: project.mediaflux_id
           ).metadata
           expect(metadata[:quota_allocation]).not_to be_empty
@@ -60,13 +60,13 @@ RSpec.describe ProjectMediaflux, type: :model do
 
       let(:incomplete_project) do
         request = FactoryBot.create(:request_project)
-        incomplete_project = request.approve(current_user)
+        incomplete_project = request.approve(sponsor_and_data_manager_user)
         incomplete_project.metadata_model.project_id = nil
         incomplete_project
       end
 
       it "should raise a MetadataError if project is invalid", integration: true do
-        project.create!(initial_metadata: incomplete_project.metadata_model, user: current_user)
+        project.create!(initial_metadata: incomplete_project.metadata_model, user: sponsor_and_data_manager_user)
 
         expect(project.valid?).to be false
         expect(project.errors.first).to be_a(ActiveModel::Error)
@@ -74,21 +74,21 @@ RSpec.describe ProjectMediaflux, type: :model do
       end
 
       it "should raise a error if any error occurs in mediaflux", integration: true do
-        expect { incomplete_request.approve(current_user) }.to raise_error(ProjectCreate::ProjectCreateError)
+        expect { incomplete_request.approve(sponsor_and_data_manager_user) }.to raise_error(ProjectCreate::ProjectCreateError)
       end
     end
   end
 
   describe "#update", connect_to_mediaflux: true do
     before do
-      described_class.create!(project: project, user: current_user)
+      described_class.create!(project: project, user: sponsor_and_data_manager_user)
     end
     # Project updates in MediaFlux are not supported yet
     xit "defaults updated_on/by when not provided",
     :integration do
       project.metadata_model.updated_on = nil
       project.metadata_model.updated_by = nil
-      described_class.update(project: project, user: current_user)
+      described_class.update(project: project, user: sponsor_and_data_manager_user)
       expect(project.metadata_model.updated_on).not_to be nil
       expect(project.metadata_model.updated_by).not_to be nil
     end
@@ -99,7 +99,7 @@ RSpec.describe ProjectMediaflux, type: :model do
       updated_on = Time.current.in_time_zone("America/New_York").iso8601
       project.metadata_model.updated_on = updated_on
       project.metadata_model.updated_by = "user123"
-      described_class.update(project: project, user: current_user)
+      described_class.update(project: project, user: sponsor_and_data_manager_user)
       expect(project.metadata_model.updated_on).to eq updated_on
       expect(project.metadata_model.updated_by).to eq "user123"
     end
@@ -110,7 +110,7 @@ RSpec.describe ProjectMediaflux, type: :model do
 
     it "raises errors when project is not accessible", :integration do
       # The project is not in mediaflux and therefore this will raise an exception
-      expect { described_class.xml_payload(project: project_not_in_mediaflux, user: current_user) }.to raise_error(StandardError)
+      expect { described_class.xml_payload(project: project_not_in_mediaflux, user: sponsor_and_data_manager_user) }.to raise_error(StandardError)
     end
   end
 end
