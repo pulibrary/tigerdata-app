8ef6f5023595d2bde2c0d422e87b049d5db0551c
HyppoliteJ <jh6441@princeton.edu>
2026-01-15 12:52:44 -0500

    emulator spec

--- spec/system/emulator_spec.rb
+++ spec/system/emulator_spec.rb
@@ -3,9 +3,9 @@
 require "rails_helper"
 
 describe "Website banner", type: :system, connect_to_mediaflux: true, js: true do
-  let(:current_user) { FactoryBot.create(:trainer, uid: "pul123") }
+  let(:trainer_user) { FactoryBot.create(:trainer, uid: "pul123") }
   it "has the banner on the homepage" do
-    sign_in current_user
+    sign_in trainer_user
     visit "/"
     expect(page).to have_css "#emulator"
   end
