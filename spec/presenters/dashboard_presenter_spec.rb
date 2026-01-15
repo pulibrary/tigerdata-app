bdaa50b2944be9b06bd13ea203992cf299a145ff
HyppoliteJ <jh6441@princeton.edu>
2026-01-15 12:35:17 -0500

    dashboard presenter

--- spec/presenters/dashboard_presenter_spec.rb
+++ spec/presenters/dashboard_presenter_spec.rb
@@ -2,31 +2,34 @@
 require "rails_helper"
 
 describe DashboardPresenter, type: :model, connect_to_mediaflux: false do
-  subject(:presenter) { described_class.new(current_user:) }
+  subject(:presenter) { described_class.new(current_user: sponsor_and_data_manager) }
 
-  let(:current_user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
+  let(:sponsor_and_data_manager) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
   let(:other_user) { FactoryBot.create :user, uid: "kl37" }
 
-  let(:request1) { FactoryBot.create :request_project, data_manager: current_user.uid, data_sponsor: other_user.uid, requested_by: other_user.uid }
-  let(:request2) { FactoryBot.create :request_project, data_manager: other_user.uid, data_sponsor: current_user.uid, requested_by: other_user.uid }
-  let(:request3) { FactoryBot.create :request_project, data_manager: other_user.uid, data_sponsor: other_user.uid, user_roles: [{"uid" => current_user.uid, "read_only" => true}], requested_by: other_user.uid }
+  let(:request1) { FactoryBot.create :request_project, data_manager: sponsor_and_data_manager.uid, data_sponsor: other_user.uid, requested_by: other_user.uid }
+  let(:request2) { FactoryBot.create :request_project, data_manager: other_user.uid, data_sponsor: sponsor_and_data_manager.uid, requested_by: other_user.uid }
+  let(:request3) do
+    FactoryBot.create :request_project, data_manager: other_user.uid, data_sponsor: other_user.uid, user_roles: [{ "uid" => sponsor_and_data_manager.uid, "read_only" => true }],
+                                        requested_by: other_user.uid
+  end
   let(:request4) { FactoryBot.create :request_project, data_manager: other_user.uid, data_sponsor: other_user.uid, requested_by: other_user.uid }
 
-  let!(:project1) { request1.approve(current_user) }
-  let!(:project2) {
-    project = request2.approve(current_user)
+  let!(:project1) { request1.approve(sponsor_and_data_manager) }
+  let!(:project2) do
+    project = request2.approve(sponsor_and_data_manager)
     project.updated_at = ActiveSupport::TimeZone.new("America/New_York").yesterday
     project.save!
     project
-  }
+  end
 
-  let!(:project3) {
-    project = request3.approve(current_user)
+  let!(:project3) do
+    project = request3.approve(sponsor_and_data_manager)
     project.updated_at = ActiveSupport::TimeZone.new("America/New_York").tomorrow
     project.save!
     project
-  }
-  let!(:project4) { request4.approve(current_user) }
+  end
+  let!(:project4) { request4.approve(sponsor_and_data_manager) }
 
   describe "#all_projects" do
     it "returns an empty array for a normal user" do
@@ -35,7 +38,7 @@ describe DashboardPresenter, type: :model, connect_to_mediaflux: false do
 
     context "for a sysadmin" do
       before do
-        current_user.developer = true # so that is detected as a sysadmin
+        sponsor_and_data_manager.developer = true # so that is detected as a sysadmin
       end
 
       it "returns the list of all projects, sorted" do
@@ -46,30 +49,31 @@ describe DashboardPresenter, type: :model, connect_to_mediaflux: false do
   end
 
   describe "dashboard_projects" do
-      it "returns the list of the user's projects, sorted" do
-        expect(presenter.dashboard_projects.count).to eq(3)
-        expect(presenter.dashboard_projects.map(&:id)).to eq([project3.id, project1.id, project2.id])
-      end
+    it "returns the list of the user's projects, sorted" do
+      expect(presenter.dashboard_projects.count).to eq(3)
+      expect(presenter.dashboard_projects.map(&:id)).to eq([project3.id, project1.id, project2.id])
+    end
   end
 
   describe "#role" do
     it "detects the proper roles for each project" do
       projects = presenter.dashboard_projects
       expect(projects.map(&:id)).to eq([project3.id, project1.id, project2.id])
-      expect(projects[0].role(current_user)).to be "Data User"
-      expect(projects[1].role(current_user)).to be "Data Manager"
-      expect(projects[2].role(current_user)).to be "Sponsor"
+      expect(projects[0].role(sponsor_and_data_manager)).to be "Data User"
+      expect(projects[1].role(sponsor_and_data_manager)).to be "Data Manager"
+      expect(projects[2].role(sponsor_and_data_manager)).to be "Sponsor"
     end
   end
 
   describe "my_inventory_requests" do
     it "returns the requests" do
-      inv_req = FileInventoryRequest.create!(user_id: current_user.id, project_id: project1.id, job_id: 123, state: UserRequest::COMPLETED,
-                                     request_details: { project_title: project1.title }, completion_time: 1.day.ago)
-      inv_req2 = FileInventoryRequest.create!(user_id: current_user.id, project_id: project1.id, job_id: 123, state: UserRequest::PENDING,
-                                     request_details: { project_title: project1.title }, completion_time: nil)
-      inv_req3 = FileInventoryRequest.create!(user_id: other_user.id, project_id: project1.id, job_id: 123, state: UserRequest::PENDING,
-                                     request_details: { project_title: project1.title }, completion_time: nil)
+      inv_req = FileInventoryRequest.create!(user_id: sponsor_and_data_manager.id, project_id: project1.id, job_id: 123, state: UserRequest::COMPLETED,
+                                             request_details: { project_title: project1.title }, completion_time: 1.day.ago)
+      inv_req2 = FileInventoryRequest.create!(user_id: sponsor_and_data_manager.id, project_id: project1.id, job_id: 123, state: UserRequest::PENDING,
+                                              request_details: { project_title: project1.title }, completion_time: nil)
+      # Leading underscore to show that the request is intentonally unused, and should be ignored by linting tools
+      _inv_req3 = FileInventoryRequest.create!(user_id: other_user.id, project_id: project1.id, job_id: 123, state: UserRequest::PENDING,
+                                               request_details: { project_title: project1.title }, completion_time: nil)
       expect(presenter.my_inventory_requests.count).to eq(2)
       expect(presenter.my_inventory_requests.map(&:id)).to eq([inv_req.id, inv_req2.id])
     end
