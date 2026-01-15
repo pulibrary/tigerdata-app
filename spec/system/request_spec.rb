b88956e9336198cf4479510f6fa563d0b4da4490
HyppoliteJ <jh6441@princeton.edu>
2026-01-15 13:22:25 -0500

    request spec

--- spec/system/request_spec.rb
+++ spec/system/request_spec.rb
@@ -13,12 +13,12 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
   end
 
   context "authenticated user" do
-    let(:current_user) { sponsor_and_data_manager }
+    # let(:sponsor_and_data_manager) { sponsor_and_data_manager }
     let(:sponsor_user) { FactoryBot.create(:project_sponsor, uid: "kl37", mediaflux_session: SystemUser.mediaflux_session) }
     let(:sysadmin_user) { FactoryBot.create(:sysadmin, uid: "puladmin", mediaflux_session: SystemUser.mediaflux_session) }
-    let(:developer) { FactoryBot.create(:developer, uid: "root", mediaflux_session: SystemUser.mediaflux_session) }
-    let!(:data_manager) { FactoryBot.create(:data_manager, uid: "rl3667", mediaflux_session: SystemUser.mediaflux_session) }
-    let(:request) { Request.create(request_title: "abc123", project_title: "project", requested_by: current_user.uid, project_folder: "folder123") }
+    let(:developer_user) { FactoryBot.create(:developer, uid: "root", mediaflux_session: SystemUser.mediaflux_session) }
+    let!(:manager_user) { FactoryBot.create(:data_manager, uid: "rl3667", mediaflux_session: SystemUser.mediaflux_session) }
+    let(:request) { Request.create(request_title: "abc123", project_title: "project", requested_by: sponsor_and_data_manager.uid, project_folder: "folder123") }
     let(:full_request) do
       Request.create(
         request_type: nil,
@@ -27,7 +27,7 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         created_at: Time.current.in_time_zone("America/New_York").iso8601,
         state: "draft",
         data_sponsor: sponsor_user.uid,
-        data_manager: data_manager.uid,
+        data_manager: manager_user.uid,
         departments:
           [{ "code" => "77777", "name" => "RDSS-Research Data and Scholarship Services" }, { "code" => "88888", "name" => "PRDS-Princeton Research Data Service" }],
         description: "Test project description",
@@ -36,10 +36,10 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         project_folder: "test_project_folder",
         project_id: nil,
         storage_size: nil,
-        requested_by: current_user.uid,
+        requested_by: sponsor_and_data_manager.uid,
         storage_unit: "GB",
         quota: "500 GB",
-        user_roles: [{ "uid" => current_user.uid, "name" => current_user.display_name }]
+        user_roles: [{ "uid" => sponsor_and_data_manager.uid, "name" => sponsor_and_data_manager.display_name }]
       )
     end
     let(:submitted_request) do
@@ -56,7 +56,7 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         created_at: Time.current.in_time_zone("America/New_York").iso8601,
         state: "draft",
         data_sponsor: sponsor_user.uid,
-        data_manager: data_manager.uid,
+        data_manager: manager_user.uid,
         departments:
           [{ "code" => "77777", "name" => "RDSS-Research Data and Scholarship Services" }, { "code" => "88888", "name" => "PRDS-Princeton Research Data Service" }],
         description: "Test project description",
@@ -65,10 +65,10 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         project_folder: "test_project_folder",
         project_id: nil,
         storage_size: "1725",
-        requested_by: current_user.uid,
+        requested_by: sponsor_and_data_manager.uid,
         storage_unit: "TB",
         quota: "custom",
-        user_roles: [{ "uid" => current_user.uid, "name" => current_user.display_name }]
+        user_roles: [{ "uid" => sponsor_and_data_manager.uid, "name" => sponsor_and_data_manager.display_name }]
       )
     end
     let(:bluemountain) do
@@ -79,7 +79,7 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         created_at: Time.current.in_time_zone("America/New_York").iso8601,
         state: Request::SUBMITTED,
         data_sponsor: sponsor_user.uid,
-        data_manager: data_manager.uid,
+        data_manager: manager_user.uid,
         departments:
           [{ "code" => "41000", "name" => "LIB-PU Library" }],
         description: "This collection contains important periodicals of the European avant-garde.",
@@ -88,10 +88,10 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         project_folder: "#{random_project_directory}-bluemountain",
         project_id: nil,
         storage_size: nil,
-        requested_by: current_user.uid,
+        requested_by: sponsor_and_data_manager.uid,
         storage_unit: "GB",
         quota: "500 GB",
-        user_roles: [{ "uid" => current_user.uid, "name" => current_user.display_name }]
+        user_roles: [{ "uid" => sponsor_and_data_manager.uid, "name" => sponsor_and_data_manager.display_name }]
       )
     end
     # rubocop:disable Layout/LineLength
@@ -103,7 +103,7 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         created_at: Time.current.in_time_zone("America/New_York").iso8601,
         state: "draft",
         data_sponsor: sponsor_user.uid,
-        data_manager: data_manager.uid,
+        data_manager: manager_user.uid,
         departments:
           [{ "code" => "77777", "name" => "RDSS-Research Data and Scholarship Services" }, { "code" => "88888", "name" => "PRDS-Princeton Research Data Service" }],
         description: "Test project description",
@@ -112,10 +112,10 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         project_folder: "test_project_folder",
         project_id: nil,
         storage_size: nil,
-        requested_by: current_user.uid,
+        requested_by: sponsor_and_data_manager.uid,
         storage_unit: "GB",
         quota: "500 GB",
-        user_roles: [{ "uid" => current_user.uid, "name" => current_user.display_name }]
+        user_roles: [{ "uid" => sponsor_and_data_manager.uid, "name" => sponsor_and_data_manager.display_name }]
       )
     end
     let(:description_too_long) do
@@ -126,7 +126,7 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         created_at: Time.current.in_time_zone("America/New_York").iso8601,
         state: "draft",
         data_sponsor: sponsor_user.uid,
-        data_manager: data_manager.uid,
+        data_manager: manager_user.uid,
         departments:
           [{ "code" => "77777", "name" => "RDSS-Research Data and Scholarship Services" }, { "code" => "88888", "name" => "PRDS-Princeton Research Data Service" }],
         description: "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos. Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.",
@@ -135,10 +135,10 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         project_folder: "test_project_folder",
         project_id: nil,
         storage_size: nil,
-        requested_by: current_user.uid,
+        requested_by: sponsor_and_data_manager.uid,
         storage_unit: "GB",
         quota: "500 GB",
-        user_roles: [{ "uid" => current_user.uid, "name" => current_user.display_name }]
+        user_roles: [{ "uid" => sponsor_and_data_manager.uid, "name" => sponsor_and_data_manager.display_name }]
       )
     end
     # rubocop:enable Layout/LineLength
@@ -148,13 +148,13 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
 
     context "user without a role" do
       it "does not show the New Project Requests page to users without a role" do
-        sign_in current_user
+        sign_in sponsor_and_data_manager
         visit "/requests"
         expect(page).to have_content "You do not have access to this page."
       end
 
       it "does not show the approve button on a single request view for users without a role" do
-        sign_in current_user
+        sign_in sponsor_and_data_manager
         visit "/requests/#{submitted_request.id}"
         expect(page).to have_content request.project_title
         expect(page).not_to have_content("Approve request")
@@ -179,12 +179,12 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
 
     context "data_manager" do
       it "does not show the New Project Requests page to data managers" do
-        sign_in data_manager
+        sign_in manager_user
         visit "/requests"
         expect(page).to have_content "You do not have access to this page."
       end
       it "does not show the approve button on a single request view for data managers" do
-        sign_in data_manager
+        sign_in manager_user
         visit "/requests/#{submitted_request.id}"
         expect(page).to have_content request.project_title
         expect(page).not_to have_content("Approve request")
@@ -281,7 +281,7 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         end
         within(".data-sponsor") do
           expect(page).to have_content("This field is required.")
-          select_user(current_user, "data_sponsor", "request[data_sponsor]")
+          select_user(sponsor_and_data_manager, "data_sponsor", "request[data_sponsor]")
           expect(page).not_to have_content("This field is required.")
         end
         within(".project-folder") do
@@ -348,13 +348,13 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
 
     context "developer" do
       it "shows the New Project Requests page to developers" do
-        sign_in developer
+        sign_in developer_user
         visit "/requests"
         expect(page).to have_content "New Project Requests"
       end
 
       it "shows the approve button on a single request view for developers" do
-        sign_in developer
+        sign_in developer_user
         put new_project_review_and_submit_save_url(full_request.id, request: { request_title: "new title", project_title: "new project" }, commit: "Next")
         expect(response).to redirect_to(request_submit_path)
         sign_in sysadmin_user
