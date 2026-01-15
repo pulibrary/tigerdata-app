e050e202f5f1ea01ca4689547e7053c0cbd3e459
HyppoliteJ <jh6441@princeton.edu>
2026-01-15 13:05:36 -0500

    mediaflux info spec

--- spec/system/mediaflux_info_spec.rb
+++ spec/system/mediaflux_info_spec.rb
@@ -3,11 +3,11 @@
 require "rails_helper"
 
 describe "mediaflux_info", type: :system, js: true, connect_to_mediaflux: true do
-  let(:current_user) { FactoryBot.create(:user, uid: "pul123", mediaflux_session: SystemUser.mediaflux_session) }
+  let(:researcher_user) { FactoryBot.create(:user, uid: "pul123", mediaflux_session: SystemUser.mediaflux_session) }
   let(:mflux_port) { Rails.configuration.mediaflux["api_port"] }
 
   it "shows the mediaflux version" do
-    sign_in current_user
+    sign_in researcher_user
     visit "/mediaflux_info"
     expect(page).to have_content("Connected to MediaFlux")
     expect(page).to have_content("Mediaflux Port:\n#{mflux_port}\n")
