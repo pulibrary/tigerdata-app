54ed16b9484562933771273e639615fa7ba86c4a
Hector Correa <hector_correa@princeton.edu>
2026-01-22 15:31:12 -0500

    Tweaked the test

--- spec/models/mediaflux/package_version_request_spec.rb
+++ spec/models/mediaflux/package_version_request_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe Mediaflux::PackageDescribeRequest, connect_to_mediaflux: true, ty
     it "returns the version information" do
       # A well-known existing package
       request = described_class.new(session_token: user.mediaflux_session, package_name: "tigerdata-plugin")
+      expect(request.error?).to be false
       expect(request.describe[:version] >= "0.0.2").to be true
 
       # A non-existing existing package
       request = described_class.new(session_token: user.mediaflux_session, package_name: "blah-blah-blah-tigerdata")
+      expect(request.error?).to be true
       expect(request.describe[:version]).to eq ""
     end
   end
