0bbc1c7ce571728b08f4d74966da0485c81356bb
Hector Correa <hector_correa@princeton.edu>
2026-03-05 15:05:51 -0500

    Adjust test to use  new data structure

--- spec/views/session_info/index.html.erb_spec.rb
+++ spec/views/session_info/index.html.erb_spec.rb
@@ -6,13 +6,14 @@ RSpec.describe "session_info/index", type: :view do
 
   before(:each) do
     assign(:current_user, user)
-    assign(:mediaflux_info, { version: "1001" })
+    assign(:mediaflux_info, { server_version: "1001" })
     assign(:mediaflux_roles, ["role1", "role2"])
   end
 
   it "renders a mediaflux information" do
     render
     assert_select "p", "Connected to MediaFlux at #{Mediaflux::Connection.host} port 8888"
+    assert_select "dd", "1001"
     assert_select "dd", user.uid
     assert_select "li", "role1"
     assert_select "li", "role2"
