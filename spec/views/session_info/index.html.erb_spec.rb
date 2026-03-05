dbd38b887e0cc3d0746baa3688885042b7adfd4d
Hector Correa <hector_correa@princeton.edu>
2026-03-05 15:01:20 -0500

    Adjust the test to use the renamed controller

--- spec/views/session_info/index.html.erb_spec.rb
+++ spec/views/session_info/index.html.erb_spec.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe "mediaflux_info/index", type: :view do
+RSpec.describe "session_info/index", type: :view do
   let(:user) { FactoryBot.create(:sponsor_and_data_manager, uid: "tigerdatatester", mediaflux_session: SystemUser.mediaflux_session) }
 
   before(:each) do
     assign(:current_user, user)
-    assign(:mf_version, { version: "1001" })
+    assign(:mediaflux_info, { version: "1001" })
     assign(:mediaflux_roles, ["role1", "role2"])
   end
 
   it "renders a mediaflux information" do
     render
-    assert_select "p", "Connected to MediaFlux 1001 at #{Mediaflux::Connection.host}"
+    assert_select "p", "Connected to MediaFlux at #{Mediaflux::Connection.host} port 8888"
     assert_select "dd", user.uid
     assert_select "li", "role1"
     assert_select "li", "role2"
