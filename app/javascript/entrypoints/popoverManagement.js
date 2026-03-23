13f6905fef4e313574034bf1f0d097b5e0092feb
HyppoliteJ <jh6441@princeton.edu>
2026-03-23 12:58:08 -0400

    fix error message text hide storage detail modal show storage detail modal when request modal is cancelled

--- app/javascript/entrypoints/popoverManagement.js
+++ app/javascript/entrypoints/popoverManagement.js
@@ -62,25 +62,47 @@ function checkStoragePopoverFields() {
   // TODO: add check for date needed once the date picker is added
 
   // check that all fields are filled out before allowing the popover to close
-  fields.forEach((field) => {
-    if (field.trim().length === 0 || !field) {
+
+  for (let i = 0; i < fields.length; i += 1) {
+    if (fields[i].trim().length === 0 || !fields[i]) {
       errorMessage.forEach((message) => {
         message.hidden = false; // eslint-disable-line no-param-reassign
       });
       return false;
     }
-
-    return true;
-  });
+  }
+  return true;
 }
 export function requestMoreStoragePopoverManagement() {
   const requestMoreStoragePopover = document.getElementById('request-more-storage');
-  const switchRequestMoreStoragePopover = document.getElementById('submit-storage-request');
+  const submitStorageRequestPopover = document.getElementById('submit-storage-request');
+  const cancelStorageRequestPopover = document.getElementById('cancel-storage-request');
+  const storageDetail = document.getElementById('storage-details');
 
-  switchRequestMoreStoragePopover.addEventListener('click', () => {
+  // hide the storage detail when the user opens more storage modal
+  const requestMoreStorageModal = document.getElementsByClassName('request-more-storage-modal');
+  for (let i = 0; i < requestMoreStorageModal.length; i += 1) {
+    requestMoreStorageModal[i].addEventListener('beforetoggle', (event) => {
+      if (event.newState === 'open') {
+        storageDetail.hidePopover();
+      } else {
+        // clear the error message when the popover is closed
+        const errorMessage = document.querySelectorAll('.storage-modal-error');
+        errorMessage.forEach((message) => {
+          message.hidden = true; // eslint-disable-line no-param-reassign
+        });
+      }
+    });
+  }
+
+  submitStorageRequestPopover.addEventListener('click', () => {
     if (checkStoragePopoverFields()) {
       // TODO: submit the form and trigger the mailer, and then close the popover
       requestMoreStoragePopover.hidePopover();
     }
+  });
+
+  cancelStorageRequestPopover.addEventListener('click', () => {
+    storageDetail.showPopover();
   });
 }
