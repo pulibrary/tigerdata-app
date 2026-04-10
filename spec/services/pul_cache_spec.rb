f2d3d9daabe8615240c3286c6dbdbc266e5093e3
HyppoliteJ <jh6441@princeton.edu>
2026-04-10 15:21:55 -0400

    Cover PULCache in test suite

---
+++ spec/services/pul_cache_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe PULCache do
+  describe "#check!" do
+    let(:subject) { described_class.new }
+
+    it "writes and reads the same value" do
+      expect { subject.check! }.not_to raise_error
+    end
+
+    it "raises an exception if the values are different" do
+      allow(Rails.cache).to receive(:read).and_return((1.hour.from_now).to_fs(:rfc2822))
+      expect { subject.check! }.to raise_error(PULCacheException, /different values/)
+    end
+  end
+end
