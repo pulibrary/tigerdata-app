a826ee41b5f178f2577b404f71d6a4ce112a053b
HyppoliteJ <jh6441@princeton.edu>
2026-04-13 11:40:17 -0400

    enable mem cache store in ci environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
     "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
 
-  # Show full error reports and disable caching.
+  # Show full error reports
+  # Allow caching to test PULCache.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.cache_store = :memory_store
