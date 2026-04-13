5c7cbcffd62cf35fad63680b19b97a65dc05985c
leefaisonr <rl3667@princeton.edu>
2026-04-13 15:41:02 -0400

    Reformat breadcrumbs to include info about each portion of the path Co-authored-by: Carolyn Cole <carolyncole@users.noreply.github.com> Co-authored-by: Jaymee Hyppolite <jaymeeh@users.noreply.github.com> Co-authored-by: Precilla Prempeh <precillap@users.noreply.github.com>

--- app/javascript/vue_components/file_browser.vue
+++ app/javascript/vue_components/file_browser.vue
@@ -1,6 +1,13 @@
 <template>
+{{ displayedFolders }}
+  <div class="breadcrumb-list">
+    <div v-for="(path, i) in displayedFolders">
+      {{ path.name }}
+      {{ path.id }}
+    </div>
+  </div>
+  <div class="table project-files">
   <div class="file-browser">
-    {{ displayedPath }}
     <table class="project-contents">
       <thead>
         <tr class="content heading">
@@ -31,6 +38,7 @@
       </tbody>
     </table>
   </div>
+  </div>
 </template>
 <script setup>
 import { ref } from 'vue';
@@ -53,6 +61,13 @@ const props = defineProps({
     required: true,
   },
   /**
+   * the current path and id of the files we are browsing
+   */
+  currentCollection: {
+    type: Object,
+    required: true,
+  },
+  /**
    * the hidden root path for mediaflux
    */
   hiddenRoot: {
@@ -68,9 +83,13 @@ const props = defineProps({
     required: true,
   },
 });
+function splitPaths(displayedPath){
+return displayedPath.split("/");
+}
 
 const displayedFiles = ref(props.files);
 const displayedPath = ref(props.currentPath);
+const displayedFolders = ref([JSON.parse(props.currentCollection)]);
 const hiddenRoot = ref(props.hiddenRoot);
 const isLoadingFiles = ref(false);
 
@@ -84,6 +103,7 @@ async function onClickCollection(file) {
   isLoadingFiles.value = true;
   displayedFiles.value = await loadFiles(file.id);
   displayedPath.value = file.path.replace(hiddenRoot.value, '');
+  displayedFolders.value.push({id: file.id, path: file.path, name: file.name});
   isLoadingFiles.value = false;
 }
 </script>
@@ -95,5 +115,13 @@ async function onClickCollection(file) {
 .loading {
   opacity: 0.5;
   background-color: gray;
+}
+
+.breadcrumb-list {
+display: flex;
+width: 65.8125rem;
+height: 1.8125rem;
+align-items: center;
+gap: 0.625rem;
 }
 </style>
