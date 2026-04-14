11c44093dda362f4ed3f5807d0e60c12d3a11c5a
JaymeeH <jaymeehyppolite@gmail.com>
2026-04-14 15:09:57 -0400

    testing cache store

--- spec/services/pul_cache_spec.rb
+++ spec/services/pul_cache_spec.rb
@@ -7,26 +7,8 @@ RSpec.describe PULCache do
 
     before do
       if ENV["CI"].present?
-        # In CI, we want to test that the cache is working at all, so we use the memory store.
-        puts "Running in CI environment, using memory store for cache."
-        puts "Current cache store: #{Rails.cache.class.name}"
-        puts Rails.env.test?.to_s
-        puts ENV["TEST"].present?
-        config = Rails.application.config
-        config.consider_all_requests_local = true
-        config.action_mailer.perform_caching = false
-        config.cache_store = :memory_store
-        puts "Current cache store: #{Rails.cache.class.name}"
-      end
-    end
-
-    after do
-      if ENV["CI"].present?
-        # Reset the cache store to the default for other tests.
-        config = Rails.application.config
-        config.consider_all_requests_local = true
-        config.action_mailer.perform_caching = true
-        config.cache_store = :null_store
+        memory_store = ActiveSupport::Cache.lookup_store(:memory_store)
+        allow(Rails).to receive(:cache).and_return(memory_store)
       end
     end
 
