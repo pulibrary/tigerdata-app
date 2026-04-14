f579aa4ae47300ec6de7e081fb8a07529402d263
JaymeeH <jaymeehyppolite@gmail.com>
2026-04-14 11:11:02 -0400

    set cache store only for pul cache spec

--- spec/services/pul_cache_spec.rb
+++ spec/services/pul_cache_spec.rb
@@ -5,6 +5,22 @@ RSpec.describe PULCache do
   describe "#check!" do
     let(:subject) { described_class.new }
 
+    before do
+      if ENV["CI"].present?
+        # In CI, we want to test that the cache is working at all, so we use the memory store.
+        config = Rails.application.config
+        config.cache_store = :memory_store
+      end
+    end
+
+    after do
+      if ENV["CI"].present?
+        # Reset the cache store to the default for other tests.
+        config = Rails.application.config
+        config.cache_store = :null_store
+      end
+    end
+
     it "writes and reads the same value" do
       expect { subject.check! }.not_to raise_error
     end
