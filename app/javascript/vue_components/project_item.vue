74943c7b07f3b1e6abcceb89506b759a124dd316
JaymeeH <jaymeehyppolite@gmail.com>
2026-05-15 15:23:44 -0400

    add badge class to netid

--- app/javascript/vue_components/project_item.vue
+++ app/javascript/vue_components/project_item.vue
@@ -77,9 +77,15 @@
         </div>
         <div class="inline-container">
           <header class="fw-semibold" data-attribute-name="createdBy">Created By</header>
-          <p class="project-file-attribute font-monospace" data-attribute-name="createdBy">
-            {{ parseCreatedBy(displayedObject.created_by) }}
-          </p>
+          <template v-if ="parseCreatedBy(displayedObject.created_by) ">
+            <p class="createdByContainer">
+              <p class="project-file-attribute font-monospace" data-attribute-name="createdBy"> {{ displayName }}</p>
+              <p class="lux-badge lux-badge-gray badge-text details-netid" data-attribute-name="createdByNetid"> {{ netid}}</p>
+            </p>
+          </template>
+          <template v-else>
+            <p class="project-file-attribute font-monospace" data-attribute-name="createdBy"> {{ displayedObject.created_by  }} </p>
+          </template>
         </div>
       </div>
     </div>
@@ -106,21 +112,20 @@ const props = defineProps({
 const hiddenRoot = ref(props.hiddenRoot);
 const displayedPath = ref(props.currentObject.path.replace(hiddenRoot.value, ''));
 const displayedObject = ref(props.currentObject);
-// replace created_by with parsed version that separates the name from the netid
-const createdBy = ref(displayedObject.created_by);
-/* eslint-disable no-console */
 
+const standardUser = ref(false);
 const displayName = ref('');
 const netid = ref('');
 
 function parseCreatedBy(createdBy) {
-  // debugger;
   if (createdBy.includes('(')) {
     displayName.value = createdBy.split('(')[0].trim();
     netid.value = createdBy.split('(')[1].split(':', 2)[1].replace(')', '');
+    standardUser.value = true;
     return true;
   }
-  return createdBy;
+  standardUser.value = false;
+  return false;
 }
 
 watch(
@@ -192,6 +197,15 @@ watch(
   .info-container {
     display: flex;
     align-items: center;
+  }
+}
+.createdByContainer {
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+
+  .details-netid {
+    margin: -0.75rem 0rem 0rem 0rem;
   }
 }
 </style>
