782f0930abb97659019c18c57a0992873a5ab3d7
Bess Sadler <bess.sadler@princeton.edu>
2026-07-08 12:25:15 -0400

    Retry NewProjectRequest.last in rspec tests (#2752)
    Sometimes tests are failing b/c of a lag in the database. This change
    will retry the failing database query until it succeeds.

--- spec/system/new_project_request_wizard_spec.rb
+++ spec/system/new_project_request_wizard_spec.rb
@@ -508,7 +508,13 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         # Clicking on the breadcrumb saves the user changes
         click_on "Dashboard"
         expect(page).to have_content "Welcome, #{researcher_user.given_name}!"
-        request = NewProjectRequest.last
+
+        # Sometimes the NewProjectRequest is not created yet and it makes the test fail, so let's retry until it is created
+        begin
+          request = NewProjectRequest.last
+          redo if request.nil?
+        end
+
         expect(request.project_title).to eq("Dashboard Redirect Test")
         expect(page).to have_content("Draft request saved automatically")
       end
@@ -523,7 +529,12 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
         # Clicking on the TigerData logo saves the user changes
         find("#logo.header-image").click
         expect(page).to have_content "Welcome, #{researcher_user.given_name}!"
-        request = NewProjectRequest.last
+
+        # Sometimes the NewProjectRequest is not created yet and it makes the test fail, so let's retry until it is created
+        begin
+          request = NewProjectRequest.last
+          redo if request.nil?
+        end
         expect(request.project_title).to eq("Dashboard Redirect Test")
         expect(page).to have_content("Draft request saved automatically")
       end
@@ -539,8 +550,14 @@ describe "New Project Request page", type: :system, connect_to_mediaflux: false,
           # Clicking on the side panel step does not display the flash message
           click_on "Roles and People"
           expect(page).not_to have_content "Draft request saved automatically"
-          request = NewProjectRequest.last
-          expect(request.project_title).to eq("Dashboard Redirect Test")
+
+          # Sometimes the NewProjectRequest is not created yet and it makes the test fail, so let's retry until it is created
+          begin
+            request = NewProjectRequest.last
+            redo if request.nil?
+          end
+
+          expect(request&.project_title).to eq("Dashboard Redirect Test")
         end.to change { NewProjectRequest.count }.by(1)
       end
 
