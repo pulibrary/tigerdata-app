87f4d9e246ca1e4e24b364be1b753328ac314525
Carolyn Cole <cac9@princeton.edu>
2026-08-20 13:58:57 -0400

    Adding a feature flipper to enable entrai id login fixes #2841

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -28,4 +28,29 @@ describe "home", type: :system do
       test_strategy.switch!(:planned_maintenance, false)
     end
   end
+
+  context "when Entra login is enabled" do
+    it "includes the Entra login URL in the header" do
+      test_strategy = Flipflop::FeatureSet.current.test!
+      original_value = Flipflop.entra_enabled?
+      test_strategy.switch!(:entra_enabled, true)
+
+      visit "/"
+
+      expect(page).to have_css "header form[action='TODO: fill in with real path likely user_entra_id_omniauth_authorize_path']"
+      test_strategy.switch!(:entra_enabled, original_value)
+    end
+  end
+  context "when Entra login is not enabled" do
+    it "includes the CAS login URL in the header" do
+      test_strategy = Flipflop::FeatureSet.current.test!
+      original_value = Flipflop.entra_enabled?
+      test_strategy.switch!(:entra_enabled, false)
+
+      visit "/"
+
+      expect(page).to have_css "header form[action='#{user_cas_omniauth_authorize_path}']"
+      test_strategy.switch!(:entra_enabled, original_value)
+    end
+  end
 end
