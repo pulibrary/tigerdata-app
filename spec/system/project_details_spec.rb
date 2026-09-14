50e051e92d4794c419d90d825573babae38579e3
leefaisonr <rl3667@princeton.edu>
2026-09-14 13:52:44 -0400

    update tests for data security field

--- spec/system/project_details_spec.rb
+++ spec/system/project_details_spec.rb
@@ -226,7 +226,7 @@ RSpec.describe "Project Details Page", type: :system, connect_to_mediaflux: true
         expect(page).to have_content("Research")
 
 
-        expect(page).to have_content("Data Security code goes here")
+        expect(page).to have_content("Data Security")
       end
       it "copies the project path to the clipboard" do
         sign_in sponsor_user
