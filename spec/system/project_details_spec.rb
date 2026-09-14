fd1fe0f43105320ae3dacaf3d3e37ebaf4bdab6c
leefaisonr <rl3667@princeton.edu>
2026-09-14 15:47:34 -0400

    add additional styling for data sec field

--- spec/system/project_details_spec.rb
+++ spec/system/project_details_spec.rb
@@ -224,8 +224,6 @@ RSpec.describe "Project Details Page", type: :system, connect_to_mediaflux: true
         # expect(page).to have_content("Storage Performance Expectations\nRequested\nstandard\nApproved\nslow")
         expect(page).to have_content("RDSS-Research Data and Scholarship Services")
         expect(page).to have_content("Research")
-
-
         expect(page).to have_content("Data Security")
       end
       it "copies the project path to the clipboard" do
